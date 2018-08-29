464b61e158a8ff223a088377fe452f1232d5fed3
Dustin Carlino <dabreegster@gmail.com>
2018-08-29 12:31:27 -0700

    make traffic signals go a little faster by filling out more turns

--- control/src/traffic_signals.rs
+++ control/src/traffic_signals.rs
@@ -23,7 +23,7 @@ impl ControlTrafficSignal {
         assert!(map.get_i(intersection).has_traffic_signal);
         ControlTrafficSignal {
             intersection,
-            cycles: ControlTrafficSignal::greedy_assignment(map, intersection),
+            cycles: greedy_assignment(map, intersection),
         }
     }
 
@@ -67,56 +67,6 @@ impl ControlTrafficSignal {
         let remaining_cycle_time = next_cycle_time - time;
         (cycle, remaining_cycle_time)
     }
-
-    fn greedy_assignment(map: &Map, intersection: IntersectionID) -> Vec<Cycle> {
-        /*
-        // TODO should be a tmp hack; intersections with no turns aren't even valid
-        if map.get_turns_in_intersection(intersection).is_empty() {
-            println!("WARNING: {} has no turns", intersection);
-            return vec![Cycle {
-                turns: Vec::new(),
-                changed: false,
-                duration: CYCLE_DURATION,
-            }];
-        }
-        */
-
-        let mut cycles = Vec::new();
-
-        // Greedily partition turns into cycles. More clever things later.
-        let mut remaining_turns: Vec<TurnID> = map.get_turns_in_intersection(intersection)
-            .iter()
-            .map(|t| t.id)
-            .collect();
-        let mut current_cycle = Cycle {
-            turns: Vec::new(),
-            changed: false,
-            duration: CYCLE_DURATION,
-        };
-        while !remaining_turns.is_empty() {
-            let add_turn = remaining_turns
-                .iter()
-                .position(|&t| !current_cycle.conflicts_with(t, map));
-            match add_turn {
-                Some(idx) => {
-                    current_cycle.turns.push(remaining_turns[idx]);
-                    remaining_turns.remove(idx);
-                }
-                None => {
-                    cycles.push(current_cycle.clone());
-                    current_cycle.turns = Vec::new();
-                }
-            }
-        }
-        // TODO not sure this condition is needed
-        if !current_cycle.turns.is_empty() {
-            cycles.push(current_cycle.clone());
-        }
-
-        // TODO second pass to add all legal turns to the cycles we came up with initially
-
-        cycles
-    }
 }
 
 #[derive(Clone, Debug, Serialize)]
@@ -152,5 +102,70 @@ impl Cycle {
         let idx = self.turns.iter().position(|&id| id == t).unwrap();
         self.turns.remove(idx);
         self.changed = true;
+    }
+}
+
+fn greedy_assignment(map: &Map, intersection: IntersectionID) -> Vec<Cycle> {
+    /*
+    // TODO should be a tmp hack; intersections with no turns aren't even valid
+    if map.get_turns_in_intersection(intersection).is_empty() {
+        println!("WARNING: {} has no turns", intersection);
+        return vec![Cycle {
+            turns: Vec::new(),
+            changed: false,
+            duration: CYCLE_DURATION,
+        }];
+    }
+    */
+
+    let mut cycles = Vec::new();
+
+    // Greedily partition turns into cycles. More clever things later.
+    let mut remaining_turns: Vec<TurnID> = map.get_turns_in_intersection(intersection)
+        .iter()
+        .map(|t| t.id)
+        .collect();
+    let mut current_cycle = Cycle {
+        turns: Vec::new(),
+        changed: false,
+        duration: CYCLE_DURATION,
+    };
+    while !remaining_turns.is_empty() {
+        let add_turn = remaining_turns
+            .iter()
+            .position(|&t| !current_cycle.conflicts_with(t, map));
+        match add_turn {
+            Some(idx) => {
+                current_cycle.turns.push(remaining_turns[idx]);
+                remaining_turns.remove(idx);
+            }
+            None => {
+                cycles.push(current_cycle.clone());
+                current_cycle.turns = Vec::new();
+            }
+        }
+    }
+    // TODO not sure this condition is needed
+    if !current_cycle.turns.is_empty() {
+        cycles.push(current_cycle.clone());
+    }
+
+    expand_all_cycles(&mut cycles, map, intersection);
+
+    cycles
+}
+
+// Add all legal turns to existing cycles.
+fn expand_all_cycles(cycles: &mut Vec<Cycle>, map: &Map, intersection: IntersectionID) {
+    let all_turns: Vec<TurnID> = map.get_turns_in_intersection(intersection)
+        .iter()
+        .map(|t| t.id)
+        .collect();
+    for cycle in cycles.iter_mut() {
+        for t in &all_turns {
+            if !cycle.contains(*t) && !cycle.conflicts_with(*t, map) {
+                cycle.add(*t);
+            }
+        }
     }
 }
