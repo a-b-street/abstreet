f49998043d2a2859f44b705fa686ef9f36bd72dd
Dustin Carlino <dabreegster@gmail.com>
2018-09-24 15:52:52 -0700

    gluing together wizard for editing a scenario

--- editor/src/plugins/scenarios.rs
+++ editor/src/plugins/scenarios.rs
@@ -10,7 +10,8 @@ use wizard::{Wizard, WrappedWizard};
 pub enum ScenarioManager {
     Inactive,
     PickScenario(Wizard),
-    EditScenario(Scenario, LogScroller),
+    ManageScenario(Scenario, LogScroller),
+    EditScenario(Scenario, Wizard),
 }
 
 impl ScenarioManager {
@@ -29,25 +30,41 @@ impl ScenarioManager {
             ScenarioManager::PickScenario(ref mut wizard) => {
                 if let Some(scenario) = pick_scenario(wizard.wrap(input, map)) {
                     let scroller = LogScroller::new_from_lines(scenario.describe());
-                    new_state = Some(ScenarioManager::EditScenario(scenario, scroller));
+                    new_state = Some(ScenarioManager::ManageScenario(scenario, scroller));
                 } else if wizard.aborted() {
                     new_state = Some(ScenarioManager::Inactive);
                 }
             }
-            ScenarioManager::EditScenario(scenario, ref mut scroller) => {
+            ScenarioManager::ManageScenario(scenario, ref mut scroller) => {
                 // TODO Keys on top of the scroller? Weird...
                 // TODO Would use S for save, except sim controls always runs... maybe it shouldnt'
                 // do that after all.
                 if input.key_pressed(Key::Q, "save this scenario") {
-                    let path = format!("../data/scenarios/{}/{}", scenario.map_name, scenario.scenario_name);
-                    abstutil::write_json(
-                        &path,
-                        scenario).expect("Saving scenario failed");
+                    let path = format!(
+                        "../data/scenarios/{}/{}",
+                        scenario.map_name, scenario.scenario_name
+                    );
+                    abstutil::write_json(&path, scenario).expect("Saving scenario failed");
                     info!("Saved {}", path);
+                } else if input.key_pressed(Key::E, "edit this scenario") {
+                    new_state = Some(ScenarioManager::EditScenario(
+                        scenario.clone(),
+                        Wizard::new(),
+                    ));
                 } else if scroller.event(input) {
                     new_state = Some(ScenarioManager::Inactive);
                 }
-                // TODO edit it
+                // TODO instantiate it
+            }
+            ScenarioManager::EditScenario(ref mut scenario, ref mut wizard) => {
+                if let Some(()) = edit_scenario(scenario, wizard.wrap(input, map)) {
+                    let scroller = LogScroller::new_from_lines(scenario.describe());
+                    // TODO autosave, or at least make it clear there are unsaved edits
+                    new_state = Some(ScenarioManager::ManageScenario(scenario.clone(), scroller));
+                } else if wizard.aborted() {
+                    let scroller = LogScroller::new_from_lines(scenario.describe());
+                    new_state = Some(ScenarioManager::ManageScenario(scenario.clone(), scroller));
+                }
             }
         }
         if let Some(s) = new_state {
@@ -65,8 +82,11 @@ impl ScenarioManager {
             ScenarioManager::PickScenario(wizard) => {
                 wizard.draw(g, canvas);
             }
-            ScenarioManager::EditScenario(_, scroller) => {
+            ScenarioManager::ManageScenario(_, scroller) => {
                 scroller.draw(g, canvas);
+            }
+            ScenarioManager::EditScenario(_, wizard) => {
+                wizard.draw(g, canvas);
             }
         }
     }
@@ -77,7 +97,6 @@ impl Colorizer for ScenarioManager {}
 fn pick_scenario(mut wizard: WrappedWizard) -> Option<Scenario> {
     let load_existing = "Load existing scenario";
     let create_new = "Create new scenario";
-
     if wizard.choose("What scenario to edit?", vec![load_existing, create_new])? == load_existing {
         // TODO Constantly load them?! Urgh...
         let scenarios: Vec<(String, Scenario)> =
@@ -105,21 +124,26 @@ fn pick_scenario(mut wizard: WrappedWizard) -> Option<Scenario> {
     }
 }
 
-fn workflow(mut wizard: WrappedWizard) -> Option<SpawnOverTime> {
-    Some(SpawnOverTime {
-        num_agents: wizard.input_usize("Spawn how many agents?")?,
-        start_tick: wizard.input_tick("Start spawning when?")?,
-        // TODO input interval, or otherwise enforce stop_tick > start_tick
-        stop_tick: wizard.input_tick("Stop spawning when?")?,
-        percent_drive: wizard.input_percent("What percent should drive?")?,
-        start_from_neighborhood: wizard.choose_neighborhood("Where should the agents start?")?,
-        go_to_neighborhood: wizard.choose_neighborhood("Where should the agents go?")?,
-    })
-}
-
-fn workflow2(mut wizard: WrappedWizard) -> Option<SeedParkedCars> {
-    Some(SeedParkedCars {
-        neighborhood: wizard.choose_neighborhood("Seed parked cars in what area?")?,
-        percent_to_fill: wizard.input_percent("What percent of parking spots to populate?")?,
-    })
+fn edit_scenario(scenario: &mut Scenario, mut wizard: WrappedWizard) -> Option<()> {
+    let seed_parked = "Seed parked cars";
+    let spawn = "Spawn agents";
+    if wizard.choose("What kind of edit?", vec![seed_parked, spawn])? == seed_parked {
+        scenario.seed_parked_cars.push(SeedParkedCars {
+            neighborhood: wizard.choose_neighborhood("Seed parked cars in what area?")?,
+            percent_to_fill: wizard.input_percent("What percent of parking spots to populate?")?,
+        });
+        Some(())
+    } else {
+        scenario.spawn_over_time.push(SpawnOverTime {
+            num_agents: wizard.input_usize("Spawn how many agents?")?,
+            start_tick: wizard.input_tick("Start spawning when?")?,
+            // TODO input interval, or otherwise enforce stop_tick > start_tick
+            stop_tick: wizard.input_tick("Stop spawning when?")?,
+            percent_drive: wizard.input_percent("What percent should drive?")?,
+            start_from_neighborhood: wizard
+                .choose_neighborhood("Where should the agents start?")?,
+            go_to_neighborhood: wizard.choose_neighborhood("Where should the agents go?")?,
+        });
+        Some(())
+    }
 }
