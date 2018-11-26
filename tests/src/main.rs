cb4c5079a63f9a9d1cd6f3c77a042f3b4471083e
Dustin Carlino <dabreegster@gmail.com>
2018-11-26 14:46:27 -0800

    woops, forgot to hook up logging in tests

--- tests/src/main.rs
+++ tests/src/main.rs
@@ -3,6 +3,7 @@ extern crate convert_osm;
 extern crate dimensioned;
 extern crate gag;
 extern crate geom;
+extern crate log;
 extern crate map_model;
 extern crate sim;
 extern crate structopt;
@@ -18,7 +19,12 @@ mod transit;
 
 use structopt::StructOpt;
 
+static LOG_ADAPTER: abstutil::LogAdapter = abstutil::LogAdapter;
+
 fn main() {
+    log::set_max_level(log::LevelFilter::Debug);
+    log::set_logger(&LOG_ADAPTER).unwrap();
+
     let mut t = runner::TestRunner::new(runner::Flags::from_args());
 
     map_conversion::run(t.suite("map_conversion"));
