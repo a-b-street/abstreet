93dd6b1c174e22842828c40cf818010fe31a877c
Dustin Carlino <dabreegster@gmail.com>
2018-11-27 10:06:58 -0800

    fix an old path in the map_conversion test

--- tests/src/map_conversion.rs
+++ tests/src/map_conversion.rs
@@ -10,11 +10,11 @@ pub fn run(t: &mut TestRunner) {
                 osm: "../data/input/montlake.osm".to_string(),
                 elevation: "../data/input/N47W122.hgt".to_string(),
                 traffic_signals: "../data/input/TrafficSignals.shp".to_string(),
-                parcels: "../data/seattle_parcels.abst".to_string(),
+                parcels: "../data/shapes/parcels".to_string(),
                 parking_shapes: "../data/shapes/blockface".to_string(),
                 gtfs: "../data/input/google_transit_2018_18_08".to_string(),
                 neighborhoods: "../data/input/neighborhoods.geojson".to_string(),
-                output: "".to_string(),
+                output: "convert_twice".to_string(),
             };
 
             let map1 = convert_osm::convert(&flags, &mut abstutil::Timer::new("convert map"));
