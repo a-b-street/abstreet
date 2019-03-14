78fe1fcc821864e2d0a5b9da6f71186ecf509c65
Dustin Carlino <dabreegster@gmail.com>
2019-03-14 16:12:28 -0700

    why was tooltip_lines in a different place than everything else? consolidate

--- editor/src/objects.rs
+++ editor/src/objects.rs
@@ -1,12 +1,13 @@
 use crate::colors::ColorScheme;
 use crate::render::{DrawMap, ExtraShapeID};
-use ezgui::{EventLoopMode, Text};
+use ezgui::{Color, EventLoopMode, GfxCtx, Text};
 use geom::Pt2D;
+use map_model::raw_data::StableRoadID;
 use map_model::{
     AreaID, BuildingID, BusStopID, IntersectionID, LaneID, Map, ParcelID, RoadID, TurnID,
 };
 use sim::{AgentID, CarID, GetDrawAgents, PedestrianID, Sim, TripID};
-use std::collections::HashSet;
+use std::collections::{BTreeMap, HashSet};
 
 #[derive(Clone, Copy, Hash, PartialEq, Eq, Debug, PartialOrd, Ord)]
 pub enum ID {
@@ -89,6 +90,123 @@ impl ID {
         }
     }
 
+    pub fn tooltip_lines(&self, g: &mut GfxCtx, ctx: &DrawCtx) -> Text {
+        let (map, sim, draw_map) = (&ctx.map, &ctx.sim, &ctx.draw_map);
+        let mut txt = Text::new();
+        match *self {
+            ID::Road(id) => {
+                let r = map.get_r(id);
+                txt.add_line(format!("{} (originally {}) is ", r.id, r.stable_id));
+                txt.append(
+                    r.osm_tags
+                        .get("name")
+                        .unwrap_or(&"???".to_string())
+                        .to_string(),
+                    Some(Color::CYAN),
+                );
+                txt.add_line(format!("From OSM way {}", r.osm_way_id));
+            }
+            ID::Lane(id) => {
+                let l = map.get_l(id);
+                let r = map.get_r(l.parent);
+                let i1 = map.get_source_intersection(id);
+                let i2 = map.get_destination_intersection(id);
+
+                txt.add_line(format!("{} is ", l.id));
+                txt.append(
+                    r.osm_tags
+                        .get("name")
+                        .unwrap_or(&"???".to_string())
+                        .to_string(),
+                    Some(Color::CYAN),
+                );
+                txt.add_line(format!("From OSM way {}", r.osm_way_id));
+                txt.add_line(format!(
+                    "Parent {} (originally {}) points to {}",
+                    r.id, r.stable_id, r.dst_i
+                ));
+                txt.add_line(format!(
+                    "Lane goes from {} to {}",
+                    i1.elevation, i2.elevation
+                ));
+                txt.add_line(format!(
+                    "Lane is {} long, parent {} is {} long",
+                    l.length(),
+                    r.id,
+                    r.center_pts.length()
+                ));
+                styled_kv(&mut txt, &r.osm_tags);
+                if l.is_parking() {
+                    txt.add_line(format!("Has {} parking spots", l.number_parking_spots()));
+                }
+            }
+            ID::Intersection(id) => {
+                txt.add_line(id.to_string());
+                let i = map.get_i(id);
+                txt.add_line(format!("Roads: {:?}", i.roads));
+                txt.add_line(format!(
+                    "Orig roads: {:?}",
+                    i.roads
+                        .iter()
+                        .map(|r| map.get_r(*r).stable_id)
+                        .collect::<Vec<StableRoadID>>()
+                ));
+                txt.add_line(format!("Originally {}", i.stable_id));
+            }
+            ID::Turn(id) => {
+                let t = map.get_t(id);
+                txt.add_line(format!("{}", id));
+                txt.add_line(format!("{:?}", t.turn_type));
+            }
+            ID::Building(id) => {
+                let b = map.get_b(id);
+                txt.add_line(format!(
+                    "Building #{:?} (from OSM way {})",
+                    id, b.osm_way_id
+                ));
+                txt.add_line(format!(
+                    "Dist along sidewalk: {}",
+                    b.front_path.sidewalk.dist_along()
+                ));
+                if let Some(units) = b.num_residential_units {
+                    txt.add_line(format!("{} residential units", units));
+                }
+                styled_kv(&mut txt, &b.osm_tags);
+            }
+            ID::Car(id) => {
+                for line in sim.car_tooltip(id) {
+                    txt.add_wrapped_line(&g.canvas, line);
+                }
+            }
+            ID::Pedestrian(id) => {
+                for line in sim.ped_tooltip(id) {
+                    txt.add_wrapped_line(&g.canvas, line);
+                }
+            }
+            ID::ExtraShape(id) => {
+                styled_kv(&mut txt, &draw_map.get_es(id).attributes);
+            }
+            ID::Parcel(id) => {
+                txt.add_line(id.to_string());
+            }
+            ID::BusStop(id) => {
+                txt.add_line(id.to_string());
+                for r in map.get_all_bus_routes() {
+                    if r.stops.contains(&id) {
+                        txt.add_line(format!("- Route {}", r.name));
+                    }
+                }
+            }
+            ID::Area(id) => {
+                let a = map.get_a(id);
+                txt.add_line(format!("{} (from OSM {})", id, a.osm_id));
+                styled_kv(&mut txt, &a.osm_tags);
+            }
+            ID::Trip(_) => {}
+        };
+        txt
+    }
+
     pub fn canonical_point(&self, map: &Map, sim: &Sim, draw_map: &DrawMap) -> Option<Pt2D> {
         match *self {
             ID::Road(id) => map
@@ -126,4 +244,12 @@ pub struct DrawCtx<'a> {
     pub draw_map: &'a DrawMap,
     pub sim: &'a Sim,
     pub hints: &'a RenderingHints,
+}
+
+fn styled_kv(txt: &mut Text, tags: &BTreeMap<String, String>) {
+    for (k, v) in tags {
+        txt.add_styled_line(k.to_string(), Some(Color::RED), None);
+        txt.append(" = ".to_string(), None);
+        txt.append(v.to_string(), Some(Color::CYAN));
+    }
 }
--- editor/src/plugins/debug/debug_objects.rs
+++ editor/src/plugins/debug/debug_objects.rs
@@ -1,8 +1,6 @@
 use crate::objects::{DrawCtx, ID};
 use crate::plugins::{AmbientPlugin, PluginCtx};
-use ezgui::{Color, GfxCtx, Key, Text};
-use map_model::raw_data::StableRoadID;
-use std::collections::BTreeMap;
+use ezgui::{GfxCtx, Key, Text};
 
 pub struct DebugObjectsState {
     tooltip_key_held: bool,
@@ -50,7 +48,7 @@ impl AmbientPlugin for DebugObjectsState {
     fn draw(&self, g: &mut GfxCtx, ctx: &DrawCtx) {
         if self.tooltip_key_held {
             if let Some(id) = self.selected {
-                let txt = tooltip_lines(id, g, ctx);
+                let txt = id.tooltip_lines(g, ctx);
                 g.draw_mouse_tooltip(txt);
             }
         }
@@ -65,130 +63,5 @@ impl AmbientPlugin for DebugObjectsState {
                 }
             }
         }
-    }
-}
-
-fn tooltip_lines(obj: ID, g: &mut GfxCtx, ctx: &DrawCtx) -> Text {
-    let (map, sim, draw_map) = (&ctx.map, &ctx.sim, &ctx.draw_map);
-    let mut txt = Text::new();
-    match obj {
-        ID::Road(id) => {
-            let r = map.get_r(id);
-            txt.add_line(format!("{} (originally {}) is ", r.id, r.stable_id));
-            txt.append(
-                r.osm_tags
-                    .get("name")
-                    .unwrap_or(&"???".to_string())
-                    .to_string(),
-                Some(Color::CYAN),
-            );
-            txt.add_line(format!("From OSM way {}", r.osm_way_id));
-        }
-        ID::Lane(id) => {
-            let l = map.get_l(id);
-            let r = map.get_r(l.parent);
-            let i1 = map.get_source_intersection(id);
-            let i2 = map.get_destination_intersection(id);
-
-            txt.add_line(format!("{} is ", l.id));
-            txt.append(
-                r.osm_tags
-                    .get("name")
-                    .unwrap_or(&"???".to_string())
-                    .to_string(),
-                Some(Color::CYAN),
-            );
-            txt.add_line(format!("From OSM way {}", r.osm_way_id));
-            txt.add_line(format!(
-                "Parent {} (originally {}) points to {}",
-                r.id, r.stable_id, r.dst_i
-            ));
-            txt.add_line(format!(
-                "Lane goes from {} to {}",
-                i1.elevation, i2.elevation
-            ));
-            txt.add_line(format!(
-                "Lane is {} long, parent {} is {} long",
-                l.length(),
-                r.id,
-                r.center_pts.length()
-            ));
-            styled_kv(&mut txt, &r.osm_tags);
-            if l.is_parking() {
-                txt.add_line(format!("Has {} parking spots", l.number_parking_spots()));
-            }
-        }
-        ID::Intersection(id) => {
-            txt.add_line(id.to_string());
-            let i = map.get_i(id);
-            txt.add_line(format!("Roads: {:?}", i.roads));
-            txt.add_line(format!(
-                "Orig roads: {:?}",
-                i.roads
-                    .iter()
-                    .map(|r| map.get_r(*r).stable_id)
-                    .collect::<Vec<StableRoadID>>()
-            ));
-            txt.add_line(format!("Originally {}", i.stable_id));
-        }
-        ID::Turn(id) => {
-            let t = map.get_t(id);
-            txt.add_line(format!("{}", id));
-            txt.add_line(format!("{:?}", t.turn_type));
-        }
-        ID::Building(id) => {
-            let b = map.get_b(id);
-            txt.add_line(format!(
-                "Building #{:?} (from OSM way {})",
-                id, b.osm_way_id
-            ));
-            txt.add_line(format!(
-                "Dist along sidewalk: {}",
-                b.front_path.sidewalk.dist_along()
-            ));
-            if let Some(units) = b.num_residential_units {
-                txt.add_line(format!("{} residential units", units));
-            }
-            styled_kv(&mut txt, &b.osm_tags);
-        }
-        ID::Car(id) => {
-            for line in sim.car_tooltip(id) {
-                txt.add_wrapped_line(&g.canvas, line);
-            }
-        }
-        ID::Pedestrian(id) => {
-            for line in sim.ped_tooltip(id) {
-                txt.add_wrapped_line(&g.canvas, line);
-            }
-        }
-        ID::ExtraShape(id) => {
-            styled_kv(&mut txt, &draw_map.get_es(id).attributes);
-        }
-        ID::Parcel(id) => {
-            txt.add_line(id.to_string());
-        }
-        ID::BusStop(id) => {
-            txt.add_line(id.to_string());
-            for r in map.get_all_bus_routes() {
-                if r.stops.contains(&id) {
-                    txt.add_line(format!("- Route {}", r.name));
-                }
-            }
-        }
-        ID::Area(id) => {
-            let a = map.get_a(id);
-            txt.add_line(format!("{} (from OSM {})", id, a.osm_id));
-            styled_kv(&mut txt, &a.osm_tags);
-        }
-        ID::Trip(_) => {}
-    };
-    txt
-}
-
-fn styled_kv(txt: &mut Text, tags: &BTreeMap<String, String>) {
-    for (k, v) in tags {
-        txt.add_styled_line(k.to_string(), Some(Color::RED), None);
-        txt.append(" = ".to_string(), None);
-        txt.append(v.to_string(), Some(Color::CYAN));
     }
 }
