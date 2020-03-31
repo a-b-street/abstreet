9f670e564190f30e13ad918c831303dd656f6f36
Orestis Malaspinas <orestis.malaspinas@hesge.ch>
2020-04-01 00:54:55 +0200

    added pandemic stuff

--- game/src/common/overlays.rs
+++ game/src/common/overlays.rs
@@ -843,7 +843,7 @@ impl Overlays {
         if let Some(ref model) = maybe_pandemic {
             for a in app.primary.sim.get_unzoomed_agents(&app.primary.map) {
                 if let Some(p) = a.person {
-                    if model.infected.contains(&p) {
+                    if model.infected.contains_key(&p) {
                         pts.push(a.pos);
                     }
                 }
@@ -866,7 +866,7 @@ impl Overlays {
                 PersonState::Inside(b) => {
                     if maybe_pandemic
                         .as_ref()
-                        .map(|m| !m.infected.contains(&person.id))
+                        .map(|m| !m.infected.contains_key(&person.id))
                         .unwrap_or(false)
                     {
                         continue;
--- sim/src/pandemic.rs
+++ sim/src/pandemic.rs
@@ -5,27 +5,37 @@ use rand::Rng;
 use rand_xorshift::XorShiftRng;
 use std::collections::{BTreeMap, BTreeSet};
 
+const T_INF: f64 = 3600.0 * 24.0 * 5.0;
+const T_INC: f64 = 3600.0 * 24.0 * 5.0;
+const R_0: f64 = 2.5;
+
 pub struct PandemicModel {
     // TODO For the moment let's develop everything with the SEIR model and refactor
     pub sane: BTreeSet<PersonID>,
-    pub exposed: BTreeSet<PersonID>,
-    pub infected: BTreeSet<PersonID>,
+    pub exposed: BTreeMap<PersonID, Time>,
+    pub infected: BTreeMap<PersonID, Time>,
     pub recovered: BTreeSet<PersonID>,
     // Since when has a person been inside a building?
     // TODO This is an awkward data structure; abstutil::MultiMap is also bad, because key removal
     // would require knowing the time. Want something closer to
     // https://guava.dev/releases/19.0/api/docs/com/google/common/collect/Table.html.
     bldg_occupants: BTreeMap<BuildingID, Vec<(PersonID, Time)>>,
+    t_inf: f64,
+    t_inc: f64, 
+    r0: f64,
 }
 
 impl PandemicModel {
     fn new() -> Self {
         PandemicModel {
             sane: BTreeSet::new(),
-            exposed: BTreeSet::new(),
-            infected: BTreeSet::new(),
+            exposed: BTreeMap::new(),
+            infected: BTreeMap::new(),
             recovered: BTreeSet::new(),
             bldg_occupants: BTreeMap::new(),
+            t_inf: T_INF,
+            t_inc: T_INC,
+            r0: R_0,
         }
     }
 
@@ -45,6 +55,8 @@ impl PandemicModel {
             if *time > now {
                 break;
             }
+
+
             if *left { // person left building let's (let's see its contacts)
                 // TODO Messy to mutate state inside a retain closure
                 let mut inside_since: Option<Time> = None;
@@ -72,15 +84,43 @@ impl PandemicModel {
                     //let time_in_bldg = time - inside_since.unwrap();
                     let mut longest_overlap_with_infected = Duration::ZERO;
                     for (p, t) in &state.bldg_occupants[bldg] {
-                        if !state.infected.contains(p) {
+                        if !state.infected.contains_key(p) {
                             continue;
                         }
                         // How much time was p inside the building with person?
                         let dt = *time - (*t).max(inside_since);
                         longest_overlap_with_infected = longest_overlap_with_infected.max(dt);
                     }
-                    if longest_overlap_with_infected > Duration::hours(1) && rng.gen_bool(0.1) {
-                        state.infected.insert(*person);
+                    if rng.gen_bool(1.0 - (- longest_overlap_with_infected.inner_seconds() * state.r0 / state.t_inf).exp()) {
+                        state.exposed.insert(*person, *time);
+                    }
+                }
+
+                // Not perfect because we are only considering people leaving building
+                // this should be performed by listening to any event actually (let's see how to get that)
+                if let Some(t0) = state.infected.get(person) {
+                    let dt = now - *t0;
+                    let m = dt.inner_seconds() - state.t_inf;
+                    let n = state.t_inf / 4.0;
+
+                    let prob = libm::erf(0.5 * m * f64::sqrt(2.0)/n.sqrt()) - libm::erf(0.5 * f64::sqrt(2.0) * (-dt.inner_seconds() + m) / n.sqrt());
+
+                    if rng.gen_bool(prob) {
+                        state.recovered.insert(*person);
+                    }
+                }
+
+                // Not perfect because we are only considering people leaving building
+                // this should be performed by listening to any event actually (let's see how to get that)
+                if let Some(t0) = state.exposed.get(person) {
+                    let dt = now - *t0;
+                    let m = dt.inner_seconds() - state.t_inc;
+                    let n = state.t_inf / 4.0;
+
+                    let prob = libm::erf(0.5 * m * f64::sqrt(2.0)/n.sqrt()) - libm::erf(0.5 * f64::sqrt(2.0) * (-dt.inner_seconds() + m) / n.sqrt());
+
+                    if rng.gen_bool(prob) {
+                        state.recovered.insert(*person);
                     }
                 }
             } else {
@@ -93,7 +133,7 @@ impl PandemicModel {
                 // Bit of a hack to seed initial state per person here, but eh
                 if *time == Time::START_OF_DAY {
                     if rng.gen_bool(0.1) {
-                        state.infected.insert(*person);
+                        state.exposed.insert(*person, *time);
                     }
                 }
             }
