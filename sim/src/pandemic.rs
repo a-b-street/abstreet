06f7b9dfa9822be649c29fa16b026b1b7ff4d66b
Orestis Malaspinas <orestis.malaspinas@hesge.ch>
2020-04-01 18:31:43 +0200

    updated value

--- sim/src/pandemic.rs
+++ sim/src/pandemic.rs
@@ -5,7 +5,7 @@ use rand::Rng;
 use rand_xorshift::XorShiftRng;
 use std::collections::{BTreeMap, BTreeSet};
 
-const T_INF: f64 = 3600.0 * 24.0 * 7.0; // TODO dummy values
+const T_INF: f64 = 3600.0 * 10.0; // TODO dummy values
 const T_INC: f64 = 3600.0; // TODO dummy values
 const R_0: f64 = 2.5;
 const I_RATIO: f64 = 0.01;
