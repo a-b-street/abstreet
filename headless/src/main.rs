d586a9054bee4001f2fff0c1b1e4d86fd63709da
Dustin Carlino <dabreegster@gmail.com>
2020-04-15 12:48:45 -0700

    rewrite the headless tool for pandemic stuff

--- headless/src/main.rs
+++ headless/src/main.rs
@@ -1,71 +1,50 @@
-use abstutil::{CmdArgs, Timer};
-use geom::Time;
-use sim::{GetDrawAgents, ScenarioGenerator, SimFlags};
+use abstutil::{prettyprint_usize, CmdArgs, Timer};
+use map_model::Map;
+use rand::SeedableRng;
+use rand_xorshift::XorShiftRng;
+use sim::{Scenario, Sim, SimFlags};
+
+// This is specialized to experiment with running the pandemic model over long time periods.
+// Original functionality for profiling and debugging gridlock have been removed.
 
 fn main() {
     let mut args = CmdArgs::new();
-    let sim_flags = SimFlags::from_args(&mut args);
-    let save_at = args.optional_parse("--save_at", Time::parse);
-    let num_agents = args.optional_parse("--num_agents", |s| s.parse::<usize>());
-    let enable_profiler = args.enabled("--enable_profiler");
-    // Every 0.1s, pretend to draw everything to make sure there are no bugs.
-    let paranoia = args.enabled("--paranoia");
+    let num_days = args
+        .optional_parse("--days", |s| s.parse::<usize>())
+        .unwrap_or(1);
     args.done();
 
+    let mut sim_flags = SimFlags::synthetic_test("montlake", "pandemic");
+    sim_flags.opts.enable_pandemic_model = Some(XorShiftRng::from_seed([42; 16]));
     let mut timer = Timer::new("setup headless");
     let (map, mut sim, mut rng) = sim_flags.load(&mut timer);
 
-    // TODO not the ideal way to distinguish what thing we loaded
-    if sim_flags.load.starts_with(&abstutil::path_all_raw_maps())
-        || sim_flags.load.starts_with(&abstutil::path_all_maps())
-    {
-        let s = if let Some(n) = num_agents {
-            ScenarioGenerator::scaled_run(n)
-        } else {
-            ScenarioGenerator::small_run(&map)
-        }
-        .generate(&map, &mut rng, &mut timer);
-        s.instantiate(&mut sim, &map, &mut rng, &mut timer);
-    }
+    let base_scenario: Scenario = abstutil::read_binary(
+        abstutil::path_scenario(map.get_name(), "weekday"),
+        &mut timer,
+    );
+    base_scenario
+        .repeat_days(num_days)
+        .instantiate(&mut sim, &map, &mut rng, &mut timer);
     timer.done();
 
-    if enable_profiler {
-        #[cfg(feature = "profiler")]
-        {
-            cpuprofiler::PROFILER
-                .lock()
-                .unwrap()
-                .start("./profile")
-                .unwrap();
-        }
-    }
+    run_experiment(&map, &mut sim);
+}
+
+fn run_experiment(map: &Map, sim: &mut Sim) {
     let timer = Timer::new("run sim until done");
     sim.run_until_done(
         &map,
-        move |sim, map| {
-            // TODO We want to savestate at the end of this time; this'll happen at the beginning.
-            if Some(sim.time()) == save_at {
-                sim.save();
-                // Some simulations run for a really long time, just do this.
-                if enable_profiler {
-                    #[cfg(feature = "profiler")]
-                    {
-                        cpuprofiler::PROFILER.lock().unwrap().stop().unwrap();
-                    }
-                }
-            }
-            if paranoia {
-                sim.get_all_draw_cars(map);
-            }
+        |sim, _map| {
+            // This'll run every 30 sim seconds
+            println!(
+                "At {}, {} infected",
+                sim.time(),
+                prettyprint_usize(sim.get_pandemic_model().unwrap().count_infected())
+            );
         },
         None,
     );
     timer.done();
     println!("Done at {}", sim.time());
-    if enable_profiler && save_at.is_none() {
-        #[cfg(feature = "profiler")]
-        {
-            cpuprofiler::PROFILER.lock().unwrap().stop().unwrap();
-        }
-    }
 }
