a-b-street/abstreet

commit b267997554b105672006174f23e720bf48a02ad7

add debugging for #65 and try setting the initial window size differently [rebuild]

ezgui/src/backend_glium.rs
@@ -71,7 +71,16 @@ pub fn setup(
     // TODO Should this be display.gl_window().window().inner_size()? I think some resize events
     // always happen soon after startup, so it probably doesn't matter much. Changing this on Linux
     // messes up the loading screen size (which happens before resize events get processed).
-    let window_size = event_loop.primary_monitor().size();
+    //
+    // But actually on Windows, maybe there isn't that initial spurt of resize events.
+    let window_size = display.gl_window().window().inner_size();
+    let scale_factor = display.gl_window().window().scale_factor();
+    println!(
+        "Initial inner window size is {:?}, monitor is {:?}, scale factor is {}",
+        window_size,
+        event_loop.primary_monitor().size(),
+        scale_factor
+    );
     (
         PrerenderInnards {
             display,

ezgui/src/backend_glow.rs
@@ -72,6 +72,7 @@ pub fn setup(
         );
     }
 
+    // TODO Once this is fixed in glium, do the same thing here
     let window_size = event_loop.primary_monitor().size();
     (
         PrerenderInnards {

game/src/options.rs
@@ -263,7 +263,8 @@ impl State for OptionsPanel {
         Transition::Keep
     }
 
-    fn draw(&self, g: &mut GfxCtx, _: &App) {
+    fn draw(&self, g: &mut GfxCtx, app: &App) {
+        State::grey_out_map(g, app);
         self.composite.draw(g);
     }
 }

commit 8f75e10ca715545e43adeb2335181ece6750e4a3

revert back to using primary monitor size on linux only. fallout from b267997554b105672006174f23e720bf48a02ad7 was that sometimes on linux when using --dev to jump directly into sandbox mode, the minimap and some panels are very screwed up, because they see incorrect window dims before the resize event. until all ezgui things properly handle resizing, do this

ezgui/src/backend_glium.rs
@@ -68,11 +68,17 @@ pub fn setup(
     )
     .unwrap();
 
-    let window_size = display.gl_window().window().inner_size();
+    let inner_window = display.gl_window().window().inner_size();
+    let monitor = event_loop.primary_monitor().size();
+    let initial_size = if cfg!(target_os = "linux") {
+        monitor
+    } else {
+        inner_window
+    };
     println!(
-        "Initial inner window size is {:?}, monitor is {:?}, scale factor is {}",
-        window_size,
-        event_loop.primary_monitor().size(),
+        "Inner window size is {:?}, monitor is {:?}, scale factor is {}",
+        inner_window,
+        monitor,
         display.gl_window().window().scale_factor()
     );
     (
@@ -82,7 +88,7 @@ pub fn setup(
             total_bytes_uploaded: Cell::new(0),
         },
         event_loop,
-        ScreenDims::new(window_size.width.into(), window_size.height.into()),
+        ScreenDims::new(initial_size.width.into(), initial_size.height.into()),
     )
 }
 

ezgui/src/backend_glow.rs
@@ -72,14 +72,19 @@ pub fn setup(
         );
     }
 
-    let window_size = windowed_context.window().inner_size();
+    let inner_window = windowed_context.window().inner_size();
+    let monitor = event_loop.primary_monitor().size();
+    let initial_size = if cfg!(target_os = "linux") {
+        monitor
+    } else {
+        inner_window
+    };
     println!(
-        "Initial inner window size is {:?}, monitor is {:?}, scale factor is {}",
-        window_size,
-        event_loop.primary_monitor().size(),
+        "Inner window size is {:?}, monitor is {:?}, scale factor is {}",
+        inner_window,
+        monitor,
         windowed_context.window().scale_factor()
     );
-
     (
         PrerenderInnards {
             gl,
@@ -88,7 +93,7 @@ pub fn setup(
             total_bytes_uploaded: Cell::new(0),
         },
         event_loop,
-        ScreenDims::new(window_size.width.into(), window_size.height.into()),
+        ScreenDims::new(initial_size.width.into(), initial_size.height.into()),
     )
 }
 