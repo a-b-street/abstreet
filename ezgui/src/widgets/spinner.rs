a-b-street/abstreet

commit 72d017130043891f303badab9adb17c70268e2be

fix #111 by delaying constructing ControlTrafficSignal from edits. also fix time warp screen issue after the UpdateType changes [rebuild]

game/src/edit/traffic_signals.rs
@@ -488,7 +488,7 @@ pub fn change_traffic_signal(signal: ControlTrafficSignal, ctx: &mut EventCtx, a
     edits.commands.push(EditCmd::ChangeIntersection {
         i: signal.id,
         old,
-        new: EditIntersection::TrafficSignal(signal),
+        new: EditIntersection::TrafficSignal(signal.export(&app.primary.map)),
     });
     apply_map_edits(ctx, app, edits);
 }

map_model/src/edits.rs
@@ -1,7 +1,5 @@
 use crate::raw::{OriginalIntersection, OriginalRoad};
-use crate::{
-    ControlStopSign, ControlTrafficSignal, IntersectionID, LaneID, LaneType, Map, RoadID, TurnID,
-};
+use crate::{ControlStopSign, IntersectionID, LaneID, LaneType, Map, RoadID, TurnID};
 use abstutil::{deserialize_btreemap, retain_btreemap, retain_btreeset, serialize_btreemap, Timer};
 use geom::Speed;
 use serde::{Deserialize, Serialize};
@@ -27,7 +25,9 @@ pub struct MapEdits {
 #[derive(Debug, Clone, PartialEq)]
 pub enum EditIntersection {
     StopSign(ControlStopSign),
-    TrafficSignal(ControlTrafficSignal),
+    // Don't keep ControlTrafficSignal here, because it contains turn groups that should be
+    // generated after all lane edits are applied.
+    TrafficSignal(seattle_traffic_signals::TrafficSignal),
     Closed,
 }
 
@@ -373,8 +373,8 @@ impl EditIntersection {
                     .map(|(r, val)| (map.get_r(*r).orig_id, val.must_stop))
                     .collect(),
             },
-            EditIntersection::TrafficSignal(ref ts) => {
-                PermanentEditIntersection::TrafficSignal(ts.export(map))
+            EditIntersection::TrafficSignal(ref raw_ts) => {
+                PermanentEditIntersection::TrafficSignal(raw_ts.clone())
             }
             EditIntersection::Closed => PermanentEditIntersection::Closed,
         }
@@ -405,9 +405,9 @@ impl PermanentEditIntersection {
 
                 Some(EditIntersection::StopSign(ss))
             }
-            PermanentEditIntersection::TrafficSignal(ts) => Some(EditIntersection::TrafficSignal(
-                ControlTrafficSignal::import(ts, i, map)?,
-            )),
+            PermanentEditIntersection::TrafficSignal(ts) => {
+                Some(EditIntersection::TrafficSignal(ts))
+            }
             PermanentEditIntersection::Closed => Some(EditIntersection::Closed),
         }
     }

map_model/src/traffic_signals.rs
@@ -183,13 +183,16 @@ impl Phase {
 
     pub fn get_priority_of_turn(&self, t: TurnID, parent: &ControlTrafficSignal) -> TurnPriority {
         // TODO Cache this?
-        let g = parent
+        if let Some(g) = parent
             .turn_groups
             .values()
             .find(|g| g.members.contains(&t))
             .map(|g| g.id)
-            .unwrap();
-        self.get_priority_of_group(g)
+        {
+            self.get_priority_of_group(g)
+        } else {
+            panic!("{} doesn't belong to any turn groups", t);
+        }
     }
 
     pub fn get_priority_of_group(&self, g: TurnGroupID) -> TurnPriority {

game/src/edit/stop_signs.rs
@@ -150,11 +150,14 @@ impl State for StopSignEditor {
                     edits.commands.push(EditCmd::ChangeIntersection {
                         i: self.id,
                         old: app.primary.map.get_i_edit(self.id),
-                        new: EditIntersection::TrafficSignal(ControlTrafficSignal::new(
-                            &app.primary.map,
-                            self.id,
-                            &mut Timer::throwaway(),
-                        )),
+                        new: EditIntersection::TrafficSignal(
+                            ControlTrafficSignal::new(
+                                &app.primary.map,
+                                self.id,
+                                &mut Timer::throwaway(),
+                            )
+                            .export(&app.primary.map),
+                        ),
                     });
                     apply_map_edits(ctx, app, edits);
                     return Transition::Replace(Box::new(TrafficSignalEditor::new(

game/src/sandbox/speed.rs
@@ -483,6 +483,7 @@ impl TimeWarpScreen {
         target: Time,
         mut traffic_jams: bool,
     ) -> Box<dyn State> {
+        ctx.request_update(UpdateType::Game);
         if traffic_jams {
             if app.primary.sim_cb.is_none() {
                 app.primary.sim_cb = Some(Box::new(FindDelayedIntersections {

map_model/src/map.rs
@@ -703,7 +703,7 @@ impl Map {
                     return Some(tg.id);
                 }
             }
-            unreachable!()
+            panic!("{} doesn't belong to any turn groups", t);
         }
         None
     }
@@ -795,7 +795,7 @@ impl Map {
         match self.get_i(i).intersection_type {
             IntersectionType::StopSign => EditIntersection::StopSign(self.get_stop_sign(i).clone()),
             IntersectionType::TrafficSignal => {
-                EditIntersection::TrafficSignal(self.get_traffic_signal(i).clone())
+                EditIntersection::TrafficSignal(self.get_traffic_signal(i).export(self))
             }
             IntersectionType::Construction => EditIntersection::Closed,
             IntersectionType::Border => unreachable!(),
@@ -1276,9 +1276,12 @@ impl EditCmd {
                         map.intersections[i.0].intersection_type = IntersectionType::StopSign;
                         map.stop_signs.insert(*i, ss.clone());
                     }
-                    EditIntersection::TrafficSignal(ref ts) => {
+                    EditIntersection::TrafficSignal(ref raw_ts) => {
                         map.intersections[i.0].intersection_type = IntersectionType::TrafficSignal;
-                        map.traffic_signals.insert(*i, ts.clone());
+                        map.traffic_signals.insert(
+                            *i,
+                            ControlTrafficSignal::import(raw_ts.clone(), *i, map).unwrap(),
+                        );
                     }
                     EditIntersection::Closed => {
                         map.intersections[i.0].intersection_type = IntersectionType::Construction;

commit 181de1240982b1260075e19e1571b7cc505b7900

woops, 72d017130043891f303badab9adb17c70268e2be broke signal editor by crashing if you were missing a turn. dont validate in the middle of editing. still seeing a problem with traffic signal -> construction

ezgui/src/widgets/spinner.rs
@@ -24,7 +24,7 @@ pub struct Spinner {
 
 impl Spinner {
     pub fn new(ctx: &EventCtx, (low, high): (isize, isize), current: isize) -> Widget {
-        let up = Btn::text_fg("▲")
+        let up = Btn::text_fg("↑")
             .build(ctx, "increase value", None)
             .take_btn();
         let down = Btn::text_fg("↓")

game/src/edit/traffic_signals.rs
@@ -91,7 +91,12 @@ impl State for TrafficSignalEditor {
         match self.composite.event(ctx) {
             Some(Outcome::Clicked(x)) => match x {
                 x if x == "Edit entire signal" => {
-                    return Transition::Push(edit_entire_signal(app, self.i, self.mode.clone()));
+                    return Transition::Push(edit_entire_signal(
+                        app,
+                        self.i,
+                        self.mode.clone(),
+                        self.command_stack.get(0).cloned(),
+                    ));
                 }
                 x if x.starts_with("change duration of phase ") => {
                     let idx = x["change duration of phase ".len()..]
@@ -109,7 +114,7 @@ impl State for TrafficSignalEditor {
                     self.command_stack.push(orig_signal.clone());
                     self.redo_stack.clear();
                     self.top_panel = make_top_panel(ctx, app, true, false);
-                    change_traffic_signal(new_signal, ctx, app);
+                    app.primary.map.incremental_edit_traffic_signal(new_signal);
                     // Don't use change_phase; it tries to preserve scroll
                     self.current_phase = if idx == num_phases { idx - 1 } else { idx };
                     self.composite =
@@ -124,7 +129,7 @@ impl State for TrafficSignalEditor {
                     self.command_stack.push(orig_signal.clone());
                     self.redo_stack.clear();
                     self.top_panel = make_top_panel(ctx, app, true, false);
-                    change_traffic_signal(new_signal, ctx, app);
+                    app.primary.map.incremental_edit_traffic_signal(new_signal);
                     self.change_phase(idx - 1, ctx, app);
                     return Transition::Keep;
                 }
@@ -136,7 +141,7 @@ impl State for TrafficSignalEditor {
                     self.command_stack.push(orig_signal.clone());
                     self.redo_stack.clear();
                     self.top_panel = make_top_panel(ctx, app, true, false);
-                    change_traffic_signal(new_signal, ctx, app);
+                    app.primary.map.incremental_edit_traffic_signal(new_signal);
                     self.change_phase(idx + 1, ctx, app);
                     return Transition::Keep;
                 }
@@ -147,7 +152,7 @@ impl State for TrafficSignalEditor {
                     self.command_stack.push(orig_signal.clone());
                     self.redo_stack.clear();
                     self.top_panel = make_top_panel(ctx, app, true, false);
-                    change_traffic_signal(new_signal, ctx, app);
+                    app.primary.map.incremental_edit_traffic_signal(new_signal);
                     self.change_phase(len - 1, ctx, app);
                     return Transition::Keep;
                 }
@@ -208,7 +213,7 @@ impl State for TrafficSignalEditor {
                     self.command_stack.push(orig_signal.clone());
                     self.redo_stack.clear();
                     self.top_panel = make_top_panel(ctx, app, true, false);
-                    change_traffic_signal(new_signal, ctx, app);
+                    app.primary.map.incremental_edit_traffic_signal(new_signal);
                     self.change_phase(self.current_phase, ctx, app);
                     return Transition::KeepWithMouseover;
                 }
@@ -218,12 +223,12 @@ impl State for TrafficSignalEditor {
         match self.top_panel.event(ctx) {
             Some(Outcome::Clicked(x)) => match x.as_ref() {
                 "Finish" => {
-                    return check_for_missing_groups(
-                        orig_signal.clone(),
-                        &mut self.composite,
-                        app,
-                        ctx,
-                    );
+                    if let Some(orig) = self.command_stack.get(0) {
+                        return check_for_missing_groups(ctx, app, &mut self.composite, orig);
+                    } else {
+                        // No changes
+                        return Transition::Pop;
+                    }
                 }
                 "Export" => {
                     let ts = orig_signal.export(&app.primary.map);
@@ -245,14 +250,18 @@ impl State for TrafficSignalEditor {
                 }
                 "undo" => {
                     self.redo_stack.push(orig_signal.clone());
-                    change_traffic_signal(self.command_stack.pop().unwrap(), ctx, app);
+                    app.primary
+                        .map
+                        .incremental_edit_traffic_signal(self.command_stack.pop().unwrap());
                     self.top_panel = make_top_panel(ctx, app, !self.command_stack.is_empty(), true);
                     self.change_phase(0, ctx, app);
                     return Transition::Keep;
                 }
                 "redo" => {
                     self.command_stack.push(orig_signal.clone());
-                    change_traffic_signal(self.redo_stack.pop().unwrap(), ctx, app);
+                    app.primary
+                        .map
+                        .incremental_edit_traffic_signal(self.redo_stack.pop().unwrap());
                     self.top_panel = make_top_panel(ctx, app, true, !self.redo_stack.is_empty());
                     self.change_phase(0, ctx, app);
                     return Transition::Keep;
@@ -459,41 +468,12 @@ pub fn make_top_panel(ctx: &mut EventCtx, app: &App, can_undo: bool, can_redo: b
         .build(ctx)
 }
 
-pub fn change_traffic_signal(signal: ControlTrafficSignal, ctx: &mut EventCtx, app: &mut App) {
-    let mut edits = app.primary.map.get_edits().clone();
-    // TODO Only record one command for the entire session. Otherwise, we can exit this editor and
-    // undo a few times, potentially ending at an invalid state!
-    let old = if let Some(prev) = edits.commands.last().and_then(|cmd| match cmd {
-        EditCmd::ChangeIntersection {
-            i,
-            ref new,
-            ref old,
-        } => {
-            if signal.id == *i {
-                match new {
-                    EditIntersection::TrafficSignal(_) => Some(old.clone()),
-                    _ => None,
-                }
-            } else {
-                None
-            }
-        }
-        _ => None,
-    }) {
-        edits.commands.pop();
-        prev
-    } else {
-        app.primary.map.get_i_edit(signal.id)
-    };
-    edits.commands.push(EditCmd::ChangeIntersection {
-        i: signal.id,
-        old,
-        new: EditIntersection::TrafficSignal(signal.export(&app.primary.map)),
-    });
-    apply_map_edits(ctx, app, edits);
-}
-
-fn edit_entire_signal(app: &App, i: IntersectionID, mode: GameplayMode) -> Box<dyn State> {
+fn edit_entire_signal(
+    app: &App,
+    i: IntersectionID,
+    mode: GameplayMode,
+    orig_signal: Option<ControlTrafficSignal>,
+) -> Box<dyn State> {
     let has_sidewalks = app
         .primary
         .map
@@ -539,7 +519,7 @@ fn edit_entire_signal(app: &App, i: IntersectionID, mode: GameplayMode) -> Box<d
                         .push(app.primary.map.get_traffic_signal(editor.i).clone());
                     editor.redo_stack.clear();
                     editor.top_panel = make_top_panel(ctx, app, true, false);
-                    change_traffic_signal(new_signal, ctx, app);
+                    app.primary.map.incremental_edit_traffic_signal(new_signal);
                     editor.change_phase(0, ctx, app);
                 })))
             }
@@ -552,12 +532,19 @@ fn edit_entire_signal(app: &App, i: IntersectionID, mode: GameplayMode) -> Box<d
                         editor.command_stack.push(orig_signal.clone());
                         editor.redo_stack.clear();
                         editor.top_panel = make_top_panel(ctx, app, true, false);
-                        change_traffic_signal(new_signal, ctx, app);
+                        app.primary.map.incremental_edit_traffic_signal(new_signal);
                         editor.change_phase(0, ctx, app);
                     }
                 })))
             }
             x if x == stop_sign => {
+                // First restore the original signal
+                if let Some(ref orig) = orig_signal {
+                    app.primary
+                        .map
+                        .incremental_edit_traffic_signal(orig.clone());
+                }
+
                 let mut edits = app.primary.map.get_edits().clone();
                 edits.commands.push(EditCmd::ChangeIntersection {
                     i,
@@ -572,7 +559,16 @@ fn edit_entire_signal(app: &App, i: IntersectionID, mode: GameplayMode) -> Box<d
                     mode.clone(),
                 ))))
             }
-            x if x == close => Some(close_intersection(ctx, app, i, false)),
+            x if x == close => {
+                // First restore the original signal
+                if let Some(ref orig) = orig_signal {
+                    app.primary
+                        .map
+                        .incremental_edit_traffic_signal(orig.clone());
+                }
+
+                Some(close_intersection(ctx, app, i, false))
+            }
             x if x == offset => {
                 let new_duration = wizard.input_usize_prefilled(
                     "What should the offset of this traffic signal be (seconds)?",
@@ -585,7 +581,7 @@ fn edit_entire_signal(app: &App, i: IntersectionID, mode: GameplayMode) -> Box<d
                     editor.redo_stack.clear();
                     editor.top_panel = make_top_panel(ctx, app, true, false);
                     signal.offset = Duration::seconds(new_duration as f64);
-                    change_traffic_signal(signal, ctx, app);
+                    app.primary.map.incremental_edit_traffic_signal(signal);
                     editor.change_phase(editor.current_phase, ctx, app);
                 })))
             }
@@ -603,7 +599,7 @@ fn edit_entire_signal(app: &App, i: IntersectionID, mode: GameplayMode) -> Box<d
                     editor.command_stack.push(orig_signal.clone());
                     editor.redo_stack.clear();
                     editor.top_panel = make_top_panel(ctx, app, true, false);
-                    change_traffic_signal(new_signal, ctx, app);
+                    app.primary.map.incremental_edit_traffic_signal(new_signal);
                     // Don't use change_phase; it tries to preserve scroll
                     editor.current_phase = 0;
                     editor.composite =
@@ -657,20 +653,22 @@ fn change_duration(app: &App, i: IntersectionID, idx: usize) -> Box<dyn State> {
             editor.command_stack.push(orig_signal.clone());
             editor.redo_stack.clear();
             editor.top_panel = make_top_panel(ctx, app, true, false);
-            change_traffic_signal(new_signal, ctx, app);
+            app.primary.map.incremental_edit_traffic_signal(new_signal);
             editor.change_phase(idx, ctx, app);
         })))
     }))
 }
 
 fn check_for_missing_groups(
-    mut signal: ControlTrafficSignal,
-    composite: &mut Composite,
-    app: &mut App,
     ctx: &mut EventCtx,
+    app: &mut App,
+    composite: &mut Composite,
+    orig_signal: &ControlTrafficSignal,
 ) -> Transition {
-    let mut missing: BTreeSet<TurnGroupID> = signal.turn_groups.keys().cloned().collect();
-    for phase in &signal.phases {
+    let mut new_signal = app.primary.map.get_traffic_signal(orig_signal.id).clone();
+
+    let mut missing: BTreeSet<TurnGroupID> = new_signal.turn_groups.keys().cloned().collect();
+    for phase in &new_signal.phases {
         for g in &phase.protected_groups {
             missing.remove(g);
         }
@@ -679,9 +677,26 @@ fn check_for_missing_groups(
         }
     }
     if missing.is_empty() {
-        let i = signal.id;
-        if let Err(err) = signal.validate() {
-            panic!("Edited traffic signal {} finalized with errors: {}", i, err);
+        match new_signal.validate() {
+            Ok(new_signal) => {
+                app.primary
+                    .map
+                    .incremental_edit_traffic_signal(orig_signal.clone());
+
+                let mut edits = app.primary.map.get_edits().clone();
+                edits.commands.push(EditCmd::ChangeIntersection {
+                    i: new_signal.id,
+                    old: app.primary.map.get_i_edit(new_signal.id),
+                    new: EditIntersection::TrafficSignal(new_signal.export(&app.primary.map)),
+                });
+                apply_map_edits(ctx, app, edits);
+            }
+            Err(err) => {
+                panic!(
+                    "Edited traffic signal {} finalized with errors: {}",
+                    orig_signal.id, err
+                );
+            }
         }
         return Transition::Pop;
     }
@@ -694,9 +709,9 @@ fn check_for_missing_groups(
             phase.yield_groups.insert(g);
         }
     }
-    signal.phases.insert(0, phase);
-    let id = signal.id;
-    change_traffic_signal(signal, ctx, app);
+    new_signal.phases.insert(0, phase);
+    let id = new_signal.id;
+    app.primary.map.incremental_edit_traffic_signal(new_signal);
     *composite = make_signal_diagram(ctx, app, id, 0, true);
 
     Transition::Push(msg(

game/src/info/person.rs
@@ -157,7 +157,7 @@ pub fn trips(
                 Btn::plaintext(if open_trips.contains_key(t) {
                     "↓"
                 } else {
-                    "▲"
+                    "↑"
                 })
                 .build(
                     ctx,

map_model/src/edits.rs
@@ -861,4 +861,14 @@ impl Map {
 
         self.pathfinder_dirty = false;
     }
+
+    // Since the player is in the middle of editing, the signal may not be valid. Don't go through
+    // the entire apply_edits flow.
+    pub fn incremental_edit_traffic_signal(&mut self, signal: ControlTrafficSignal) {
+        assert_eq!(
+            self.get_i(signal.id).intersection_type,
+            IntersectionType::TrafficSignal
+        );
+        self.traffic_signals.insert(signal.id, signal);
+    }
 }

map_model/src/make/traffic_signals.rs
@@ -19,7 +19,7 @@ pub fn get_possible_policies(
         .unwrap()
         .remove(&map.get_i(id).orig_id.osm_node_id)
     {
-        if let Some(ts) = ControlTrafficSignal::import(raw, id, map) {
+        if let Ok(ts) = ControlTrafficSignal::import(raw, id, map) {
             results.push(("hand-mapped current real settings".to_string(), ts));
         } else {
             timer.error(format!(

map_model/src/objects/traffic_signals.rs
@@ -262,7 +262,7 @@ impl ControlTrafficSignal {
         raw: seattle_traffic_signals::TrafficSignal,
         id: IntersectionID,
         map: &Map,
-    ) -> Option<ControlTrafficSignal> {
+    ) -> Result<ControlTrafficSignal, String> {
         let mut phases = Vec::new();
         for p in raw.phases {
             let num_protected = p.protected_turns.len();
@@ -291,7 +291,10 @@ impl ControlTrafficSignal {
                     },
                 });
             } else {
-                return None;
+                return Err(format!(
+                    "Failed to import some of the turn groups for {}",
+                    raw.intersection_osm_node_id
+                ));
             }
         }
         ControlTrafficSignal {
@@ -301,7 +304,6 @@ impl ControlTrafficSignal {
             turn_groups: TurnGroup::for_i(id, map),
         }
         .validate()
-        .ok()
     }
 }
 