a-b-street/abstreet

commit f1ef02b90752d9474f97634247c2d1e6e9dfa50c

store Tags in map_model objects, to reap its benefits anywhere in the codebase. serialization format doesnt change!

map_model/src/pathfind/mod.rs
@@ -459,9 +459,10 @@ impl PathConstraints {
                     true
                 } else if l.is_driving() || (l.is_bus() && map.config.bikes_can_use_bus_lanes) {
                     let road = map.get_r(l.parent);
-                    road.osm_tags.get("bicycle") != Some(&"no".to_string())
-                        && road.osm_tags.get(osm::HIGHWAY) != Some(&"motorway".to_string())
-                        && road.osm_tags.get(osm::HIGHWAY) != Some(&"motorway_link".to_string())
+                    !road.osm_tags.is("bicycle", "no")
+                        && road
+                            .osm_tags
+                            .is_any(osm::HIGHWAY, vec!["motorway", "motorway_link"])
                 } else {
                     false
                 }

game/src/debug/mod.rs
@@ -9,7 +9,7 @@ use crate::game::{msg, DrawBaselayer, State, Transition, WizardState};
 use crate::helpers::ID;
 use crate::managed::{WrappedComposite, WrappedOutcome};
 use crate::render::{calculate_corners, DrawOptions};
-use abstutil::Timer;
+use abstutil::{Tags, Timer};
 use ezgui::{
     hotkey, lctrl, Btn, Checkbox, Color, Composite, Drawable, EventCtx, GeomBatch, GfxCtx,
     HorizontalAlignment, Key, Line, Outcome, Text, UpdateType, VerticalAlignment, Widget, Wizard,
@@ -17,7 +17,7 @@ use ezgui::{
 use geom::Pt2D;
 use map_model::{osm, ControlTrafficSignal, NORMAL_LANE_THICKNESS};
 use sim::{AgentID, Sim};
-use std::collections::{BTreeMap, HashSet};
+use std::collections::HashSet;
 
 pub struct DebugMode {
     composite: Composite,
@@ -345,6 +345,7 @@ fn search_osm(wiz: &mut Wizard, ctx: &mut EventCtx, app: &mut App) -> Option<Tra
     let color = Color::RED;
     for r in map.all_roads() {
         if r.osm_tags
+            .inner()
             .iter()
             .any(|(k, v)| format!("{} = {}", k, v).contains(&filter))
         {
@@ -354,6 +355,7 @@ fn search_osm(wiz: &mut Wizard, ctx: &mut EventCtx, app: &mut App) -> Option<Tra
     }
     for a in map.all_areas() {
         if a.osm_tags
+            .inner()
             .iter()
             .any(|(k, v)| format!("{} = {}", k, v).contains(&filter))
         {
@@ -672,8 +674,8 @@ fn find_degenerate_roads(app: &App) {
     }
 }
 
-fn diff_tags(t1: &BTreeMap<String, String>, t2: &BTreeMap<String, String>) {
-    for (k, v1) in t1 {
+fn diff_tags(t1: &Tags, t2: &Tags) {
+    for (k, v1) in t1.inner() {
         if k == osm::OSM_WAY_ID {
             continue;
         }
@@ -682,7 +684,7 @@ fn diff_tags(t1: &BTreeMap<String, String>, t2: &BTreeMap<String, String>) {
             println!("- {} = \"{}\" vs \"{}\"", k, v1, v2);
         }
     }
-    for (k, v2) in t2 {
+    for (k, v2) in t2.inner() {
         if !t1.contains_key(k) {
             println!("- {} = \"\" vs \"{}\"", k, v2);
         }

map_model/src/make/initial/mod.rs
@@ -4,7 +4,7 @@ pub mod lane_specs;
 pub use self::geometry::intersection_polygon;
 use crate::raw::{DrivingSide, OriginalIntersection, OriginalRoad, RawMap, RawRoad};
 use crate::{IntersectionType, LaneType, NORMAL_LANE_THICKNESS, SIDEWALK_THICKNESS};
-use abstutil::Timer;
+use abstutil::{Tags, Timer};
 use geom::{Bounds, Distance, PolyLine, Pt2D};
 use std::collections::{BTreeMap, BTreeSet};
 
@@ -132,7 +132,7 @@ impl LaneSpec {
     }
 }
 
-pub fn get_lane_specs(osm_tags: &BTreeMap<String, String>) -> Vec<LaneSpec> {
+pub fn get_lane_specs(osm_tags: &Tags) -> Vec<LaneSpec> {
     let (side1_types, side2_types) = lane_specs::get_lane_types(osm_tags);
 
     let mut specs: Vec<LaneSpec> = Vec::new();

map_model/src/make/mod.rs
@@ -197,7 +197,7 @@ impl Map {
             }
             if road.get_name() == "???" {
                 // Suppress the warning in some cases.
-                if !(road.osm_tags.get("noname") == Some(&"yes".to_string())
+                if !(road.osm_tags.is("noname", "yes")
                     || road
                         .osm_tags
                         .get(osm::HIGHWAY)
@@ -234,8 +234,7 @@ impl Map {
                 for r in &i.roads {
                     // Skip signals only connected to roads under construction or purely to control
                     // light rail tracks.
-                    if map.roads[r.0].osm_tags.get(osm::HIGHWAY)
-                        != Some(&"construction".to_string())
+                    if !map.roads[r.0].osm_tags.is(osm::HIGHWAY, "construction")
                         && !map.roads[r.0].is_light_rail()
                     {
                         ok = true;

abstutil/src/collections.rs
@@ -1,4 +1,5 @@
 use itertools::Itertools;
+use serde::{Deserialize, Serialize};
 use std::cmp::Ord;
 use std::collections::{BTreeMap, BTreeSet};
 
@@ -202,7 +203,7 @@ impl<K: Clone + PartialEq, V> VecMap<K, V> {
 }
 
 // Convenience functions around a string->string map
-#[derive(Clone)]
+#[derive(Clone, Debug, PartialEq, Serialize, Deserialize)]
 pub struct Tags(BTreeMap<String, String>);
 
 impl Tags {
@@ -233,9 +234,12 @@ impl Tags {
     pub fn insert<K: Into<String>, V: Into<String>>(&mut self, k: K, v: V) {
         self.0.insert(k.into(), v.into());
     }
+    pub fn remove(&mut self, k: &str) -> Option<String> {
+        self.0.remove(k)
+    }
 
-    // TODO Maybe store this directly instead.
-    pub fn take(self) -> BTreeMap<String, String> {
-        self.0
+    // TODO Really just iter()
+    pub fn inner(&self) -> &BTreeMap<String, String> {
+        &self.0
     }
 }

convert_osm/src/lib.rs
@@ -90,17 +90,15 @@ pub fn convert(opts: Options, timer: &mut abstutil::Timer) -> RawMap {
             let pct = pct as i64;
             for (id, r) in map.roads.iter_mut() {
                 if r.osm_tags.contains_key(osm::INFERRED_PARKING)
-                    && (r.osm_tags.get(osm::HIGHWAY) == Some(&"residential".to_string())
-                        || r.osm_tags.get(osm::HIGHWAY) == Some(&"tertiary".to_string()))
+                    && r.osm_tags
+                        .is_any(osm::HIGHWAY, vec!["residential", "tertiary"])
                     && id.osm_way_id % 100 <= pct
                 {
-                    if r.osm_tags.get("oneway") == Some(&"yes".to_string()) {
+                    if r.osm_tags.is("oneway", "yes") {
                         r.osm_tags.remove(osm::PARKING_BOTH);
-                        r.osm_tags
-                            .insert(osm::PARKING_RIGHT.to_string(), "parallel".to_string());
+                        r.osm_tags.insert(osm::PARKING_RIGHT, "parallel");
                     } else {
-                        r.osm_tags
-                            .insert(osm::PARKING_BOTH.to_string(), "parallel".to_string());
+                        r.osm_tags.insert(osm::PARKING_BOTH, "parallel");
                     }
                 }
             }
@@ -175,10 +173,8 @@ fn use_parking_hints(map: &mut RawMap, path: String, timer: &mut Timer) {
             let has_parking = category != Some(&"None".to_string())
                 && category != Some(&"No Parking Allowed".to_string());
 
-            let definitely_no_parking = match tags.get(osm::HIGHWAY) {
-                Some(hwy) => hwy == "motorway" || hwy == "motorway_link",
-                None => false,
-            };
+            let definitely_no_parking =
+                tags.is_any(osm::HIGHWAY, vec!["motorway", "motorway_link"]);
             if has_parking && definitely_no_parking {
                 timer.warn(format!(
                     "Blockface says there's parking along motorway {}, ignoring",
@@ -188,20 +184,20 @@ fn use_parking_hints(map: &mut RawMap, path: String, timer: &mut Timer) {
             }
 
             if let Some(both) = tags.remove(osm::PARKING_BOTH) {
-                tags.insert(osm::PARKING_LEFT.to_string(), both.clone());
-                tags.insert(osm::PARKING_RIGHT.to_string(), both.clone());
+                tags.insert(osm::PARKING_LEFT, both.clone());
+                tags.insert(osm::PARKING_RIGHT, both);
             }
 
             tags.insert(
                 if fwds {
-                    osm::PARKING_RIGHT.to_string()
+                    osm::PARKING_RIGHT
                 } else {
-                    osm::PARKING_LEFT.to_string()
+                    osm::PARKING_LEFT
                 },
                 if has_parking {
-                    "parallel".to_string()
+                    "parallel"
                 } else {
-                    "no_parking".to_string()
+                    "no_parking"
                 },
             );
 
@@ -211,7 +207,7 @@ fn use_parking_hints(map: &mut RawMap, path: String, timer: &mut Timer) {
             {
                 let value = tags.remove(osm::PARKING_LEFT).unwrap();
                 tags.remove(osm::PARKING_RIGHT).unwrap();
-                tags.insert(osm::PARKING_BOTH.to_string(), value);
+                tags.insert(osm::PARKING_BOTH, value);
             }
         }
     }

convert_osm/src/osm_reader.rs
@@ -135,7 +135,7 @@ pub fn extract_osm(
                 way.id,
                 RawRoad {
                     center_points: pts,
-                    osm_tags: tags.take(),
+                    osm_tags: tags,
                     turn_restrictions: Vec::new(),
                     complicated_turn_restrictions: Vec::new(),
                 },
@@ -154,7 +154,7 @@ pub fn extract_osm(
                     public_garage_name: None,
                     num_parking_spots: 0,
                     amenities: get_bldg_amenities(&tags),
-                    osm_tags: tags.take(),
+                    osm_tags: tags,
                 },
             );
         } else if let Some(at) = get_area_type(&tags) {
@@ -165,7 +165,7 @@ pub fn extract_osm(
                 area_type: at,
                 osm_id: way.id,
                 polygon: Polygon::new(&pts),
-                osm_tags: tags.take(),
+                osm_tags: tags,
             });
         } else if tags.is("natural", "coastline") {
             coastline_groups.push((way.id, pts));
@@ -209,7 +209,7 @@ pub fn extract_osm(
                             area_type,
                             osm_id: rel.id,
                             polygon,
-                            osm_tags: tags.clone().take(),
+                            osm_tags: tags.clone(),
                         });
                     }
                 }
@@ -272,7 +272,7 @@ pub fn extract_osm(
                             public_garage_name: None,
                             num_parking_spots: 0,
                             amenities: get_bldg_amenities(&tags),
-                            osm_tags: tags.take(),
+                            osm_tags: tags,
                         },
                     );
                 }
@@ -337,8 +337,8 @@ pub fn extract_osm(
     // Special case the coastline.
     println!("{} ways of coastline", coastline_groups.len());
     for polygon in glue_multipolygon(-1, coastline_groups, &boundary) {
-        let mut osm_tags = BTreeMap::new();
-        osm_tags.insert("water".to_string(), "ocean".to_string());
+        let mut osm_tags = Tags::new(BTreeMap::new());
+        osm_tags.insert("water", "ocean");
         // Put it at the beginning, so that it's naturally beneath island areas
         map.areas.insert(
             0,

convert_osm/src/split_ways.rs
@@ -196,7 +196,7 @@ pub fn split_up_roads(
         for (id, r) in &map.roads {
             if r.center_points.contains(&pt.to_pt2d()) {
                 // Example: https://www.openstreetmap.org/node/26734224
-                if r.osm_tags.get(osm::HIGHWAY) == Some(&"construction".to_string()) {
+                if r.osm_tags.is(osm::HIGHWAY, "construction") {
                     break;
                 }
 

commit d278739f49ce4195f336c8907c0a60537d74893f

fix shameful bug introduced in f1ef02b90752d9474f97634247c2d1e6e9dfa50c. regenerate all data, with fresh krakow osm

map_model/src/pathfind/mod.rs
@@ -460,7 +460,7 @@ impl PathConstraints {
                 } else if l.is_driving() || (l.is_bus() && map.config.bikes_can_use_bus_lanes) {
                     let road = map.get_r(l.parent);
                     !road.osm_tags.is("bicycle", "no")
-                        && road
+                        && !road
                             .osm_tags
                             .is_any(osm::HIGHWAY, vec!["motorway", "motorway_link"])
                 } else {