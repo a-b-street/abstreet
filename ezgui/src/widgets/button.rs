5f1f4b74d3530e39eebfb6cb766b1eed5e10a536
Michael Kirk <michael.code@endoftheworl.de>
2020-07-27 17:04:34 -0600

    minimize diff

--- ezgui/src/widgets/button.rs
+++ ezgui/src/widgets/button.rs
@@ -386,16 +386,13 @@ impl BtnBuilder {
                 maybe_tooltip,
                 unselected_bg_color,
                 selected_bg_color,
-                label,
                 ..
             } => {
                 const HORIZ_PADDING: f64 = 30.0;
                 const VERT_PADDING: f64 = 10.0;
 
-                // let dims = text.size(ctx);
                 let txt_batch = text.render_ctx(ctx);
                 let dims = txt_batch.get_dims();
-                println!("label: {}, dims: {:?}", label, dims);
                 let geom = Polygon::rounded_rectangle(
                     dims.width + 2.0 * HORIZ_PADDING,
                     dims.height + 2.0 * VERT_PADDING,
