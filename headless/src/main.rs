8ce256828011a1988561a94551de3af42b09d817
Dustin Carlino <dabreegster@gmail.com>
2020-08-11 16:44:19 -0700

    turn the headless runner into a demo of a JSON API, for #245 (#268)
    turn the headless runner into a demo of a JSON API, for #245

--- headless/src/main.rs
+++ headless/src/main.rs
@@ -1,58 +1,83 @@
-use abstutil::{prettyprint_usize, CmdArgs, Timer};
+// This runs a simulation without any graphics and serves a very basic API to control things. The
+// API is not documented yet. To run this:
+//
+// > cd headless; cargo run -- --port=1234 ../data/system/scenarios/montlake/weekday.bin
+// > curl http://localhost:1234/get-time
+// 00:00:00.0
+// > curl http://localhost:1234/goto-time?t=01:01:00
+// it's now 01:01:00.0
+// > curl http://localhost:1234/get-delays
+// ... huge JSON blob
+
+use abstutil::{CmdArgs, Timer};
+use geom::Time;
+use hyper::{Body, Request, Response, Server};
 use map_model::Map;
-use rand::SeedableRng;
-use rand_xorshift::XorShiftRng;
-use sim::{AlertHandler, Scenario, ScenarioModifier, Sim, SimFlags};
+use sim::{AlertHandler, Sim, SimFlags, SimOptions};
+use std::collections::HashMap;
+use std::sync::RwLock;
 
-// This is specialized to experiment with running the pandemic model over long time periods.
-// Original functionality for profiling and debugging gridlock have been removed.
+lazy_static::lazy_static! {
+    static ref MAP: RwLock<Map> = RwLock::new(Map::blank());
+    static ref SIM: RwLock<Sim> = RwLock::new(Sim::new(&Map::blank(), SimOptions::new("tmp"), &mut Timer::throwaway()));
+}
 
-fn main() {
+#[tokio::main]
+async fn main() {
     let mut args = CmdArgs::new();
-    let num_days = args
-        .optional_parse("--days", |s| s.parse::<usize>())
-        .unwrap_or(1);
+    let mut sim_flags = SimFlags::from_args(&mut args);
+    let port = args.required("--port").parse::<u16>().unwrap();
     args.done();
 
-    let mut sim_flags = SimFlags::synthetic_test("montlake", "pandemic");
-    sim_flags.opts.enable_pandemic_model = Some(XorShiftRng::from_seed([sim_flags.rng_seed; 16]));
     // Less spam
     sim_flags.opts.alerts = AlertHandler::Silence;
     let mut timer = Timer::new("setup headless");
-    let (mut map, _, mut rng) = sim_flags.load(&mut timer);
-    map.hack_override_offstreet_spots(num_days);
-    // After the map changes, have to re-create the (empty) Sim, because things like
-    // ParkingSimState are out of sync.
-    let mut sim = Sim::new(&map, sim_flags.opts.clone(), &mut timer);
+    let (map, sim, _) = sim_flags.load(&mut timer);
+    *MAP.write().unwrap() = map;
+    *SIM.write().unwrap() = sim;
 
-    let base_scenario: Scenario = abstutil::read_binary(
-        abstutil::path_scenario(map.get_name(), "weekday"),
-        &mut timer,
-    );
-    ScenarioModifier::RepeatDays(num_days)
-        .apply(&map, base_scenario, &mut rng)
-        .instantiate(&mut sim, &map, &mut rng, &mut timer);
-    timer.done();
-
-    run_experiment(&map, &mut sim);
+    let addr = std::net::SocketAddr::from(([127, 0, 0, 1], port));
+    println!("Listening on http://{}", addr);
+    let serve_future = Server::bind(&addr).serve(hyper::service::make_service_fn(|_| async {
+        Ok::<_, hyper::Error>(hyper::service::service_fn(serve_req))
+    }));
+    if let Err(err) = serve_future.await {
+        panic!("Server error: {}", err);
+    }
 }
 
-fn run_experiment(map: &Map, sim: &mut Sim) {
-    let timer = Timer::new("run sim until done");
-    sim.run_until_done(
-        &map,
-        |sim, _map| {
-            // This'll run every 30 sim seconds
-            if false {
-                println!(
-                    "At {}, {} infected",
-                    sim.time(),
-                    prettyprint_usize(sim.get_pandemic_model().unwrap().count_infected())
-                );
-            }
-        },
-        None,
+async fn serve_req(req: Request<Body>) -> Result<Response<Body>, hyper::Error> {
+    let path = req.uri().path();
+    // Url::parse needs an absolute URL
+    let params: HashMap<String, String> =
+        url::Url::parse(&format!("http://localhost{}", req.uri()))
+            .unwrap()
+            .query_pairs()
+            .map(|(k, v)| (k.to_string(), v.to_string()))
+            .collect();
+    let resp = handle_command(
+        path,
+        params,
+        &mut SIM.write().unwrap(),
+        &MAP.read().unwrap(),
     );
-    timer.done();
-    println!("Done at {}", sim.time());
+    Ok(Response::new(Body::from(resp)))
+}
+
+fn handle_command(path: &str, params: HashMap<String, String>, sim: &mut Sim, map: &Map) -> String {
+    match path {
+        "/get-time" => sim.time().to_string(),
+        "/goto-time" => {
+            let t = Time::parse(&params["t"]).unwrap();
+            if t <= sim.time() {
+                format!("{} is in the past", t)
+            } else {
+                let dt = t - sim.time();
+                sim.timed_step(map, dt, &mut None, &mut Timer::throwaway());
+                format!("it's now {}", t)
+            }
+        }
+        "/get-delays" => abstutil::to_json(&sim.get_analytics().intersection_delays),
+        _ => format!("Unknown command {} with params {:?}", path, params),
+    }
 }
