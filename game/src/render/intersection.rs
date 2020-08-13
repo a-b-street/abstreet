a-b-street/abstreet

commit 9ce7ab1c88fdaf350d09fe00e587aa00579fc963

defer drawing intersections, same as lanes. faster startup time and melts my GPU less trying to load london and tokyo

game/src/render/intersection.rs
@@ -5,7 +5,7 @@ use crate::options::TrafficSignalStyle;
 use crate::render::{
     draw_signal_phase, DrawOptions, Renderable, CROSSWALK_LINE_THICKNESS, OUTLINE_THICKNESS,
 };
-use ezgui::{Color, Drawable, EventCtx, GeomBatch, GfxCtx, Line, RewriteColor, Text};
+use ezgui::{Color, Drawable, GeomBatch, GfxCtx, Line, RewriteColor, Text};
 use geom::{Angle, ArrowCap, Distance, Line, PolyLine, Polygon, Pt2D, Ring, Time, EPSILON_DIST};
 use map_model::{
     Intersection, IntersectionID, IntersectionType, Map, Road, RoadWithStopSign, Turn, TurnType,
@@ -18,41 +18,58 @@ pub struct DrawIntersection {
     intersection_type: IntersectionType,
     zorder: isize,
 
-    draw_default: Drawable,
+    draw_default: RefCell<Option<Drawable>>,
     pub draw_traffic_signal: RefCell<Option<(Time, Drawable)>>,
 }
 
 impl DrawIntersection {
-    pub fn new(ctx: &EventCtx, i: &Intersection, map: &Map, cs: &ColorScheme) -> DrawIntersection {
+    pub fn new(i: &Intersection, map: &Map) -> DrawIntersection {
+        DrawIntersection {
+            id: i.id,
+            intersection_type: i.intersection_type,
+            zorder: i.get_zorder(map),
+            draw_default: RefCell::new(None),
+            draw_traffic_signal: RefCell::new(None),
+        }
+    }
+
+    pub fn clear_rendering(&mut self) {
+        *self.draw_default.borrow_mut() = None;
+    }
+
+    fn render(&self, g: &mut GfxCtx, app: &App) -> Drawable {
+        let map = &app.primary.map;
+        let i = map.get_i(self.id);
+
         // Order matters... main polygon first, then sidewalk corners.
         let mut default_geom = GeomBatch::new();
-        default_geom.push(cs.normal_intersection, i.polygon.clone());
-        default_geom.extend(cs.sidewalk, calculate_corners(i, map));
+        default_geom.push(app.cs.normal_intersection, i.polygon.clone());
+        default_geom.extend(app.cs.sidewalk, calculate_corners(i, map));
 
         for turn in map.get_turns_in_intersection(i.id) {
             // Avoid double-rendering
             if turn.turn_type == TurnType::Crosswalk
                 && !turn.other_crosswalk_ids.iter().any(|id| *id < turn.id)
             {
-                make_crosswalk(&mut default_geom, turn, map, cs);
+                make_crosswalk(&mut default_geom, turn, map, &app.cs);
             }
         }
 
         if i.is_private(map) {
-            default_geom.push(cs.private_road.alpha(0.5), i.polygon.clone());
+            default_geom.push(app.cs.private_road.alpha(0.5), i.polygon.clone());
         }
 
         match i.intersection_type {
             IntersectionType::Border => {
                 let r = map.get_r(*i.roads.iter().next().unwrap());
-                default_geom.extend(cs.road_center_line, calculate_border_arrows(i, r, map));
+                default_geom.extend(app.cs.road_center_line, calculate_border_arrows(i, r, map));
             }
             IntersectionType::StopSign => {
                 for ss in map.get_stop_sign(i.id).roads.values() {
                     if ss.must_stop {
                         if let Some((octagon, pole)) = DrawIntersection::stop_sign_geom(ss, map) {
-                            default_geom.push(cs.stop_sign, octagon);
-                            default_geom.push(cs.stop_sign_pole, pole);
+                            default_geom.push(app.cs.stop_sign, octagon);
+                            default_geom.push(app.cs.stop_sign_pole, pole);
                         }
                     }
                 }
@@ -61,7 +78,7 @@ impl DrawIntersection {
                 // TODO Centering seems weird
                 default_geom.append(
                     GeomBatch::mapspace_svg(
-                        ctx.prerender,
+                        g.prerender,
                         "system/assets/map/under_construction.svg",
                     )
                     .scale(0.08)
@@ -76,13 +93,7 @@ impl DrawIntersection {
             default_geom = default_geom.color(RewriteColor::ChangeAlpha(0.5));
         }
 
-        DrawIntersection {
-            id: i.id,
-            intersection_type: i.intersection_type,
-            zorder,
-            draw_default: ctx.upload(default_geom),
-            draw_traffic_signal: RefCell::new(None),
-        }
+        g.upload(default_geom)
     }
 
     // Returns the (octagon, pole) if there's room to draw it.
@@ -123,7 +134,13 @@ impl Renderable for DrawIntersection {
     }
 
     fn draw(&self, g: &mut GfxCtx, app: &App, opts: &DrawOptions) {
-        g.redraw(&self.draw_default);
+        // Lazily calculate, because these are expensive to all do up-front, and most players won't
+        // exhaustively see every lane during a single session
+        let mut draw = self.draw_default.borrow_mut();
+        if draw.is_none() {
+            *draw = Some(self.render(g, app));
+        }
+        g.redraw(draw.as_ref().unwrap());
 
         if self.intersection_type == IntersectionType::TrafficSignal
             && !opts.suppress_traffic_signal_details.contains(&self.id)

game/src/edit/mod.rs
@@ -22,7 +22,7 @@ use crate::debug::DebugMode;
 use crate::game::{PopupMsg, State, Transition};
 use crate::helpers::ID;
 use crate::options::OptionsPanel;
-use crate::render::{DrawIntersection, DrawMap, DrawRoad};
+use crate::render::{DrawMap, DrawRoad};
 use crate::sandbox::{GameplayMode, SandboxMode, TimeWarpScreen};
 use abstutil::Timer;
 use ezgui::{
@@ -578,8 +578,7 @@ pub fn apply_map_edits(ctx: &mut EventCtx, app: &mut App, edits: MapEdits) {
     }
 
     for i in modified_intersections {
-        app.primary.draw_map.intersections[i.0] =
-            DrawIntersection::new(ctx, app.primary.map.get_i(i), &app.primary.map, &app.cs);
+        app.primary.draw_map.intersections[i.0].clear_rendering();
     }
 
     if app.layer.as_ref().and_then(|l| l.name()) == Some("map edits") {

game/src/render/map.rs
@@ -64,7 +64,7 @@ impl DrawMap {
         timer.start_iter("make DrawIntersections", map.all_intersections().len());
         for i in map.all_intersections() {
             timer.next();
-            intersections.push(DrawIntersection::new(ctx, i, map, cs));
+            intersections.push(DrawIntersection::new(i, map));
         }
 
         let draw_all_unzoomed_roads_and_intersections =

commit f70d01626f9907356e51bb7a5b7535916d362758

Fix bug introduced in 9ce7ab1c88fdaf350d09fe00e587aa00579fc963. Don't cache intersection_type in DrawIntersection; it changes when converting stop signs<->traffic signals<->closed for construction.

game/src/render/intersection.rs
@@ -15,7 +15,6 @@ use std::cell::RefCell;
 
 pub struct DrawIntersection {
     pub id: IntersectionID,
-    intersection_type: IntersectionType,
     zorder: isize,
 
     draw_default: RefCell<Option<Drawable>>,
@@ -26,7 +25,6 @@ impl DrawIntersection {
     pub fn new(i: &Intersection, map: &Map) -> DrawIntersection {
         DrawIntersection {
             id: i.id,
-            intersection_type: i.intersection_type,
             zorder: i.get_zorder(map),
             draw_default: RefCell::new(None),
             draw_traffic_signal: RefCell::new(None),
@@ -139,39 +137,39 @@ impl Renderable for DrawIntersection {
         }
         g.redraw(draw.as_ref().unwrap());
 
-        if self.intersection_type == IntersectionType::TrafficSignal
-            && !opts.suppress_traffic_signal_details.contains(&self.id)
-        {
-            let signal = app.primary.map.get_traffic_signal(self.id);
-            let mut maybe_redraw = self.draw_traffic_signal.borrow_mut();
-            let recalc = maybe_redraw
-                .as_ref()
-                .map(|(t, _)| *t != app.primary.sim.time())
-                .unwrap_or(true);
-            if recalc {
-                let (idx, remaining) = app.primary.sim.current_phase_and_remaining_time(self.id);
-                let mut batch = GeomBatch::new();
-                draw_signal_phase(
-                    g.prerender,
-                    &signal.phases[idx],
-                    self.id,
-                    Some(remaining),
-                    &mut batch,
-                    app,
-                    app.opts.traffic_signal_style.clone(),
-                );
-                if app.opts.traffic_signal_style != TrafficSignalStyle::BAP {
-                    batch.append(
-                        Text::from(Line(format!("{}", idx + 1)))
-                            .render_to_batch(g.prerender)
-                            .scale(0.1)
-                            .centered_on(app.primary.map.get_i(self.id).polygon.center()),
+        if let Some(signal) = app.primary.map.maybe_get_traffic_signal(self.id) {
+            if !opts.suppress_traffic_signal_details.contains(&self.id) {
+                let mut maybe_redraw = self.draw_traffic_signal.borrow_mut();
+                let recalc = maybe_redraw
+                    .as_ref()
+                    .map(|(t, _)| *t != app.primary.sim.time())
+                    .unwrap_or(true);
+                if recalc {
+                    let (idx, remaining) =
+                        app.primary.sim.current_phase_and_remaining_time(self.id);
+                    let mut batch = GeomBatch::new();
+                    draw_signal_phase(
+                        g.prerender,
+                        &signal.phases[idx],
+                        self.id,
+                        Some(remaining),
+                        &mut batch,
+                        app,
+                        app.opts.traffic_signal_style.clone(),
                     );
+                    if app.opts.traffic_signal_style != TrafficSignalStyle::BAP {
+                        batch.append(
+                            Text::from(Line(format!("{}", idx + 1)))
+                                .render_to_batch(g.prerender)
+                                .scale(0.1)
+                                .centered_on(app.primary.map.get_i(self.id).polygon.center()),
+                        );
+                    }
+                    *maybe_redraw = Some((app.primary.sim.time(), g.prerender.upload(batch)));
                 }
-                *maybe_redraw = Some((app.primary.sim.time(), g.prerender.upload(batch)));
+                let (_, batch) = maybe_redraw.as_ref().unwrap();
+                g.redraw(batch);
             }
-            let (_, batch) = maybe_redraw.as_ref().unwrap();
-            g.redraw(batch);
         }
     }
 