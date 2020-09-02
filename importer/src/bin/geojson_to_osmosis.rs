0f8fe341382ba67a798d8867e9b77bcdd1a7b776
Michael Kirk <michael.code@endoftheworl.de>
2020-09-02 13:09:20 -0700

    remove boilerplate error handling

--- importer/src/bin/geojson_to_osmosis.rs
+++ importer/src/bin/geojson_to_osmosis.rs
@@ -2,11 +2,11 @@ use std::io::{self, Read};
 use geojson::{GeoJson, Value};
 
 /// Convert geojson boundary suitable for osmfilter and other osmosis based tools.
-/// Expects the input to no elements other than the boundary of interest.
+/// Expects the input to contain no element other than the boundary of interest.
 //
 /// Reads geojson text from stdin
 /// Writes "poly" formatted text to stdout
-fn main() -> Result<(), Error> {
+fn main() -> Result<(), Box<dyn std::error::Error>> {
     let mut buffer = String::new();
     io::stdin().read_to_string(&mut buffer)?;
     let geojson = buffer.parse::<GeoJson>()?;
@@ -22,11 +22,11 @@ fn main() -> Result<(), Error> {
     Ok(())
 }
 
-fn boundary_coords(geojson: &GeoJson) -> Result<Vec<Vec<f64>>, Error> {
+fn boundary_coords(geojson: &GeoJson) -> Result<Vec<Vec<f64>>, Box<dyn std::error::Error>> {
     let feature = match geojson {
         GeoJson::Feature(feature) => feature,
         GeoJson::FeatureCollection(feature_collection) => &feature_collection.features[0],
-        _ => return Err(Error::BadInput(format!("Unexpected geojson: {:?}", geojson)))
+        _ => return Err(format!("Unexpected geojson: {:?}", geojson).into())
     };
 
     match &feature.geometry.as_ref().map(|g| &g.value) {
@@ -36,26 +36,7 @@ fn boundary_coords(geojson: &GeoJson) -> Result<Vec<Vec<f64>>, Error> {
         Some(Value::Polygon(polygon)) => {
             return Ok(polygon[0].clone())
         },
-        _ => Err(Error::BadInput(format!("Unexpected feature: {:?}", feature)))
-    }
-}
-
-#[derive(Debug)]
-pub enum Error {
-    IO(std::io::Error),
-    GeoJson(geojson::Error),
-    BadInput(String),
-}
-
-impl std::convert::From<std::io::Error> for Error {
-    fn from(e: std::io::Error) -> Error {
-        Error::IO(e)
-    }
-}
-
-impl std::convert::From<geojson::Error> for Error {
-    fn from(e: geojson::Error) -> Error {
-        Error::GeoJson(e)
+        _ => Err(format!("Unexpected feature: {:?}", feature).into())
     }
 }
 
