495aedd39f495ac0cc43a33583fb0e33618bb2c6
dabreegster <dabreegster@users.noreply.github.com>
2020-09-07 17:46:31 +0000

    deploy: 0b9b7e1a65ff5d46db501598c651e4ebfea1192b

--- rustdoc/game/sandbox/gameplay/commute/sidebar-items.js
+++ rustdoc/game/sandbox/gameplay/commute/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["METER_HACK",""]],"fn":[["cutscene_task",""],["final_score",""],["get_score",""],["make_meter",""]],"struct":[["Actions",""],["OptimizeCommute",""]]});
+initSidebarItems({"constant":[["METER_HACK",""]],"fn":[["cutscene_task",""],["final_score",""],["get_score",""],["make_meter",""]],"struct":[["OptimizeCommute",""]]});
