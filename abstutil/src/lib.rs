a-b-street/abstreet

commit 65e07d9cc78205c651e3504ce042ce9d34afb8a0

Read/write scenarios in JSON, so people can manipulate them in any language. #313

abstutil/src/lib.rs
@@ -12,7 +12,7 @@ pub use crate::collections::{
 pub use crate::io::{
     basename, deserialize_btreemap, deserialize_multimap, deserialize_usize, file_exists,
     find_next_file, find_prev_file, from_json, list_all_objects, list_dir, load_all_objects,
-    maybe_read_binary, maybe_read_json, read_binary, read_json, serialize_btreemap,
+    maybe_read_binary, maybe_read_json, read_binary, read_json, read_object, serialize_btreemap,
     serialize_multimap, serialize_usize, serialized_size_bytes, slurp_file, to_json, write_binary,
     write_json, FileWithProgress,
 };
@@ -119,8 +119,16 @@ pub fn path_prebaked_results(map_name: &str, scenario_name: &str) -> String {
 }
 
 pub fn path_scenario(map_name: &str, scenario_name: &str) -> String {
-    path(format!(
+    // TODO Getting complicated. Looks for .bin, then .json.
+    let p = path(format!(
         "system/scenarios/{}/{}.bin",
+        map_name, scenario_name
+    ));
+    if file_exists(&p) {
+        return p;
+    }
+    path(format!(
+        "system/scenarios/{}/{}.json",
         map_name, scenario_name
     ))
 }

abstutil/src/io.rs
@@ -156,6 +156,14 @@ pub fn read_binary<T: DeserializeOwned>(path: String, timer: &mut Timer) -> T {
     }
 }
 
+pub fn read_object<T: DeserializeOwned>(path: String, timer: &mut Timer) -> Result<T, Error> {
+    if path.ends_with(".bin") {
+        maybe_read_binary(path, timer)
+    } else {
+        maybe_read_json(path, timer)
+    }
+}
+
 // For BTreeMaps with struct keys. See https://github.com/serde-rs/json/issues/402.
 
 pub fn serialize_btreemap<S: Serializer, K: Serialize, V: Serialize>(
@@ -438,13 +446,13 @@ pub fn basename(path: &str) -> String {
 }
 
 #[cfg(not(target_arch = "wasm32"))]
-pub fn file_exists(path: String) -> bool {
-    Path::new(&path).exists()
+pub fn file_exists<I: Into<String>>(path: I) -> bool {
+    Path::new(&path.into()).exists()
 }
 
 #[cfg(target_arch = "wasm32")]
-pub fn file_exists(path: String) -> bool {
+pub fn file_exists<I: Into<String>>(path: I) -> bool {
     SYSTEM_DATA
-        .get_file(path.trim_start_matches("../data/system/"))
+        .get_file(path.into().trim_start_matches("../data/system/"))
         .is_some()
 }

abstutil/src/time.rs
@@ -652,13 +652,15 @@ impl<'a> Read for Timer<'a> {
                 prettyprint_usize(file.total_bytes / 1024 / 1024),
                 prettyprint_time(elapsed)
             );
-            if file.last_printed_at.is_none() {
-                self.println(line.clone());
-            } else {
-                clear_current_line();
-                println!("{}", line);
-                if let Some(ref mut sink) = self.sink {
-                    sink.reprintln(line.clone());
+            if self.outermost_name != "throwaway" {
+                if file.last_printed_at.is_none() {
+                    self.println(line.clone());
+                } else {
+                    clear_current_line();
+                    println!("{}", line);
+                    if let Some(ref mut sink) = self.sink {
+                        sink.reprintln(line.clone());
+                    }
                 }
             }
             self.stack.pop();
@@ -666,23 +668,25 @@ impl<'a> Read for Timer<'a> {
         } else if file.last_printed_at.is_none()
             || elapsed_seconds(file.last_printed_at.unwrap()) >= PROGRESS_FREQUENCY_SECONDS
         {
-            let line = format!(
-                "Reading {}: {}/{} MB... {}",
-                file.path,
-                prettyprint_usize(file.processed_bytes / 1024 / 1024),
-                prettyprint_usize(file.total_bytes / 1024 / 1024),
-                prettyprint_time(elapsed_seconds(file.started_at))
-            );
-            // TODO Refactor this pattern...
-            clear_current_line();
-            print!("{}", line);
-            stdout().flush().unwrap();
+            if self.outermost_name != "throwaway" {
+                let line = format!(
+                    "Reading {}: {}/{} MB... {}",
+                    file.path,
+                    prettyprint_usize(file.processed_bytes / 1024 / 1024),
+                    prettyprint_usize(file.total_bytes / 1024 / 1024),
+                    prettyprint_time(elapsed_seconds(file.started_at))
+                );
+                // TODO Refactor this pattern...
+                clear_current_line();
+                print!("{}", line);
+                stdout().flush().unwrap();
 
-            if let Some(ref mut sink) = self.sink {
-                if file.last_printed_at.is_none() {
-                    sink.println(line);
-                } else {
-                    sink.reprintln(line);
+                if let Some(ref mut sink) = self.sink {
+                    if file.last_printed_at.is_none() {
+                        sink.println(line);
+                    } else {
+                        sink.reprintln(line);
+                    }
                 }
             }
 

game/src/sandbox/gameplay/mod.rs
@@ -117,7 +117,7 @@ impl GameplayMode {
             ScenarioGenerator::proletariat_robot(map, &mut rng, timer)
         } else {
             let path = abstutil::path_scenario(map.get_name(), &name);
-            let mut scenario = match abstutil::maybe_read_binary(path.clone(), timer) {
+            let mut scenario = match abstutil::read_object(path.clone(), timer) {
                 Ok(s) => s,
                 Err(err) => {
                     Map::corrupt_err(path, err);

importer/src/bin/dump_scenario.rs
@@ -1,0 +1,9 @@
+use abstutil::{CmdArgs, Timer};
+use sim::Scenario;
+
+fn main() {
+    let mut args = CmdArgs::new();
+    let scenario: Scenario = abstutil::read_binary(args.required_free(), &mut Timer::throwaway());
+    println!("{}", abstutil::to_json(&scenario));
+    args.done();
+}

commit c5bf1fcb748e9efe09064125b06202f2cf49ada6

Fix saving scenarios, broken in 65e07d9cc78205c651e3504ce042ce9d34afb8a0

abstutil/src/lib.rs
@@ -119,18 +119,23 @@ pub fn path_prebaked_results(map_name: &str, scenario_name: &str) -> String {
 }
 
 pub fn path_scenario(map_name: &str, scenario_name: &str) -> String {
-    // TODO Getting complicated. Looks for .bin, then .json.
-    let p = path(format!(
+    // TODO Getting complicated. Sometimes we're trying to load, so we should look for .bin, then
+    // .json. But when we're writing a custom scenario, we actually want to write a .bin.
+    let bin = path(format!(
         "system/scenarios/{}/{}.bin",
         map_name, scenario_name
     ));
-    if file_exists(&p) {
-        return p;
-    }
-    path(format!(
+    let json = path(format!(
         "system/scenarios/{}/{}.json",
         map_name, scenario_name
-    ))
+    ));
+    if file_exists(&bin) {
+        return bin;
+    }
+    if file_exists(&json) {
+        return json;
+    }
+    bin
 }
 pub fn path_all_scenarios(map_name: &str) -> String {
     path(format!("system/scenarios/{}", map_name))