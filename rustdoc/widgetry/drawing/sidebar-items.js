a2143d709f7f3e0adf13db522393bba1c73d9dc5
dabreegster <dabreegster@users.noreply.github.com>
2020-09-21 19:13:50 +0000

    deploy: 8042ad414f78f96ec7a643d00d497c2e1fb27e0a

--- rustdoc/widgetry/drawing/sidebar-items.js
+++ rustdoc/widgetry/drawing/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["MAPSPACE_Z",""],["SCREENSPACE_Z",""],["TOOLTIP_Z",""]],"struct":[["GfxCtx",""],["Prerender",""],["Uniforms",""]]});
+initSidebarItems({"constant":[["MAPSPACE_Z",""],["MENU_Z",""],["SCREENSPACE_Z",""],["TOOLTIP_Z",""]],"struct":[["GfxCtx",""],["Prerender",""],["Uniforms",""]]});
