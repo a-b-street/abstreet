ff4e87a74ac7462900b0b2d5af1e84bf81fb4559
dabreegster <dabreegster@users.noreply.github.com>
2020-10-03 23:31:43 +0000

    deploy: acaf7eb5873fda3c4d4303daaa2fdc45f665df4b

--- rustdoc/game/layer/traffic/sidebar-items.js
+++ rustdoc/game/layer/traffic/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["cluster_jams",""]],"struct":[["Backpressure",""],["Delay",""],["Jam",""],["Throughput",""],["TrafficJams",""]]});
+initSidebarItems({"fn":[["cluster_jams",""]],"struct":[["Backpressure",""],["CompareThroughput",""],["Delay",""],["Jam",""],["Throughput",""],["TrafficJams",""]]});
