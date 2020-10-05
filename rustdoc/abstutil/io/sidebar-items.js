394e55100f5046e29f7cb27755d1d608cf9baecc
dabreegster <dabreegster@users.noreply.github.com>
2020-10-05 22:23:23 +0000

    deploy: 5706226790c3d6d3f0290e6104a4ba5116d6e302

--- rustdoc/abstutil/io/sidebar-items.js
+++ rustdoc/abstutil/io/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["find_next_file",""],["find_prev_file",""],["load_all_objects",""],["maybe_read_json",""],["read_binary",""],["read_json",""],["read_object",""]]});
+initSidebarItems({"fn":[["find_next_file",""],["find_prev_file",""],["list_all_objects",""],["load_all_objects",""],["maybe_read_json",""],["read_binary",""],["read_json",""],["read_object",""]]});
