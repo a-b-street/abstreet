455ba83b4a8ef0ec6445d06dd6278fce14bb34bc
Dustin Carlino <dabreegster@gmail.com>
2020-10-08 10:48:52 -0700

    Symlinks

--- game/run_web.sh
+++ game/run_web.sh
@@ -2,8 +2,5 @@
 
 set -e
 wasm-pack build --dev --target web -- --no-default-features --features wasm
-cp index.html pkg
 cd pkg
-rm -f system
-ln -s ../../data/system/ .
 python3 -m http.server 8000
