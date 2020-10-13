a-b-street/abstreet

commit 1906fb0913e53f18b6cf2cdb623a830b789e045b

Remove the GetDrawAgents indirection. Originally the UI could pull the set of agents to draw either from the simulation or from this "time-traveler" plugin, which would let you rewind sim time. That plugin is long gone, and it never worked well, because much of the UI would use the GetDrawAgents to select something, then query the live sim for lots of details anyway. The plugin never served all of those calls, so the results would be kind of out-of-sync anyway.
There are some parts of the UI that need to temporarily not draw agents.
Use the same suspended_sim trick that edit mode does.

game/src/sandbox/dashboards/traffic_signals.rs
@@ -3,7 +3,7 @@ use std::collections::HashMap;
 use abstutil::{prettyprint_usize, Counter, Parallelism, Timer};
 use geom::{ArrowCap, Distance, Duration, Polygon, Time};
 use map_model::{ControlTrafficSignal, IntersectionID, MovementID, PathStep, TurnType};
-use sim::{DontDrawAgents, TripEndpoint};
+use sim::TripEndpoint;
 use widgetry::{
     Btn, Color, Drawable, EventCtx, GeomBatch, GfxCtx, HorizontalAlignment, Key, Line, Outcome,
     Panel, Spinner, Text, TextExt, VerticalAlignment, Widget,
@@ -26,6 +26,8 @@ pub struct TrafficSignalDemand {
 impl TrafficSignalDemand {
     pub fn new(ctx: &mut EventCtx, app: &mut App) -> Box<dyn State> {
         app.primary.current_selection = None;
+        assert!(app.primary.suspended_sim.is_none());
+        app.primary.suspended_sim = Some(app.primary.clear_sim());
 
         let all_demand = ctx.loading_screen("predict all demand", |_, timer| {
             Demand::all_demand(app, timer)
@@ -100,6 +102,7 @@ impl State for TrafficSignalDemand {
         match self.panel.event(ctx) {
             Outcome::Clicked(x) => match x.as_ref() {
                 "close" => {
+                    app.primary.sim = app.primary.suspended_sim.take().unwrap();
                     return Transition::Pop;
                 }
                 _ => unreachable!(),
@@ -133,7 +136,7 @@ impl State for TrafficSignalDemand {
         let mut opts = DrawOptions::new();
         opts.suppress_traffic_signal_details
             .extend(self.all_demand.keys().cloned());
-        app.draw(g, opts, &DontDrawAgents {}, &ShowEverything::new());
+        app.draw(g, opts, &ShowEverything::new());
 
         g.redraw(&self.draw_all);
         if let Some((ref draw, ref count)) = self.selected {

game/src/debug/mod.rs
@@ -337,7 +337,7 @@ impl State for DebugMode {
     fn draw(&self, g: &mut GfxCtx, app: &App) {
         let mut opts = DrawOptions::new();
         opts.label_buildings = self.layers.show_labels;
-        app.draw(g, opts, &app.primary.sim, self);
+        app.draw(g, opts, self);
 
         if let Some(ref results) = self.search_results {
             g.redraw(&results.draw);

game/src/edit/traffic_signals/mod.rs
@@ -454,7 +454,7 @@ impl State for TrafficSignalEditor {
             let mut opts = DrawOptions::new();
             opts.suppress_traffic_signal_details
                 .extend(self.members.clone());
-            app.draw(g, opts, &app.primary.sim, &ShowEverything::new());
+            app.draw(g, opts, &ShowEverything::new());
         }
         g.redraw(&self.fade_irrelevant);
         g.redraw(&self.draw_current);

sim/src/sim/mod.rs
@@ -17,13 +17,11 @@ use map_model::{
 
 pub use self::queries::AgentProperties;
 use crate::{
-    AgentID, AlertLocation, Analytics, CapSimState, CarID, Command, CreateCar, DrawCarInput,
-    DrawPedCrowdInput, DrawPedestrianInput, DrivingSimState, Event, GetDrawAgents,
-    IntersectionSimState, OrigPersonID, PandemicModel, ParkedCar, ParkingSim, ParkingSimState,
-    ParkingSpot, PedestrianID, Person, PersonID, Router, Scheduler, SidewalkPOI, SidewalkSpot,
-    TransitSimState, TripID, TripManager, TripPhaseType, TripSpawner, UnzoomedAgent, Vehicle,
-    VehicleSpec, VehicleType, WalkingSimState, BUS_LENGTH, LIGHT_RAIL_LENGTH, MIN_CAR_LENGTH,
-    SPAWN_DIST,
+    AgentID, AlertLocation, Analytics, CapSimState, CarID, Command, CreateCar, DrivingSimState,
+    Event, IntersectionSimState, OrigPersonID, PandemicModel, ParkedCar, ParkingSim,
+    ParkingSimState, ParkingSpot, Person, PersonID, Router, Scheduler, SidewalkPOI, SidewalkSpot,
+    TransitSimState, TripID, TripManager, TripPhaseType, TripSpawner, Vehicle, VehicleSpec,
+    VehicleType, WalkingSimState, BUS_LENGTH, LIGHT_RAIL_LENGTH, MIN_CAR_LENGTH, SPAWN_DIST,
 };
 
 mod queries;
@@ -362,69 +360,6 @@ impl Sim {
 
     pub fn set_name(&mut self, name: String) {
         self.run_name = name;
-    }
-}
-
-// Drawing
-impl GetDrawAgents for Sim {
-    fn time(&self) -> Time {
-        self.time
-    }
-
-    fn step_count(&self) -> usize {
-        self.step_count
-    }
-
-    fn get_draw_car(&self, id: CarID, map: &Map) -> Option<DrawCarInput> {
-        self.parking.get_draw_car(id, map).or_else(|| {
-            self.driving
-                .get_single_draw_car(id, self.time, map, &self.transit)
-        })
-    }
-
-    fn get_draw_ped(&self, id: PedestrianID, map: &Map) -> Option<DrawPedestrianInput> {
-        self.walking.get_draw_ped(id, self.time, map)
-    }
-
-    fn get_draw_cars(&self, on: Traversable, map: &Map) -> Vec<DrawCarInput> {
-        let mut results = Vec::new();
-        if let Traversable::Lane(l) = on {
-            if map.get_l(l).is_parking() {
-                return self.parking.get_draw_cars(l, map);
-            }
-            results.extend(self.parking.get_draw_cars_in_lots(l, map));
-        }
-        results.extend(
-            self.driving
-                .get_draw_cars_on(self.time, on, map, &self.transit),
-        );
-        results
-    }
-
-    fn get_draw_peds(
-        &self,
-        on: Traversable,
-        map: &Map,
-    ) -> (Vec<DrawPedestrianInput>, Vec<DrawPedCrowdInput>) {
-        self.walking.get_draw_peds_on(self.time, on, map)
-    }
-
-    fn get_all_draw_cars(&self, map: &Map) -> Vec<DrawCarInput> {
-        let mut result = self
-            .driving
-            .get_all_draw_cars(self.time, map, &self.transit);
-        result.extend(self.parking.get_all_draw_cars(map));
-        result
-    }
-
-    fn get_all_draw_peds(&self, map: &Map) -> Vec<DrawPedestrianInput> {
-        self.walking.get_all_draw_peds(self.time, map)
-    }
-
-    fn get_unzoomed_agents(&self, map: &Map) -> Vec<UnzoomedAgent> {
-        let mut result = self.driving.get_unzoomed_agents(self.time, map);
-        result.extend(self.walking.get_unzoomed_agents(self.time, map));
-        result
     }
 }
 

game/src/app.rs
@@ -7,7 +7,7 @@ use rand::seq::SliceRandom;
 use abstutil::Timer;
 use geom::{Bounds, Circle, Distance, Duration, Pt2D, Time};
 use map_model::{IntersectionID, Map, Traversable};
-use sim::{Analytics, DontDrawAgents, GetDrawAgents, Sim, SimCallback, SimFlags};
+use sim::{Analytics, Sim, SimCallback, SimFlags};
 use widgetry::{EventCtx, GfxCtx, Prerender};
 
 use crate::challenges::HighScore;
@@ -138,13 +138,7 @@ impl App {
         )
     }
 
-    pub fn draw(
-        &self,
-        g: &mut GfxCtx,
-        opts: DrawOptions,
-        source: &dyn GetDrawAgents,
-        show_objs: &dyn ShowObject,
-    ) {
+    pub fn draw(&self, g: &mut GfxCtx, opts: DrawOptions, show_objs: &dyn ShowObject) {
         let mut sample_intersection: Option<String> = None;
 
         g.clear(self.cs.void_background);
@@ -198,21 +192,13 @@ impl App {
             }
 
             let mut cache = self.primary.draw_map.agents.borrow_mut();
-            cache.draw_unzoomed_agents(
-                g,
-                source,
-                &self.primary.map,
-                &self.unzoomed_agents,
-                self.opts.debug_all_agents,
-                &self.cs,
-            );
+            cache.draw_unzoomed_agents(g, self);
         } else {
             let mut cache = self.primary.draw_map.agents.borrow_mut();
             let objects = self.get_renderables_back_to_front(
                 g.get_screen_bounds(),
                 &g.prerender,
                 &mut cache,
-                source,
                 show_objs,
             );
 
@@ -259,54 +245,26 @@ impl App {
 
     /// Assumes some defaults.
     pub fn recalculate_current_selection(&mut self, ctx: &EventCtx) {
-        self.primary.current_selection = self.calculate_current_selection(
-            ctx,
-            &self.primary.sim,
-            &ShowEverything::new(),
-            false,
-            false,
-            false,
-        );
+        self.primary.current_selection =
+            self.calculate_current_selection(ctx, &ShowEverything::new(), false, false, false);
     }
 
     pub fn mouseover_unzoomed_roads_and_intersections(&self, ctx: &EventCtx) -> Option<ID> {
-        self.calculate_current_selection(
-            ctx,
-            &DontDrawAgents {},
-            &ShowEverything::new(),
-            false,
-            true,
-            false,
-        )
+        self.calculate_current_selection(ctx, &ShowEverything::new(), false, true, false)
     }
     pub fn mouseover_unzoomed_buildings(&self, ctx: &EventCtx) -> Option<ID> {
-        self.calculate_current_selection(
-            ctx,
-            &DontDrawAgents {},
-            &ShowEverything::new(),
-            false,
-            false,
-            true,
-        )
+        self.calculate_current_selection(ctx, &ShowEverything::new(), false, false, true)
     }
     pub fn mouseover_unzoomed_everything(&self, ctx: &EventCtx) -> Option<ID> {
-        self.calculate_current_selection(
-            ctx,
-            &DontDrawAgents {},
-            &ShowEverything::new(),
-            false,
-            true,
-            true,
-        )
+        self.calculate_current_selection(ctx, &ShowEverything::new(), false, true, true)
     }
     pub fn mouseover_debug_mode(&self, ctx: &EventCtx, show_objs: &dyn ShowObject) -> Option<ID> {
-        self.calculate_current_selection(ctx, &self.primary.sim, show_objs, true, false, false)
+        self.calculate_current_selection(ctx, show_objs, true, false, false)
     }
 
     fn calculate_current_selection(
         &self,
         ctx: &EventCtx,
-        source: &dyn GetDrawAgents,
         show_objs: &dyn ShowObject,
         debug_mode: bool,
         unzoomed_roads_and_intersections: bool,
@@ -326,7 +284,6 @@ impl App {
             Circle::new(pt, Distance::meters(3.0)).get_bounds(),
             ctx.prerender,
             &mut cache,
-            source,
             show_objs,
         );
         objects.reverse();
@@ -377,7 +334,6 @@ impl App {
         bounds: Bounds,
         prerender: &Prerender,
         agents: &'a mut AgentCache,
-        source: &dyn GetDrawAgents,
         show_objs: &dyn ShowObject,
     ) -> Vec<&'a (dyn Renderable + 'a)> {
         let map = &self.primary.map;
@@ -442,7 +398,7 @@ impl App {
         // Expand all of the Traversables into agents, populating the cache if needed.
         {
             for on in &agents_on {
-                agents.populate_if_needed(*on, map, source, &self.cs, prerender);
+                agents.populate_if_needed(*on, map, &self.primary.sim, &self.cs, prerender);
             }
         }
 

game/src/common/minimap.rs
@@ -284,14 +284,7 @@ impl Minimap {
         }
 
         let mut cache = app.primary.draw_map.agents.borrow_mut();
-        cache.draw_unzoomed_agents(
-            g,
-            &app.primary.sim,
-            &app.primary.map,
-            &app.unzoomed_agents,
-            app.opts.debug_all_agents,
-            &app.cs,
-        );
+        cache.draw_unzoomed_agents(g, app);
 
         // The cursor
         let (x1, y1) = {

game/src/devtools/story.rs
@@ -1,7 +1,6 @@
 use serde::{Deserialize, Serialize};
 
 use geom::{Distance, LonLat, PolyLine, Polygon, Pt2D, Ring};
-use sim::DontDrawAgents;
 use widgetry::{
     lctrl, Btn, Choice, Color, Drawable, EventCtx, GeomBatch, GfxCtx, HorizontalAlignment, Key,
     Line, Outcome, Panel, RewriteColor, Text, VerticalAlignment, Widget,
@@ -272,7 +271,7 @@ impl State for StoryMapEditor {
     fn draw(&self, g: &mut GfxCtx, app: &App) {
         let mut opts = DrawOptions::new();
         opts.label_buildings = true;
-        app.draw(g, opts, &DontDrawAgents {}, &ShowEverything::new());
+        app.draw(g, opts, &ShowEverything::new());
 
         match self.mode {
             Mode::PlacingMarker => {

game/src/edit/cluster_traffic_signals.rs
@@ -72,7 +72,7 @@ impl State for ClusterTrafficSignalEditor {
             let mut opts = DrawOptions::new();
             opts.suppress_traffic_signal_details
                 .extend(self.members.clone());
-            app.draw(g, opts, &app.primary.sim, &ShowEverything::new());
+            app.draw(g, opts, &ShowEverything::new());
         }
 
         let mut batch = GeomBatch::new();

commit bc856a4c2b89bcb3a78606f187bee8f079397eed

Fix the signal demand dash, broken by 1906fb0913e53f18b6cf2cdb623a830b789e045b. And enable warp to ID for non-dev mode too.

game/src/common/mod.rs
@@ -51,7 +51,7 @@ impl CommonState {
         if ctx.input.pressed(lctrl(Key::S)) {
             app.opts.dev = !app.opts.dev;
         }
-        if app.opts.dev && ctx.input.pressed(lctrl(Key::J)) {
+        if ctx.input.pressed(lctrl(Key::J)) {
             return Some(Transition::Push(warp::DebugWarp::new(ctx)));
         }
 

game/src/sandbox/dashboards/traffic_signals.rs
@@ -25,13 +25,14 @@ pub struct TrafficSignalDemand {
 
 impl TrafficSignalDemand {
     pub fn new(ctx: &mut EventCtx, app: &mut App) -> Box<dyn State> {
+        let all_demand = ctx.loading_screen("predict all demand", |_, timer| {
+            Demand::all_demand(app, timer)
+        });
+
         app.primary.current_selection = None;
         assert!(app.primary.suspended_sim.is_none());
         app.primary.suspended_sim = Some(app.primary.clear_sim());
 
-        let all_demand = ctx.loading_screen("predict all demand", |_, timer| {
-            Demand::all_demand(app, timer)
-        });
         let hour = Time::START_OF_DAY;
         let draw_all = Demand::draw_demand(ctx, app, &all_demand, hour);
         Box::new(TrafficSignalDemand {