8c5a6478d1ddad0999b1298e72d2ee13e9dad568
dabreegster <dabreegster@users.noreply.github.com>
2020-10-16 17:33:14 +0000

    deploy: 491f4bbffe9fb51201080bb9cdf725fae77028c0

---
+++ rustdoc/implementors/abstutil/collections/trait.IndexableKey.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["sim"] = [{"text":"impl IndexableKey for CarID","synthetic":false,"types":[]},{"text":"impl IndexableKey for PedestrianID","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ rustdoc/implementors/core/ops/index/trait.Index.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["abstutil"] = [{"text":"impl&lt;K:&nbsp;IndexableKey, V, '_&gt; Index&lt;&amp;'_ K&gt; for FixedMap&lt;K, V&gt;","synthetic":false,"types":[]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
