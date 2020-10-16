a71b9265415da47fc9418a95399451e56bc481e2
dabreegster <dabreegster@users.noreply.github.com>
2020-10-16 19:02:48 +0000

    deploy: 06f088afcc55fc4fd35047cdbb2719ae6c1105c7

--- rustdoc/abstutil/abst_data/sidebar-items.js
+++ rustdoc/abstutil/abst_data/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Entry","A single file"],["Manifest",""]]});
+initSidebarItems({"struct":[["Entry","A single file"],["Manifest","A list of all canonical data files for A/B Street that're uploaded somewhere. The file formats are tied to the latest version of the git repo. Players use the updater crate to sync these files with local copies."]]});
