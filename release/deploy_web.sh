ba9d14102fc99399752bcb9f7ce0aa11320202c5
Dustin Carlino <dabreegster@gmail.com>
2020-11-02 12:30:56 -0800

    Set the wasm MIME header in S3, to speed up initial web loading

--- release/deploy_web.sh
+++ release/deploy_web.sh
@@ -10,4 +10,14 @@ ln -s ~/Dropbox/abstreet_data/data/system/ system
 aws s3 sync --delete --exclude '*/osm_demo/*' . s3://abstreet
 # Undo that symlink swap
 git checkout system
+# Set the content type for .wasm files, to speed up how browsers load them
+aws s3 cp \
+       s3://abstreet/ \
+       s3://abstreet/ \
+       --exclude '*' \
+       --include '*.wasm' \
+       --no-guess-mime-type \
+       --content-type="application/wasm" \
+       --metadata-directive="REPLACE" \
+       --recursive
 echo "Have the appropriate amount of fun: http://abstreet.s3-website.us-east-2.amazonaws.com/"
