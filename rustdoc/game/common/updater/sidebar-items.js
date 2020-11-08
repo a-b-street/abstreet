850a1936f2fbcf620ed9f1794ad8c2731de6ec1c
dabreegster <dabreegster@users.noreply.github.com>
2020-11-08 02:57:47 +0000

    deploy: 1b4e25e12fa21fab3ea7149e33a29282054730dd

--- rustdoc/game/common/updater/sidebar-items.js
+++ rustdoc/game/common/updater/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["prettyprint_bytes",""],["size_per_city",""]],"struct":[["Picker",""]]});
+initSidebarItems({"constant":[["LATEST_RELEASE",""]],"fn":[["download",""],["prettyprint_bytes",""],["size_per_city",""],["sync",""]],"struct":[["Picker",""]]});
