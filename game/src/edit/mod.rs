a-b-street/abstreet

commit e3b9c425065f6b928d59614c7567ca29fca98ac0

Refactor: panel.replace() will set the same name on the new element

game/src/edit/mod.rs
@@ -432,40 +432,29 @@ impl SaveEdits {
 
     fn recalc_btn(&mut self, ctx: &mut EventCtx, app: &App) {
         if self.current_name.is_empty() {
-            self.panel.replace(
-                ctx,
-                "save",
-                Btn::text_bg2("Save").inactive(ctx).named("save"),
-            );
             self.panel
-                .replace(ctx, "warning", Text::new().draw(ctx).named("warning"));
+                .replace(ctx, "save", Btn::text_bg2("Save").inactive(ctx));
+            self.panel.replace(ctx, "warning", Text::new().draw(ctx));
         } else if abstutil::file_exists(abstutil::path_edits(
             app.primary.map.get_name(),
             &self.current_name,
         )) {
-            self.panel.replace(
-                ctx,
-                "save",
-                Btn::text_bg2("Save").inactive(ctx).named("save"),
-            );
+            self.panel
+                .replace(ctx, "save", Btn::text_bg2("Save").inactive(ctx));
             self.panel.replace(
                 ctx,
                 "warning",
                 Line("A proposal with this name already exists")
                     .fg(Color::hex("#FF5E5E"))
-                    .draw(ctx)
-                    .named("warning"),
+                    .draw(ctx),
             );
         } else {
             self.panel.replace(
                 ctx,
                 "save",
-                Btn::text_bg2("Save")
-                    .build_def(ctx, Key::Enter)
-                    .named("save"),
+                Btn::text_bg2("Save").build_def(ctx, Key::Enter),
             );
-            self.panel
-                .replace(ctx, "warning", Text::new().draw(ctx).named("warning"));
+            self.panel.replace(ctx, "warning", Text::new().draw(ctx));
         }
     }
 }

game/src/sandbox/gameplay/freeform.rs
@@ -258,9 +258,7 @@ impl AgentSpawner {
             spawner.panel.replace(
                 ctx,
                 "instructions",
-                "Click a building or border to specify end"
-                    .draw_text(ctx)
-                    .named("instructions"),
+                "Click a building or border to specify end".draw_text(ctx),
             );
         }
         Box::new(spawner)
@@ -334,15 +332,10 @@ impl State<App> for AgentSpawner {
                         self.panel.replace(
                             ctx,
                             "instructions",
-                            "Click a building or border to specify end"
-                                .draw_text(ctx)
-                                .named("instructions"),
+                            "Click a building or border to specify end".draw_text(ctx),
                         );
-                        self.panel.replace(
-                            ctx,
-                            "Confirm",
-                            Btn::text_fg("Confirm").inactive(ctx).named("Confirm"),
-                        );
+                        self.panel
+                            .replace(ctx, "Confirm", Btn::text_fg("Confirm").inactive(ctx));
                     }
                 }
             }
@@ -377,9 +370,7 @@ impl State<App> for AgentSpawner {
                 self.panel.replace(
                     ctx,
                     "instructions",
-                    "Click a building or border to specify end"
-                        .draw_text(ctx)
-                        .named("instructions"),
+                    "Click a building or border to specify end".draw_text(ctx),
                 );
             } else if self.source.is_some() && self.source != Some(hovering.clone()) {
                 if self
@@ -412,9 +403,7 @@ impl State<App> for AgentSpawner {
                     self.panel.replace(
                         ctx,
                         "instructions",
-                        "Confirm the trip settings"
-                            .draw_text(ctx)
-                            .named("instructions"),
+                        "Confirm the trip settings".draw_text(ctx),
                     );
                     self.panel.replace(
                         ctx,

widgetry/src/widgets/panel.rs
@@ -454,8 +454,13 @@ impl Panel {
         ctx.no_op_event(true, |ctx| assert_eq!(self.event(ctx), Outcome::Nothing));
     }
 
-    /// All margins/padding/etc from the previous widget are retained.
+    /// All margins/padding/etc from the previous widget are retained. The ID is set on the new
+    /// widget; no need to do that yourself.
     pub fn replace(&mut self, ctx: &mut EventCtx, id: &str, mut new: Widget) {
+        if let Some(ref new_id) = new.id {
+            assert_eq!(id, new_id);
+        }
+        new = new.named(id);
         let old = self.top_level.find_mut(id).unwrap();
         new.layout.style = old.layout.style;
         *old = new;

game/src/debug/mod.rs
@@ -105,8 +105,7 @@ impl DebugMode {
                 abstutil::prettyprint_usize(n)
             )));
         }
-        self.panel
-            .replace(ctx, "current info", txt.draw(ctx).named("current info"));
+        self.panel.replace(ctx, "current info", txt.draw(ctx));
     }
 }
 

game/src/debug/polygons.rs
@@ -82,9 +82,7 @@ impl State<App> for PolygonDebugger {
         self.panel.replace(
             ctx,
             "pointer",
-            format!("{} {}/{}", self.noun, self.idx + 1, self.items.len())
-                .draw_text(ctx)
-                .named("pointer"),
+            format!("{} {}/{}", self.noun, self.idx + 1, self.items.len()).draw_text(ctx),
         );
 
         Transition::Keep

game/src/devtools/collisions.rs
@@ -261,9 +261,7 @@ impl State<App> for CollisionsViewer {
             } else {
                 Dataviz::aggregated(ctx, app, &self.data, indices)
             };
-            let count = format!("{} collisions", prettyprint_usize(count))
-                .draw_text(ctx)
-                .named("count");
+            let count = format!("{} collisions", prettyprint_usize(count)).draw_text(ctx);
             self.panel.replace(ctx, "count", count);
         }
 

game/src/devtools/kml.rs
@@ -221,9 +221,7 @@ impl State<App> for ViewKML {
                 self.panel.replace(
                     ctx,
                     "matches",
-                    format!("Query matches {} objects", cnt)
-                        .draw_text(ctx)
-                        .named("matches"),
+                    format!("Query matches {} objects", cnt).draw_text(ctx),
                 );
             }
             _ => {}

game/src/devtools/mapping.rs
@@ -254,12 +254,12 @@ impl State<App> for ParkingMapper {
                             txt.add(Line(format!("{} = {}", k, v)).secondary());
                         }
                     }
-                    self.panel.replace(ctx, "info", txt.draw(ctx).named("info"));
+                    self.panel.replace(ctx, "info", txt.draw(ctx));
                 }
             } else if self.selected.is_some() {
                 self.selected = None;
                 self.panel
-                    .replace(ctx, "info", "Select a road".draw_text(ctx).named("info"));
+                    .replace(ctx, "info", "Select a road".draw_text(ctx));
             }
         }
         if self.selected.is_some() && app.per_obj.left_click(ctx, "map parking") {

commit 3583d068a66c53f0918578464a2691e8ed09be0c

Fix a bug introduced in e3b9c425065f6b928d59614c7567ca29fca98ac0 on the proposal saver, and allow blocking-the-box on another Ballard intersection

game/src/edit/mod.rs
@@ -417,7 +417,7 @@ impl SaveEdits {
                     } else {
                         Widget::nothing()
                     },
-                    Btn::text_bg2("Save").inactive(ctx).named("save"),
+                    Btn::text_bg2("Save").inactive(ctx),
                 ])
                 .align_right(),
             ]))
@@ -433,14 +433,14 @@ impl SaveEdits {
     fn recalc_btn(&mut self, ctx: &mut EventCtx, app: &App) {
         if self.current_name.is_empty() {
             self.panel
-                .replace(ctx, "save", Btn::text_bg2("Save").inactive(ctx));
+                .replace(ctx, "Save", Btn::text_bg2("Save").inactive(ctx));
             self.panel.replace(ctx, "warning", Text::new().draw(ctx));
         } else if abstutil::file_exists(abstutil::path_edits(
             app.primary.map.get_name(),
             &self.current_name,
         )) {
             self.panel
-                .replace(ctx, "save", Btn::text_bg2("Save").inactive(ctx));
+                .replace(ctx, "Save", Btn::text_bg2("Save").inactive(ctx));
             self.panel.replace(
                 ctx,
                 "warning",
@@ -451,7 +451,7 @@ impl SaveEdits {
         } else {
             self.panel.replace(
                 ctx,
-                "save",
+                "Save",
                 Btn::text_bg2("Save").build_def(ctx, Key::Enter),
             );
             self.panel.replace(ctx, "warning", Text::new().draw(ctx));

game/src/sandbox/gameplay/freeform.rs
@@ -252,7 +252,7 @@ impl AgentSpawner {
                     "Number of trips:".draw_text(ctx),
                     Spinner::new(ctx, (1, 1000), 1).named("number"),
                 ]),
-                Btn::text_fg("Confirm").inactive(ctx).named("Confirm"),
+                Btn::text_fg("Confirm").inactive(ctx),
             ]))
             .aligned(HorizontalAlignment::Right, VerticalAlignment::Top)
             .build(ctx),

sim/src/mechanics/intersection.rs
@@ -840,4 +840,5 @@ fn allow_block_the_box(osm_node_id: i64) -> bool {
         || osm_node_id == 3393025729
         || osm_node_id == 59995197
         || osm_node_id == 53077575
+        || osm_node_id == 4273547929
 }

widgetry/src/widgets/button.rs
@@ -481,26 +481,33 @@ impl BtnBuilder {
 
     pub fn inactive(self, ctx: &EventCtx) -> Widget {
         match self {
-            BtnBuilder::TextFG(_, txt, _) => txt
+            BtnBuilder::TextFG(label, txt, _) => txt
                 .change_fg(Color::grey(0.5))
                 .draw(ctx)
                 .container()
                 .padding(8)
-                .outline(2.0, Color::WHITE),
+                .outline(2.0, Color::WHITE)
+                .named(label),
             // TODO This'll only work reasonably for text_bg2
             BtnBuilder::TextBG {
                 text,
                 unselected_bg_color,
+                label,
                 ..
             } => {
                 assert_eq!(unselected_bg_color, Color::WHITE);
-                text.draw(ctx).container().padding(15).bg(Color::grey(0.7))
+                text.draw(ctx)
+                    .container()
+                    .padding(15)
+                    .bg(Color::grey(0.7))
+                    .named(label)
             }
-            BtnBuilder::PlainText { txt, .. } => txt
+            BtnBuilder::PlainText { txt, label, .. } => txt
                 .change_fg(Color::grey(0.5))
                 .draw(ctx)
                 .container()
-                .padding(8),
+                .padding(8)
+                .named(label),
             _ => panic!("Can't use inactive on this kind of button"),
         }
     }

widgetry/src/widgets/panel.rs
@@ -461,7 +461,10 @@ impl Panel {
             assert_eq!(id, new_id);
         }
         new = new.named(id);
-        let old = self.top_level.find_mut(id).unwrap();
+        let old = self
+            .top_level
+            .find_mut(id)
+            .expect(&format!("Panel doesn't have {}", id));
         new.layout.style = old.layout.style;
         *old = new;
         self.recompute_layout(ctx, true);