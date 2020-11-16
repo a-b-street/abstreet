617dff01d233b1071b77faebe827c7d7ec14b3d3
dabreegster <dabreegster@users.noreply.github.com>
2020-11-16 01:29:15 +0000

    deploy: d3aa81d2d2ffddf1287644ec34271444b0f7fe77

---
+++ rustdoc/sim/recorder/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["TrafficRecorder","Records trips beginning and ending at a specified set of intersections. This can be used to capture and reproduce behavior in a gridlock-prone chunk of the map, without simulating everything."]]});
