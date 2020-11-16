31c066a6ac73e182eb1a7a77efc75201026b2b6b
dabreegster <dabreegster@users.noreply.github.com>
2020-11-16 17:57:58 +0000

    deploy: 754749a5787cabba2a6b03bb5826b95f0371a127

--- rustdoc/sim/make/spawner/sidebar-items.js
+++ rustdoc/sim/make/spawner/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["TripSpec",""]],"struct":[["TripSpawner","This structure is created temporarily by a Scenario or to interactively spawn agents."]]});
+initSidebarItems({"enum":[["TripSpec",""]],"struct":[["TripSpawner","This structure is created temporarily by a Scenario or to interactively spawn agents."]],"type":[["TripSpawnPlan",""]]});
