a856692efb0854df4e8c72df192189910c1aff5a
dabreegster <dabreegster@users.noreply.github.com>
2020-11-19 22:31:55 +0000

    deploy: d33d051d47e37adaaeb5fe1515f9fa2f4efbd4ae

--- rustdoc/map_model/pathfind/dijkstra/sidebar-items.js
+++ rustdoc/map_model/pathfind/dijkstra/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["calc_path",""],["pathfind",""],["pathfind_avoiding_lanes",""],["pathfind_walking",""]]});
+initSidebarItems({"fn":[["build_graph_for_pedestrians",""],["build_graph_for_vehicles",""],["calc_path",""],["pathfind",""],["pathfind_avoiding_lanes",""],["pathfind_walking",""]]});
--- rustdoc/map_model/pathfind/walking/sidebar-items.js
+++ rustdoc/map_model/pathfind/walking/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["WalkingNode",""]],"fn":[["make_input_graph",""],["one_step_walking_path",""],["transit_input_graph",""],["walking_cost",""],["walking_path_to_steps",""]],"struct":[["SidewalkPathfinder",""]]});
+initSidebarItems({"enum":[["WalkingNode",""]],"fn":[["make_input_graph",""],["one_step_walking_path",""],["transit_input_graph",""],["walking_cost","The cost is time in seconds, rounded to a usize"],["walking_path_to_steps",""]],"struct":[["SidewalkPathfinder",""]]});
