8b439b2339b055f10bf53f97bb3d338a721e6245
dabreegster <dabreegster@users.noreply.github.com>
2020-11-20 18:44:52 +0000

    deploy: 308eb90956be7d963f308957168bf0e91a1c504c

--- rustdoc/game/devtools/kml/sidebar-items.js
+++ rustdoc/game/devtools/kml/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["RADIUS",""],["THICKNESS",""]],"fn":[["make_object",""],["make_query",""]],"struct":[["Object",""],["ViewKML",""]]});
+initSidebarItems({"constant":[["RADIUS",""],["THICKNESS",""]],"fn":[["load_objects","Loads and clips objects to the current map. Also returns the dataset name."],["make_object",""],["make_query",""]],"struct":[["Object",""],["ViewKML",""]]});
