6eb037eb4d2f3dc7b0a4bd26c72d5597bd0d4a64
dabreegster <dabreegster@users.noreply.github.com>
2020-11-21 21:04:35 +0000

    deploy: d8b1df597570a2306b7c4bdd69c4dbd6434fce0c

--- rustdoc/game/devtools/fifteen_min/sidebar-items.js
+++ rustdoc/game/devtools/fifteen_min/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["build_panel",""],["draw_star",""]],"mod":[["isochrone",""]],"struct":[["Viewer","This is the UI state for exploring the isochrone/walkshed from a single building."]]});
+initSidebarItems({"fn":[["build_panel",""],["draw_star","Draw a star on the start building."]],"mod":[["isochrone",""]],"struct":[["Viewer","This is the UI state for exploring the isochrone/walkshed from a single building."]]});
