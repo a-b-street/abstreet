670c933aa48ae105381a3cce030bfe35192d0322
dabreegster <dabreegster@users.noreply.github.com>
2020-11-22 19:05:03 +0000

    deploy: d83133fa094897be9d1497007020d10cffedf45a

--- rustdoc/map_model/pathfind/driving/sidebar-items.js
+++ rustdoc/map_model/pathfind/driving/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Node",""]],"fn":[["driving_cost","Roughly equivalent to seconds"],["make_input_graph",""],["round",""]],"struct":[["VehiclePathfinder",""]]});
+initSidebarItems({"enum":[["Node",""]],"fn":[["driving_cost","Different unit based on constraints."],["make_input_graph",""],["round",""]],"struct":[["VehiclePathfinder",""]]});
