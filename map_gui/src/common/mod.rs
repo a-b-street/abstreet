fdeac3e87170602e5438e33e6052300cccd23e71
Dustin Carlino <dabreegster@gmail.com>
2020-11-23 15:39:32 -0800

    Make a SimpleMinimap for map_gui. Will refactor later.

--- map_gui/src/common/mod.rs
+++ map_gui/src/common/mod.rs
@@ -1,9 +1,11 @@
 pub use self::city_picker::CityPicker;
-pub use colors::{ColorDiscrete, ColorLegend, ColorNetwork, ColorScale, DivergingScale};
-pub use navigate::Navigator;
+pub use self::colors::{ColorDiscrete, ColorLegend, ColorNetwork, ColorScale, DivergingScale};
+pub use self::minimap::SimpleMinimap;
+pub use self::navigate::Navigator;
 
 mod city_picker;
 mod colors;
+mod minimap;
 mod navigate;
 #[cfg(not(target_arch = "wasm32"))]
 mod updater;
