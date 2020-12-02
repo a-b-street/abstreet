4eea22db597e51b0feb032cb67a25f686692c4e8
dabreegster <dabreegster@users.noreply.github.com>
2020-12-02 17:01:01 +0000

    deploy: 69b14e444f5600ec92f14c9680bcbf00973df1fc

--- rustdoc/abstutil/io_native/sidebar-items.js
+++ rustdoc/abstutil/io_native/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["delete_file","Idempotent"],["file_exists",""],["list_dir","Returns full paths"],["maybe_read_binary",""],["maybe_write_binary",""],["maybe_write_json",""],["slurp_file",""],["write_binary",""],["write_json",""]],"struct":[["FileWithProgress",""]]});
+initSidebarItems({"fn":[["delete_file","Idempotent"],["file_exists",""],["inner_slurp_file",""],["list_dir","Returns full paths"],["maybe_read_binary",""],["maybe_write_binary",""],["maybe_write_json",""],["slurp_file",""],["write_binary",""],["write_json",""]],"struct":[["FileWithProgress",""]]});
