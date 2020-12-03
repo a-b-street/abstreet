86bfe71f7acb1e6a1b791bae2cad21b7f2feddb3
dabreegster <dabreegster@users.noreply.github.com>
2020-12-03 23:26:53 +0000

    deploy: 12d0294692f1f7a1693c711929bbc124ba56d0ed

--- rustdoc/widgetry/canvas/sidebar-items.js
+++ rustdoc/widgetry/canvas/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["DRAG_THRESHOLD",""],["PANNING_THRESHOLD",""],["PAN_SPEED",""]],"enum":[["HorizontalAlignment",""],["VerticalAlignment",""]],"struct":[["CameraState",""],["Canvas",""]]});
+initSidebarItems({"constant":[["DRAG_THRESHOLD",""],["INSET",""],["PANNING_THRESHOLD",""],["PAN_SPEED",""]],"enum":[["HorizontalAlignment",""],["VerticalAlignment",""]],"struct":[["CameraState",""],["Canvas",""]]});
