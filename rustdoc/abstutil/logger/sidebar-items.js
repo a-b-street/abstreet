0bc7accf9c3820d45afe66a91f2ed5c8ca6e66cc
dabreegster <dabreegster@users.noreply.github.com>
2020-12-07 21:14:42 +0000

    deploy: 0e759aa93432c9f7b87cfc6272ad81f76d67ea2d

--- rustdoc/abstutil/logger/sidebar-items.js
+++ rustdoc/abstutil/logger/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Logger",""]]});
+initSidebarItems({"fn":[["setup","On native: uses env_logYou can adjust the log level without recompiling with the RUST_LOG env variable."]]});
