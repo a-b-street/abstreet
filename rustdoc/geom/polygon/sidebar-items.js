0809445eef48e61a9d27b36857efd72890f24f61
dabreegster <dabreegster@users.noreply.github.com>
2020-12-15 02:09:02 +0000

    deploy: 8b9b3ce2b1ac52e32247551c5c1b6e71c501768b

--- rustdoc/geom/polygon/sidebar-items.js
+++ rustdoc/geom/polygon/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["downsize",""],["from_geo",""],["from_multi",""],["to_geo",""]],"struct":[["Polygon",""],["Triangle",""]]});
+initSidebarItems({"fn":[["downsize",""],["from_multi",""],["to_geo",""]],"struct":[["Polygon",""],["Triangle",""]]});
