fa4bf4628950c5b0ecd25001a4816c67f2161de5
dabreegster <dabreegster@users.noreply.github.com>
2020-12-17 02:13:41 +0000

    deploy: e13dd74ab48d106593d832c93d0e10e7d86d0703

--- rustdoc/experiment/game/sidebar-items.js
+++ rustdoc/experiment/game/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["ACQUIRE_BOOST_RATE",""],["BOOST_SPEED_MULTIPLIER",""]],"struct":[["EnergylessArrow",""],["Game",""],["GameState",""],["MinimapController",""]]});
+initSidebarItems({"constant":[["ACQUIRE_BOOST_RATE",""],["BOOST_SPEED_MULTIPLIER",""],["HANGRY_SPEED_MULTIPLIER",""],["MAX_BOOST",""]],"struct":[["EnergylessArrow",""],["Game",""],["GameState",""],["MinimapController",""]]});
