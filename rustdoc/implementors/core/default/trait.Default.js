b438c014943051f8ac6f5aaa6df09a97d3358d5a
dabreegster <dabreegster@users.noreply.github.com>
2020-12-19 18:19:07 +0000

    deploy: 6642c6e1a9872a76378c4d2b904fe609fb23aebf

--- rustdoc/implementors/core/default/trait.Default.js
+++ rustdoc/implementors/core/default/trait.Default.js
@@ -1,8 +1,8 @@
 (function() {var implementors = {};
 implementors["abstutil"] = [{"text":"impl&lt;T:&nbsp;Ord + PartialEq + Clone&gt; Default for Counter&lt;T&gt;","synthetic":false,"types":[]}];
-implementors["experiment"] = [{"text":"impl Default for Music","synthetic":false,"types":[]}];
 implementors["geom"] = [{"text":"impl Default for Distance","synthetic":false,"types":[]},{"text":"impl Default for Duration","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;HgramValue&lt;T&gt;&gt; Default for Histogram&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["importer"] = [{"text":"impl Default for ImporterConfiguration","synthetic":false,"types":[]}];
 implementors["map_model"] = [{"text":"impl Default for MapEdits","synthetic":false,"types":[]}];
+implementors["santa"] = [{"text":"impl Default for Music","synthetic":false,"types":[]}];
 implementors["sim"] = [{"text":"impl Default for Analytics","synthetic":false,"types":[]},{"text":"impl Default for SimOptions","synthetic":false,"types":[]},{"text":"impl Default for AlertHandler","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
--- rustdoc/implementors/map_gui/tools/minimap/trait.MinimapControls.js
+++ rustdoc/implementors/map_gui/tools/minimap/trait.MinimapControls.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["experiment"] = [{"text":"impl MinimapControls&lt;SimpleApp&lt;Session&gt;&gt; for MinimapController","synthetic":false,"types":[]}];
 implementors["game"] = [{"text":"impl MinimapControls&lt;App&gt; for MinimapController","synthetic":false,"types":[]}];
 implementors["osm_viewer"] = [{"text":"impl MinimapControls&lt;SimpleApp&lt;()&gt;&gt; for MinimapController","synthetic":false,"types":[]}];
+implementors["santa"] = [{"text":"impl MinimapControls&lt;SimpleApp&lt;Session&gt;&gt; for MinimapController","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
