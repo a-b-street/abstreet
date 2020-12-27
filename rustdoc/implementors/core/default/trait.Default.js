a052a0ff1b98dccf315f3274f0d18bb1bd271dcd
dabreegster <dabreegster@users.noreply.github.com>
2020-12-27 04:29:35 +0000

    deploy: f8ac0fc96b77349c4cf5a92b33b5c9b1e20df0a1

--- rustdoc/implementors/core/default/trait.Default.js
+++ rustdoc/implementors/core/default/trait.Default.js
@@ -1,5 +1,5 @@
 (function() {var implementors = {};
-implementors["abstutil"] = [{"text":"impl&lt;T:&nbsp;Ord + PartialEq + Clone&gt; Default for Counter&lt;T&gt;","synthetic":false,"types":[]}];
+implementors["abstutil"] = [{"text":"impl&lt;K, V&gt; Default for MultiMap&lt;K, V&gt; <span class=\"where fmt-newline\">where<br>&nbsp;&nbsp;&nbsp;&nbsp;K: Ord + PartialEq + Clone,<br>&nbsp;&nbsp;&nbsp;&nbsp;V: Ord + PartialEq + Clone,&nbsp;</span>","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;Ord + PartialEq + Clone&gt; Default for Counter&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["geom"] = [{"text":"impl Default for Distance","synthetic":false,"types":[]},{"text":"impl Default for Duration","synthetic":false,"types":[]},{"text":"impl&lt;T:&nbsp;HgramValue&lt;T&gt;&gt; Default for Histogram&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["importer"] = [{"text":"impl Default for ImporterConfiguration","synthetic":false,"types":[]}];
 implementors["map_model"] = [{"text":"impl Default for MapEdits","synthetic":false,"types":[]}];
