110f440be2507c3356f9e78c4573513aec6ecf8a
dabreegster <dabreegster@users.noreply.github.com>
2021-01-09 23:03:31 +0000

    deploy: b1dda3628faa8bedcebaa6723a38eb288fe204f9

--- rustdoc/implementors/widgetry/app_state/trait.SharedAppState.js
+++ rustdoc/implementors/widgetry/app_state/trait.SharedAppState.js
@@ -1,6 +1,6 @@
 (function() {var implementors = {};
 implementors["game"] = [{"text":"impl SharedAppState for App","synthetic":false,"types":[]}];
 implementors["map_editor"] = [{"text":"impl SharedAppState for App","synthetic":false,"types":[]}];
-implementors["map_gui"] = [{"text":"impl&lt;T&gt; SharedAppState for SimpleApp&lt;T&gt;","synthetic":false,"types":[]}];
+implementors["map_gui"] = [{"text":"impl&lt;T:&nbsp;'static&gt; SharedAppState for SimpleApp&lt;T&gt;","synthetic":false,"types":[]}];
 implementors["widgetry_demo"] = [{"text":"impl SharedAppState for App","synthetic":false,"types":[]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
