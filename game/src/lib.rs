a-b-street/abstreet

commit b4e72debcfdf5f51503f68aca8d31ec84c91795d

Make the game crate also start by asynchronously loading any map, in the common case of starting with a map as an argument. #344
Now we can remove montlake.bin from the wasm also, bringing game down to
just 18MB!!!

game/src/lib.rs
@@ -3,10 +3,12 @@ extern crate anyhow;
 #[macro_use]
 extern crate log;
 
-use abstutil::CmdArgs;
+use abstio::MapName;
+use abstutil::{CmdArgs, Timer};
 use map_gui::options::Options;
-use sim::SimFlags;
-use widgetry::{EventCtx, State};
+use map_model::Map;
+use sim::{Sim, SimFlags};
+use widgetry::{EventCtx, State, Transition};
 
 use crate::app::{App, Flags};
 use crate::pregame::TitleScreen;
@@ -115,7 +117,84 @@ fn setup_app(
         && !flags.sim_flags.load.contains("player/save")
         && !flags.sim_flags.load.contains("/scenarios/")
         && maybe_mode.is_none();
-    let mut app = App::new(flags, opts, ctx, title);
+    let cs = map_gui::colors::ColorScheme::new(ctx, opts.color_scheme);
+
+    // SimFlags::load doesn't know how to do async IO, which we need on the web. But in the common
+    // case, all we're creating there is a map. If so, use the proper async interface.
+    if flags.sim_flags.load.contains("/maps/") {
+        // Get App created with a dummy blank map
+        let map = Map::blank();
+        let sim = Sim::new(&map, flags.sim_flags.opts.clone(), &mut Timer::throwaway());
+        let primary = crate::app::PerMap::map_loaded(
+            map,
+            sim,
+            flags,
+            &opts,
+            &cs,
+            ctx,
+            &mut Timer::throwaway(),
+        );
+        let app = App {
+            primary,
+            cs,
+            opts,
+            per_obj: crate::app::PerObjectActions::new(),
+            session: crate::app::SessionState::empty(),
+        };
+        let map_name = MapName::from_path(&app.primary.current_flags.sim_flags.load).unwrap();
+        let states = vec![map_gui::load::MapLoader::new(
+            ctx,
+            &app,
+            map_name,
+            Box::new(move |ctx, app| {
+                Transition::Clear(finish_app_setup(
+                    ctx,
+                    app,
+                    title,
+                    start_with_edits,
+                    maybe_mode,
+                    initialize_tutorial,
+                ))
+            }),
+        )];
+        (app, states)
+    } else {
+        // We're loading a savestate or a scenario or a RawMap. Do it with blocking IO. This won't
+        // work on the web.
+        // TODO Handle starting from a scenario; that's also a likely useful case for the web.
+        let primary = ctx.loading_screen("load map", |ctx, mut timer| {
+            assert!(flags.sim_flags.modifiers.is_empty());
+            let (map, sim, _) = flags.sim_flags.load(timer);
+            crate::app::PerMap::map_loaded(map, sim, flags, &opts, &cs, ctx, &mut timer)
+        });
+        let mut app = App {
+            primary,
+            cs,
+            opts,
+            per_obj: crate::app::PerObjectActions::new(),
+            session: crate::app::SessionState::empty(),
+        };
+        let states = finish_app_setup(
+            ctx,
+            &mut app,
+            title,
+            start_with_edits,
+            maybe_mode,
+            initialize_tutorial,
+        );
+        (app, states)
+    }
+}
+
+fn finish_app_setup(
+    ctx: &mut EventCtx,
+    app: &mut App,
+    title: bool,
+    start_with_edits: Option<String>,
+    maybe_mode: Option<GameplayMode>,
+    initialize_tutorial: bool,
+) -> Vec<Box<dyn State<App>>> {
+    app.primary.init_camera_for_loaded_map(ctx, !title);
 
     // Handle savestates
     let savestate = if app
@@ -135,14 +214,14 @@ fn setup_app(
     // these flags later, but we don't want to keep applying the same edits.
     if let Some(edits_name) = start_with_edits {
         // TODO Maybe loading screen
-        let mut timer = abstutil::Timer::new("apply initial edits");
+        let mut timer = Timer::new("apply initial edits");
         let edits = map_model::MapEdits::load(
             &app.primary.map,
             abstio::path_edits(app.primary.map.get_name(), &edits_name),
             &mut timer,
         )
         .unwrap();
-        crate::edit::apply_map_edits(ctx, &mut app, edits);
+        crate::edit::apply_map_edits(ctx, app, edits);
         app.primary
             .map
             .recalculate_pathfinding_after_edits(&mut timer);
@@ -150,15 +229,15 @@ fn setup_app(
     }
 
     if initialize_tutorial {
-        crate::sandbox::gameplay::Tutorial::initialize(ctx, &mut app);
+        crate::sandbox::gameplay::Tutorial::initialize(ctx, app);
     }
 
     let states: Vec<Box<dyn State<App>>> = if title {
-        vec![Box::new(TitleScreen::new(ctx, &mut app))]
+        vec![Box::new(TitleScreen::new(ctx, app))]
     } else {
         let mode = maybe_mode
             .unwrap_or_else(|| GameplayMode::Freeform(app.primary.map.get_name().clone()));
-        vec![SandboxMode::simple_new(ctx, &mut app, mode)]
+        vec![SandboxMode::simple_new(ctx, app, mode)]
     };
     if let Some(ss) = savestate {
         // TODO This is weird, we're left in Freeform mode with the wrong UI. Can't instantiate
@@ -166,7 +245,7 @@ fn setup_app(
         app.primary.sim = ss;
     }
 
-    (app, states)
+    states
 }
 
 #[cfg(target_arch = "wasm32")]

abstio/src/io_web.rs
@@ -12,16 +12,10 @@ use abstutil::{to_json, Timer};
 pub use crate::io::*;
 use crate::{path_player, Manifest};
 
-// Bring in everything from data/system/ matching one of the prefixes -- aka, no scenarios, and
-// only the smallest map. Everything else has to be dynamically loaded over HTTP.
-static SYSTEM_DATA: include_dir::Dir = include_dir::include_dir!(
-    "../data/system",
-    "assets/",
-    // don't need this
-    "-assets/music/",
-    "proposals/",
-    "seattle/maps/montlake.bin",
-);
+// Bring in all assets (except for music) and proposals. Everything else has to be dynamically
+// loaded over HTTP.
+static SYSTEM_DATA: include_dir::Dir =
+    include_dir::include_dir!("../data/system", "assets/", "-assets/music/", "proposals/",);
 
 // For file_exists and list_dir only, also check if the file is in the Manifest. The caller has to
 // know when to load this remotely, though.

game/src/app.rs
@@ -40,24 +40,6 @@ pub struct App {
 }
 
 impl App {
-    pub fn new(flags: Flags, opts: Options, ctx: &mut EventCtx, splash: bool) -> App {
-        let cs = ColorScheme::new(ctx, opts.color_scheme);
-
-        let primary = ctx.loading_screen("load map", |ctx, mut timer| {
-            assert!(flags.sim_flags.modifiers.is_empty());
-            let (map, sim, _) = flags.sim_flags.load(timer);
-            PerMap::map_loaded(map, sim, splash, flags, &opts, &cs, ctx, &mut timer)
-        });
-
-        App {
-            primary,
-            cs,
-            opts,
-            per_obj: PerObjectActions::new(),
-            session: SessionState::empty(),
-        }
-    }
-
     // TODO Should the prebaked methods be on primary along with the data?
     pub fn has_prebaked(&self) -> Option<(&MapName, &String)> {
         self.primary.prebaked.as_ref().map(|(m, s, _)| (m, s))
@@ -512,13 +494,13 @@ impl map_gui::AppLike for App {
         self.primary = PerMap::map_loaded(
             map,
             sim,
-            false,
             self.primary.current_flags.clone(),
             &self.opts,
             &self.cs,
             ctx,
             timer,
-        )
+        );
+        self.primary.init_camera_for_loaded_map(ctx, false);
     }
 
     fn draw_with_opts(&self, g: &mut GfxCtx, opts: DrawOptions) {
@@ -627,10 +609,9 @@ pub struct PerMap {
 }
 
 impl PerMap {
-    fn map_loaded(
+    pub fn map_loaded(
         map: Map,
         sim: Sim,
-        splash: bool,
         flags: Flags,
         opts: &Options,
         cs: &ColorScheme,
@@ -641,7 +622,7 @@ impl PerMap {
         let draw_map = DrawMap::new(ctx, &map, opts, cs, timer);
         timer.stop("draw_map");
 
-        let per_map = PerMap {
+        PerMap {
             map,
             draw_map,
             sim,
@@ -657,33 +638,32 @@ impl PerMap {
             suspended_sim: None,
             prebaked: None,
             scenario: None,
-        };
+        }
+    }
 
-        let mut rng = per_map.current_flags.sim_flags.make_rng();
-        let rand_focus_pt = per_map
+    pub fn init_camera_for_loaded_map(&mut self, ctx: &mut EventCtx, splash: bool) {
+        let mut rng = self.current_flags.sim_flags.make_rng();
+        let rand_focus_pt = self
             .map
             .all_buildings()
             .choose(&mut rng)
-            .and_then(|b| per_map.canonical_point(ID::Building(b.id)))
+            .and_then(|b| self.canonical_point(ID::Building(b.id)))
             .or_else(|| {
-                per_map
-                    .map
+                self.map
                     .all_lanes()
                     .choose(&mut rng)
-                    .and_then(|l| per_map.canonical_point(ID::Lane(l.id)))
+                    .and_then(|l| self.canonical_point(ID::Lane(l.id)))
             })
-            .unwrap_or_else(|| per_map.map.get_bounds().center());
+            .unwrap_or_else(|| self.map.get_bounds().center());
 
         if splash {
             ctx.canvas.center_on_map_pt(rand_focus_pt);
         } else {
-            if !CameraState::load(ctx, per_map.map.get_name()) {
+            if !CameraState::load(ctx, self.map.get_name()) {
                 info!("Couldn't load camera state, just focusing on an arbitrary building");
                 ctx.canvas.center_on_map_pt(rand_focus_pt);
             }
         }
-
-        per_map
     }
 
     /// Returns whatever was there

commit 9f72de94346077ca4a50ac7d10474ccfd50f28e8

Fix initial camera loading for game, broken in b4e72debcfdf5f51503f68aca8d31ec84c91795d. Booleans are hard.

game/src/lib.rs
@@ -194,7 +194,7 @@ fn finish_app_setup(
     maybe_mode: Option<GameplayMode>,
     initialize_tutorial: bool,
 ) -> Vec<Box<dyn State<App>>> {
-    app.primary.init_camera_for_loaded_map(ctx, !title);
+    app.primary.init_camera_for_loaded_map(ctx, title);
 
     // Handle savestates
     let savestate = if app