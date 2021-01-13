27456ae18bd8da76280552772244c16e117d64b5
dabreegster <dabreegster@users.noreply.github.com>
2021-01-13 19:39:58 +0000

    deploy: d49c6c5d7d501bdc318a4937361cadd2ed5c99ab

--- rustdoc/popdat/distribute_people/sidebar-items.js
+++ rustdoc/popdat/distribute_people/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["assign_people_to_houses",""]]});
+initSidebarItems({"fn":[["assign_people_to_houses",""],["distribute_population_to_homes","Starting from some number of total people living in a polygonal area, randomly distribute them to residential buildings within that area. Returns a list of homes with the number of residents in each."]]});
