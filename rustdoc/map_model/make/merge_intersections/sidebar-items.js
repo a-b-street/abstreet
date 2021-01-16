a6b1e9268675bf8e77f1f3e635d8df4ba5ab774b
dabreegster <dabreegster@users.noreply.github.com>
2021-01-16 18:43:54 +0000

    deploy: 322fb81a9d9cb43392c59de8c6cb6dd1291f19df

--- rustdoc/map_model/make/merge_intersections/sidebar-items.js
+++ rustdoc/map_model/make/merge_intersections/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["SHORT_ROAD_THRESHOLD",""]],"fn":[["merge_short_roads","Merge tiny \"roads\" that're actually just part of a complicated intersection. Returns all surviving intersections adjacent to one of these merged roads."]]});
+initSidebarItems({"fn":[["angle",""],["connects_dual_carriageway",""],["merge_short_roads","Merge tiny \"roads\" that're actually just part of a complicated intersection. Returns all surviving intersections adjacent to one of these merged roads."],["should_merge",""]]});
