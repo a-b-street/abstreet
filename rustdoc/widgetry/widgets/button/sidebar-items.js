857f17c0d9f2f1b9f9303b11f5741b6736df52b5
dabreegster <dabreegster@users.noreply.github.com>
2021-01-21 23:44:33 +0000

    deploy: 02e3235de2686d750f0568fa1f2c9d3578efe746

--- rustdoc/widgetry/widgets/button/sidebar-items.js
+++ rustdoc/widgetry/widgets/button/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["BtnBuilder",""]],"mod":[["geom_batch_stack",""]],"struct":[["Btn","A questionably named place to start creating buttons."],["Button",""],["ButtonBuilder",""],["ButtonStyle",""],["Image",""],["Label",""],["MultiButton",""]]});
+initSidebarItems({"mod":[["geom_batch_stack",""]],"struct":[["Button",""],["ButtonBuilder",""],["ButtonStyle",""],["Image",""],["Label",""],["MultiButton",""]]});
