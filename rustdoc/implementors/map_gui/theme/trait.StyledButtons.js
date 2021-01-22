1f57d3b67053ee2bec1d5b40c9a3bfb964841f66
dabreegster <dabreegster@users.noreply.github.com>
2021-01-22 16:02:52 +0000

    deploy: 2505d64e8b6976ec568bca8097d6e0a938894b53

--- rustdoc/implementors/map_gui/theme/trait.StyledButtons.js
+++
@@ -1,3 +0,0 @@
-(function() {var implementors = {};
-implementors["map_gui"] = [{"text":"impl&lt;'a&gt; StyledButtons&lt;'a&gt; for ColorScheme","synthetic":false,"types":[]}];
-if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
