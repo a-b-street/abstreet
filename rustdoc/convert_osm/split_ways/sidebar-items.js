2b6567803e3bdda8d04b538daa8f1e89cd913b61
dabreegster <dabreegster@users.noreply.github.com>
2021-01-25 18:19:05 +0000

    deploy: e7e8bb2ca0f6d2fba2cf0ff42b7d7bd407de99b5

--- rustdoc/convert_osm/split_ways/sidebar-items.js
+++ rustdoc/convert_osm/split_ways/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["dedupe_angles",""],["split_up_roads","Returns amenities and a mapping of all points to split road. (Some internal points on roads are removed, so this mapping isn't redundant.)"]]});
+initSidebarItems({"fn":[["dedupe_angles",""],["split_up_roads","Returns amenities and a mapping of all points to split road. (Some internal points on roads get removed in this call, so this mapping isn't redundant.)"]]});
