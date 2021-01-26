c4ea8a3b21599af6f14683457a19671760bbea3f
Bruce Brown <bruce_brown_1207@yahoo.com>
2021-01-26 14:50:23 -0800

    Add a template for lagging green
    Lagging green is variable. Crosswalks are also variable.
    convert_to_ped_scramble() is refactored to allow a call that doesn't promote yield to protected; as this could unintentionally extend a variable phase.

--- map_model/src/make/traffic_signals.rs
+++ map_model/src/make/traffic_signals.rs
@@ -1,4 +1,4 @@
-use std::collections::HashSet;
+use std::collections::{BTreeSet, HashSet};
 
 use geom::Duration;
 
@@ -64,6 +64,9 @@ pub fn get_possible_policies(
     }
     if let Some(ts) = degenerate(map, id) {
         results.push(("degenerate (2 roads)".to_string(), ts));
+    }
+    if let Some(ts) = lagging_green(map, id) {
+        results.push(("lagging green".to_string(), ts));
     }
     results.push(("stage per road".to_string(), stage_per_road(map, id)));
     results.push((
@@ -325,6 +328,375 @@ fn stage_per_road(map: &Map, i: IntersectionID) -> ControlTrafficSignal {
     ts
 }
 
+
+///
+/// Create a traffic signal which has a stage that is: protected straight, protected right, unprotected left, unprotected right on red.
+/// Followed by a variable stage that has protected left, unprotected right on red.
+/// With a last stage that is all-walk and variable.
+/// In some degenerate cases, usually with one or more one-way, this can reduce to stage per road
+fn lagging_green(map: &Map, id: IntersectionID) -> Option<ControlTrafficSignal> {
+
+    let mut ts = new(id, map);
+    if let Some(other) = three_way_three_stage(map, id) {
+        println!("using built-in 3-way");
+        ts.stages = other.stages;
+    } else if let Some(other) = four_way_four_stage(map, id) {
+        println!("using built-in 4-way");
+        ts.stages = other.stages;
+    }
+    ts.convert_to_ped_scramble_without_promotion();
+    if let Err(err) = ts.validate() {
+        println!("trying multi-way validation_error={}", err);
+        if let Some(other) = multi_way_stages(map, id) {
+            println!("using multi-way");
+            ts.stages = other.stages;
+            ts.convert_to_ped_scramble_without_promotion();
+        }
+    }
+    if let Err(err) = ts.validate() {
+        println!("multi-way validation_error={} ts={:#?}", err, ts);
+        ts = stage_per_road(map, id);
+        ts.convert_to_ped_scramble_without_promotion();
+    }
+    // remove stages which don't contain a protected route
+    ts.stages.retain(|s| !s.protected_movements.is_empty());
+    make_lagging_green_variable(&mut ts);
+    // determine if and stages can be merged
+    while let Some(merged_ts) = merge_stages(&ts) {
+        ts = merged_ts;
+    }
+
+    println!("ts={:#?}", ts);
+    // convert walk to variable
+    for mut s in ts.stages.iter_mut() {
+        let mut is_walk = true;
+        for m in &s.protected_movements {
+            is_walk &= m.crosswalk
+        }
+        if is_walk {
+            println!("converting walk stage to variable walk={:#?}", s);
+            match s.stage_type {
+                StageType::Fixed(d) => s.stage_type = StageType::Variable(d, Duration::const_seconds(1.0), Duration::const_seconds(1.0)),
+                _ => (),
+            }
+        }
+    }
+    return Some(ts);
+
+    fn merge_stages(ts: &ControlTrafficSignal) -> Option<ControlTrafficSignal> {
+        for s_src in &ts.stages {
+            // s_src is the stage we want to apply to the other stages
+            for s_dst in &ts.stages {
+                if s_src == s_dst {
+                    continue;
+                }
+                let mut merged_stage = s_dst.clone();
+                for m in &s_src.protected_movements {
+                    merged_stage.protected_movements.insert(m.clone());
+                }
+                let mut maybe_ts = ts.clone();
+                // insert at the head, keeping crosswalk last
+                maybe_ts.stages.insert(0, merged_stage);
+                if maybe_ts.validate().is_ok() {
+                    println!("can merge s_src={:#?} and s_dst={:#?}", s_src, s_dst);
+                    let mut stages: Vec<Stage> = Vec::new();
+                    for s in maybe_ts.stages {
+                        if s != *s_src && s != *s_dst {
+                            stages.push(s);
+                        }
+                    }
+                    maybe_ts.stages = stages;
+                    return Some(maybe_ts);
+                }
+            }
+        }
+        None
+    }
+
+    fn multi_way_stages(map: &Map, id: IntersectionID) -> Option<ControlTrafficSignal> {       
+        let mut ts = new(id, map);
+        let (mut right, mut left, straight, mut roads) = movements(&ts);
+        println!("right turns");
+        for id in &right {println!("from={} to={}", id.from.id.0, id.to.id.0);}
+        println!("left turns");
+        for id in &left {println!("from={} to={}", id.from.id.0, id.to.id.0);}
+        println!("straight turns");
+        for id in &straight {println!("from={} to={}", id.from.id.0, id.to.id.0);}
+        let (one_way, two_way) = straight_types(&straight);
+        for m in &one_way {
+            println!("stage");
+            println!("straight from={} to={}", m.from.id.0, m.to.id.0);
+            println!("right from={} to={:#?}", m.from.id.0, turns(&m.from.id, &right));
+            println!("left from={} to={:#?}", m.from.id.0, turns(&m.from.id, &left));
+            let mut stage = Stage::new();
+            stage.protected_movements.insert(m.clone());
+            for t in turns(&m.from.id, &right) {
+                stage.protected_movements.insert(t.clone());
+            }
+            for t in turns(&m.from.id, &left) {
+                stage.protected_movements.insert(t.clone());
+            }
+            add_stage(&mut ts, stage);
+            roads.remove(&m.from.id);
+        }
+        for (m1,m2) in &two_way {
+            println!("stage");
+            println!("straight from={} to={}", m1.from.id.0, m1.to.id.0);
+            println!("straight from={} to={}", m1.to.id.0, m1.from.id.0);
+            println!("right from={} to={:#?}", m1.from.id.0, turns(&m1.from.id, &right));
+            println!("left from={} to={:#?}", m1.from.id.0, turns(&m1.from.id, &left));
+            println!("right from={} to={:#?}", m1.to.id.0, turns(&m1.to.id, &right));
+            println!("left from={} to={:#?}", m1.to.id.0, turns(&m1.to.id, &left));
+    
+            let mut stage1 = Stage::new();
+            let mut stage2 = Stage::new();
+            stage1.protected_movements.insert(m1.clone());
+            stage1.protected_movements.insert(m2.clone());
+            for t in turns(&m1.from.id, &right) {
+                stage1.protected_movements.insert(t.clone());
+            }
+            for t in turns(&m2.from.id, &right) {
+                stage1.protected_movements.insert(t.clone());
+            }
+            for t in turns(&m1.from.id, &left) {
+                stage1.yield_movements.insert(t.clone());
+                stage2.protected_movements.insert(t.clone());
+            }
+            for t in turns(&m2.from.id, &left) {
+                stage1.yield_movements.insert(t.clone());
+                stage2.protected_movements.insert(t.clone());
+            }
+            if !stage1.protected_movements.is_empty() {
+                add_stage(&mut ts, stage1);
+    
+            }
+            if !stage2.protected_movements.is_empty() {
+                add_stage(&mut ts, stage2);
+            }
+            roads.remove(&m1.from.id);
+            roads.remove(&m2.from.id);
+        }
+
+        // At this point, we may be done
+        if !roads.is_empty() {
+            println!("roads={:#?}", roads);
+        }
+        let mut vec: Vec<_> = roads.into_iter().collect();
+        // we're going to treat the roads as if thery are straight. Otherwise,
+        // we'd end up with overlapping protected left turns
+        while let Some(r1) = vec.pop() {
+            let mut stage1 = Stage::new();
+            let mut stage2 = Stage::new();
+            if let Some(r2) = vec.pop() {
+               if let Some(m) = remove_movement(&r1, &r2, &mut right) {
+                    stage1.protected_movements.insert(m.clone());
+                } else if let Some(m) = remove_movement(&r1, &r2, &mut left) {
+                    stage1.protected_movements.insert(m.clone());        
+                }
+                if let Some(m) = remove_movement(&r2, &r1, &mut right) {
+                    stage1.protected_movements.insert(m.clone());
+                } else if let Some(m) = remove_movement(&r2, &r1, &mut left) {
+                    stage1.protected_movements.insert(m.clone());
+                }
+                for t in turns(&r1, &right) {
+                    stage1.protected_movements.insert(t.clone());
+                }
+                for t in turns(&r2, &right) {
+                    stage1.protected_movements.insert(t.clone());
+                }
+                for t in turns(&r1, &left) {
+                    stage1.yield_movements.insert(t.clone());
+                    stage2.protected_movements.insert(t.clone());
+                }
+                for t in turns(&r2, &left) {
+                    stage1.yield_movements.insert(t.clone());
+                    stage2.protected_movements.insert(t.clone());
+                }
+                if !stage1.protected_movements.is_empty() {
+                    add_stage(&mut ts, stage1);
+        
+                }
+                if !stage2.protected_movements.is_empty() {
+                    add_stage(&mut ts, stage2);
+                }
+            } else {
+                for t in turns(&r1, &right) {
+                    stage1.protected_movements.insert(t.clone());
+                }
+                for t in turns(&r1, &left) {
+                    stage1.protected_movements.insert(t.clone());
+                }
+                add_stage(&mut ts, stage1);
+            }
+        }
+        Some(ts)
+    }
+
+    fn add_stage(ts: &mut ControlTrafficSignal, stage: Stage) {
+        for s in &ts.stages {
+            if *s == stage { return }
+        }
+        ts.stages.push(stage)
+    }
+
+    fn turns(from: &RoadID, turns: &Vec<MovementID> ) -> Vec<MovementID> {
+        turns.iter().filter_map(|turn| if from.0 == turn.from.id.0 {
+            Some(turn.clone())
+        } else {
+            None
+        }
+        ).collect::<Vec<_>>()
+    }
+
+    fn remove_movement(from: &RoadID, to: &RoadID, turns: &mut Vec<MovementID>) -> Option<MovementID> {
+        let result = if let Some(m) = turns.iter().filter(|turn| from.0 == turn.from.id.0 && to.0 == turn.to.id.0).next() {
+            Some(m.clone())
+        } else { None };
+        if result.is_some() {
+            turns.retain(|turn| from.0 != turn.from.id.0 || to.0 != turn.to.id.0);
+        }
+        result
+    }
+
+    fn three_way_three_stage(map: &Map, i: IntersectionID) -> Option<ControlTrafficSignal> {
+        let roads = get_sorted_incoming_roads(i, map);
+        if roads.len() != 3 {
+            return None;
+        }
+        let mut ts = new(i, map);
+    
+        // Picture a T intersection. Use turn angles to figure out the "main" two roads.
+        let straight = ts
+            .movements
+            .values()
+            .find(|g| g.turn_type == TurnType::Straight)?;
+        let (north, south) = (straight.id.from.id, straight.id.to.id);
+        let east = roads
+            .into_iter()
+            .find(|r| *r != north && *r != south)
+            .unwrap();
+    
+        // Three-stage with protected lefts, right turn on red
+        make_stages(
+            &mut ts,
+            vec![
+                vec![
+                    (vec![north, south], TurnType::Straight, PROTECTED),
+                    (vec![north, south], TurnType::Right, PROTECTED),
+                    (vec![north, south], TurnType::Left, YIELD),
+                    (vec![east], TurnType::Right, YIELD),
+                ],
+                vec![
+                    (vec![north, south], TurnType::Left, PROTECTED),    
+                    (vec![east], TurnType::Right, YIELD),
+                ],
+                vec![
+                    (vec![east], TurnType::Straight, PROTECTED),
+                    (vec![east], TurnType::Right, PROTECTED),
+                    (vec![east], TurnType::Left, PROTECTED),
+                    (vec![north, south], TurnType::Right, YIELD),
+                ],
+            ],
+        );
+        Some(ts)
+    }
+    
+    fn four_way_four_stage(map: &Map, i: IntersectionID) -> Option<ControlTrafficSignal> {
+        let roads = get_sorted_incoming_roads(i, map);
+        if roads.len() != 4 {
+            println!("four_way_four_stage called with roads={}", roads.len());
+            return None;
+        }
+    
+        // Just to refer to these easily, label with directions. Imagine an axis-aligned four-way.
+        let (north, west, south, east) = (roads[0], roads[1], roads[2], roads[3]);
+    
+        // Four-stage with protected lefts, right turn on red (except for the protected lefts),
+        // turning cars yield to peds
+        let mut ts = new(i, map);
+        make_stages(
+            &mut ts,
+            vec![
+                vec![
+                    (vec![north, south], TurnType::Straight, PROTECTED),
+                    (vec![north, south], TurnType::Left, YIELD),
+                    (vec![north, south], TurnType::Right, PROTECTED),
+                    (vec![east, west], TurnType::Right, YIELD),
+                ],
+                vec![
+                    (vec![north, south], TurnType::Left, PROTECTED),
+                    (vec![east, west], TurnType::Right, YIELD),
+                ],
+                vec![
+                    (vec![east, west], TurnType::Straight, PROTECTED),
+                    (vec![east, west], TurnType::Left, YIELD),
+                    (vec![east, west], TurnType::Right, PROTECTED),
+                    (vec![north, south], TurnType::Right, YIELD),
+                ],
+                vec![
+                    (vec![east, west], TurnType::Left, PROTECTED),
+                    (vec![north, south], TurnType::Right, YIELD),
+                ],
+            ],
+        );
+        Some(ts)
+    }
+    
+    fn movements(ts: &ControlTrafficSignal) -> (Vec<MovementID>, Vec<MovementID>, Vec<MovementID>, BTreeSet<RoadID>) {
+        let mut right: Vec<MovementID> = Vec::new();
+        let mut left: Vec<MovementID> = Vec::new();
+        let mut straight: Vec<MovementID> = Vec::new();
+        let mut set: BTreeSet<RoadID> = BTreeSet::new();
+        
+        for (id, m) in &ts.movements {
+            if id.crosswalk { continue }
+            match m.turn_type {
+                TurnType::Right => right.push(id.clone()),
+                TurnType::Left => left.push(id.clone()),
+                TurnType::Straight => straight.push(id.clone()),
+                _ => (),
+            }
+            set.insert(id.from.id);
+        }
+        (right, left, straight, set)
+    }
+
+    fn straight_types(movements: &Vec<MovementID>) -> (Vec<MovementID>, Vec<(MovementID, MovementID)>) {
+        let mut one_way: Vec<MovementID> = Vec::new();
+        let mut two_way: Vec<(MovementID, MovementID)> = Vec::new();
+        for m in movements {
+            if let Some(other) = movements.iter().find(|&other| m.from.id == other.to.id && m.to.id == other.from.id) {
+                two_way.push((m.clone(), other.clone()));
+            } else {
+                one_way.push(m.clone());
+            }
+        }
+        (one_way, two_way)
+    }
+}
+
+fn make_lagging_green_variable(ts: &mut ControlTrafficSignal) {
+    let mut prev_stage: Option<&mut Stage> = None;
+    for (idx, stage) in ts.stages.iter_mut().enumerate() {
+        // Lagging green: if this stage's protected is in the prev as unprotected, make this stage optional.
+        if let Some(prev) = prev_stage {
+            let mut make_variable = true;
+            for m in &stage.protected_movements {
+                println!("protected: stage={} from={} to={} previous_contains={}", idx, m.from.id.0, m.to.id.0, prev.yield_movements.contains(m));
+                if !prev.yield_movements.contains(m) {
+                    make_variable = false;
+                }
+            }
+            if make_variable {
+                if let StageType::Fixed(_) = stage.stage_type {
+                    stage.stage_type = StageType::Variable(Duration::ZERO, Duration::const_seconds(10.0), Duration::const_seconds(20.0));
+                }
+            }
+        }
+        prev_stage = Some(stage);
+    }
+}
+
 // Add all possible protected movements to existing stages.
 fn expand_all_stages(ts: &mut ControlTrafficSignal) {
     for stage in ts.stages.iter_mut() {
@@ -343,6 +715,15 @@ fn make_stages(
     ts: &mut ControlTrafficSignal,
     stage_specs: Vec<Vec<(Vec<RoadID>, TurnType, bool)>>,
 ) {
+    // Don't filter unprotected only stages
+    make_stages_filtered(ts, stage_specs, false);
+}
+fn make_stages_filtered(
+    ts: &mut ControlTrafficSignal,
+    stage_specs: Vec<Vec<(Vec<RoadID>, TurnType, bool)>>,
+    filter_unprotected: bool,
+) 
+{
     for specs in stage_specs {
         let mut stage = Stage::new();
 
@@ -361,6 +742,10 @@ fn make_stages(
                     },
                 );
             }
+        }
+        // Filter out unprotected only stages if they happen.
+        if filter_unprotected && stage.protected_movements.is_empty() {
+            continue;
         }
 
         // Add in all compatible crosswalks. Specifying this in specs explicitly doesn't work when
