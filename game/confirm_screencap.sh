4c1d610e4f2428be84398c016e2b66681c723bf8
Dustin Carlino <dabreegster@gmail.com>
2021-02-11 15:09:06 -0800

    Restore screenshots for Great Kneighton

--- game/confirm_screencap.sh
+++ game/confirm_screencap.sh
@@ -6,6 +6,7 @@ map=$2;
 rm -rf ../data/input/${city}/screenshots/${map}.zip diff screens_before;
 cd screenshots/${city}/${map};
 zip ${map}.zip *;
+mkdir -p ../../../../data/input/${city}/screenshots/;
 mv ${map}.zip ../../../../data/input/${city}/screenshots/;
 cd ../../../;
 rm -rf screenshots/${city}/${map};
