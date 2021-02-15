7c3ced6401d3906bccc522e5275edec1bf9f2d41
Dustin Carlino <dabreegster@gmail.com>
2021-02-15 11:06:28 -0800

    Import 10 more actdev sites. Start a script for importing Robin's scenario JSON, but don't run it yet.

---
+++ importer/actdev_scenarios.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+# This script imports scenarios an actdev site that's already been imported.
+# It's run manually once per site.
+
+set -e
+
+SITE=$1
+if [ "$SITE" == "" ]; then
+	echo Missing args;
+	exit 1;
+fi
+CITY=`echo $SITE | sed -r 's/-/_/g'`
+
+wget https://raw.githubusercontent.com/cyipt/actdev/main/data-small/$SITE/scenario-base.json
+wget https://raw.githubusercontent.com/cyipt/actdev/main/data-small/$SITE/scenario-godutch.json
+
+cargo run --release --bin import_traffic -- --map=data/system/gb/$CITY/maps/center.bin --input=scenario-base.json --skip_problems
+cargo run --release --bin import_traffic -- --map=data/system/gb/$CITY/maps/center.bin --input=scenario-godutch.json --skip_problems
+rm -fv scenario-base.json scenario-godutch.json
