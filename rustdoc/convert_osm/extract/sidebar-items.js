685bc8a7dfcc7c5c0157a1593837135af80a3f90
dabreegster <dabreegster@users.noreply.github.com>
2021-02-22 02:39:59 +0000

    deploy: 6ceac88cbea505c2336456bdd695d5cb7e189c5d

--- rustdoc/convert_osm/extract/sidebar-items.js
+++ rustdoc/convert_osm/extract/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["extract_osm",""],["get_area_type",""],["get_bldg_amenities",""],["is_bldg",""],["is_road",""]],"struct":[["OsmExtract",""]]});
+initSidebarItems({"fn":[["extract_osm",""],["find_parking_aisles",""],["get_area_type",""],["get_bldg_amenities",""],["is_bldg",""],["is_road",""],["service_road_crosses_parking_lot",""]],"struct":[["OsmExtract",""]]});
