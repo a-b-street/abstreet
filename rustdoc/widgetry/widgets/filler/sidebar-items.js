a914c94e09450bb740d6cb12cd074cb8a2fb29a7
dabreegster <dabreegster@users.noreply.github.com>
2021-02-23 19:28:28 +0000

    deploy: 8bee0038082822140a2cf3db44c09055b2c5d88e

--- rustdoc/widgetry/widgets/filler/sidebar-items.js
+++ rustdoc/widgetry/widgets/filler/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Filler",""]]});
+initSidebarItems({"struct":[["Filler","Doesn't do anything by itself, just used for widgetsing. Something else reaches in, asks for the ScreenRectangle to use."]]});
