24f88d833e56e7189d3a9736cf930018c7958b44
dabreegster <dabreegster@users.noreply.github.com>
2021-02-25 21:14:31 +0000

    deploy: 92e0c5c0af58933dc21c33160358bb8f3ae44b17

--- rustdoc/updater/sidebar-items.js
+++ rustdoc/updater/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["MD5_BUF_READ_SIZE",""]],"fn":[["curl",""],["download",""],["generate_manifest",""],["just_compare",""],["main",""],["md5sum","Returns (checksum, uncompressed_size_bytes)"],["opt_into_all",""],["rm",""],["upload",""]]});
+initSidebarItems({"constant":[["MD5_BUF_READ_SIZE",""]],"fn":[["curl",""],["download",""],["generate_manifest",""],["just_compare",""],["main",""],["md5sum","Returns (checksum, uncompressed_size_bytes)"],["opt_into_all",""],["remove_empty_directories",""],["rm",""],["upload",""]]});
