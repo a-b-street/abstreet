48b51dadfbd2fe1ed624b270e4b17fe466dd99ad
dabreegster <dabreegster@users.noreply.github.com>
2021-03-01 20:52:35 +0000

    deploy: 99bf415edef5f0004a936652d4cbbb92b8d7a03f

--- rustdoc/augment_scenario/sidebar-items.js
+++ rustdoc/augment_scenario/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["add_lunch_trips",""],["add_return_trips",""],["main",""]]});
+initSidebarItems({"fn":[["add_lunch_trips",""],["add_return_trips",""],["main",""],["rand_duration",""]]});
