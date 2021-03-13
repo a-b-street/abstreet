ad81bd2879c07066c7103eb75010ec14963f760d
dabreegster <dabreegster@users.noreply.github.com>
2021-03-13 18:19:13 +0000

    deploy: 27a07b02a6c747d48e9b653e34b04d2450c4aa72

---
+++ rustdoc/clip_osm/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["clip",""],["main","Clips an .osm.pbf specified by `--pbf` using the Osmosis boundary polygon specified by `--clip`, writing the result as .osm.xml to `--out`. This is a simple Rust port of `osmconvert large_map.osm -B=clipping.poly --complete-ways -o=smaller_map.osm`."],["to_pt",""]]});
