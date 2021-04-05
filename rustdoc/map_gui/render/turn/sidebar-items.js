678e6639c8aaa5221b4571de1444eb5f18095f22
dabreegster <dabreegster@users.noreply.github.com>
2021-04-05 17:39:18 +0000

    deploy: b6d46db2ea4261a89728caced0608194b2fc1a0f

--- rustdoc/map_gui/render/turn/sidebar-items.js
+++ rustdoc/map_gui/render/turn/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["TURN_ICON_ARROW_LENGTH",""]],"fn":[["make_block_geom",""],["make_circle_geom",""]],"struct":[["DrawMovement",""],["DrawUberTurnGroup",""]]});
+initSidebarItems({"constant":[["TURN_ICON_ARROW_LENGTH",""]],"fn":[["make_circle_geom",""]],"struct":[["DrawMovement",""]]});
