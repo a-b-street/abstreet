4b1a206a43bc8b28c325b4afde8c24cabe89c11e
Dustin Carlino <dabreegster@gmail.com>
2021-04-04 17:15:38 -0700

    Start versioning the web apps weekly too

--- release/update_docs.sh
+++ release/update_docs.sh
@@ -17,7 +17,7 @@ perl -pi -e "s/${MAJOR}_${MINOR}_${OLD_PATCH}/${MAJOR}_${MINOR}_${NEW_PATCH}/g" 
 perl -pi -e "s/${MAJOR}\.${MINOR}\.${OLD_PATCH}/${MAJOR}\.${MINOR}\.${NEW_PATCH}/g" README.md ~/docs/book/src/howto/README.md ~/docs/book/src/side_projects/santa.md
 
 echo "Don't forget to:"
-echo "1) aws s3 cp --recursive s3://abstreet/dev/data/system s3://abstreet/${MAJOR}.${MINOR}.${NEW_PATCH}/data/system"
+echo "1) aws s3 cp --recursive --exclude 'data/input/*' s3://abstreet/dev/ s3://abstreet/${MAJOR}.${MINOR}.${NEW_PATCH}"
 echo "2) ./release/deploy_web.sh"
 echo "3) Post to r/abstreet"
 echo "4) Update map_gui/src/tools/updater.rs"
