a-b-street/abstreet

commit b8318917a83997a213857417d0e7df44fd16fcb9

Write to the proper data/input/shared directory for elevation. #82
It was always using the current directory, which breaks when running the
UI map importer from game/. [rebuild] [release]

convert_osm/src/elevation.rs
@@ -17,7 +17,7 @@ pub fn add_data(map: &mut RawMap, timer: &mut Timer) -> Result<()> {
 
     timer.start("run elevation_lookups");
     std::fs::create_dir_all("elevation_output")?;
-    std::fs::create_dir_all("data/input/shared/elevation")?;
+    std::fs::create_dir_all(abstio::path_shared_input("elevation"))?;
     let pwd = std::env::current_dir()?.display().to_string();
     // Because elevation_lookups has so many dependencies, just depend on Docker.
     let status = Command::new("docker")
@@ -32,8 +32,8 @@ pub fn add_data(map: &mut RawMap, timer: &mut Timer) -> Result<()> {
         // our data/input/shared directory.
         .arg("--mount")
         .arg(format!(
-            "type=bind,source={}/data/input/shared/elevation,target=/elevation/data",
-            pwd
+            "type=bind,source={},target=/elevation/data",
+            abstio::path_shared_input("elevation")
         ))
         .arg("--mount")
         .arg(format!(

commit de20a74f3a28b3370319436d5655b5be6f13367d

Fix elevation imports, broken in b8318917a83997a213857417d0e7df44fd16fcb9. #82

convert_osm/src/elevation.rs
@@ -11,6 +11,8 @@ use map_model::raw::{OriginalRoad, RawMap};
 pub fn add_data(map: &mut RawMap, timer: &mut Timer) -> Result<()> {
     timer.start("add elevation data");
 
+    // TODO General problem: If we bail out early, the timer gets screwed up and later crashes. I
+    // think we need to start using scoped objects that call timer.stop() when dropped.
     timer.start("generate input");
     let ids = generate_input(map)?;
     timer.stop("generate input");
@@ -33,7 +35,8 @@ pub fn add_data(map: &mut RawMap, timer: &mut Timer) -> Result<()> {
         .arg("--mount")
         .arg(format!(
             "type=bind,source={},target=/elevation/data",
-            abstio::path_shared_input("elevation")
+            // Docker requires absolute paths
+            format!("{}/{}", pwd, abstio::path_shared_input("elevation"))
         ))
         .arg("--mount")
         .arg(format!(