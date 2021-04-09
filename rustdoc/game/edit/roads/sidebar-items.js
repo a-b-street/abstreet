334dae33ebc9ce28aa52bc001b1a903548e1cb26
dabreegster <dabreegster@users.noreply.github.com>
2021-04-09 21:44:55 +0000

    deploy: 711d1443231bb75570f9e8b1ccc493a90df20bff

---
+++ rustdoc/game/edit/roads/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"fn":[["lane_type_codes",""],["lanes_to_string",""],["prompt_for_lanes","Specify the lane types for a road using a text box. This is a temporary UI to start experimenting with widening roads. It'll be replaced by a real UI once the design is ready."],["string_to_lanes",""]]});
