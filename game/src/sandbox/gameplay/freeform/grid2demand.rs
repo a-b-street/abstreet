092ea198994bab54036e22aa8137dfb44d488825
Dustin Carlino <dabreegster@gmail.com>
2021-04-14 09:06:25 -0700

    Change the grid2demand file picker to be async. #605

--- game/src/sandbox/gameplay/freeform/grid2demand.rs
+++ game/src/sandbox/gameplay/freeform/grid2demand.rs
@@ -1,5 +1,6 @@
 // TODO This doesn't really belong in gameplay/freeform
 
+use map_gui::load::FutureLoader;
 use map_gui::tools::{find_exe_dir, RunCommand};
 use widgetry::EventCtx;
 
@@ -7,36 +8,49 @@ use crate::app::{App, Transition};
 use crate::sandbox::gameplay::GameplayMode;
 use crate::sandbox::SandboxMode;
 
-pub fn import(ctx: &mut EventCtx, app: &App) -> Transition {
-    // Blockingly run the file dialog. We could use a proper State and await the async file dialog,
-    // but this version seems to work fine!
-    if let Some(path) = rfd::FileDialog::new().pick_file() {
-        Transition::Replace(RunCommand::new(
-            ctx,
-            app,
-            vec![
-                format!("{}/import_grid2demand", find_exe_dir()),
-                format!("--map={}", app.primary.map.get_name().path()),
-                format!("--input={}", path.display()),
-            ],
-            Box::new(|_, app, success, _| {
-                if success {
-                    Transition::Replace(SandboxMode::simple_new(
-                        app,
-                        GameplayMode::PlayScenario(
-                            app.primary.map.get_name().clone(),
-                            "grid2demand".to_string(),
-                            Vec::new(),
-                        ),
-                    ))
-                } else {
-                    // The popup already explained the failure
-                    Transition::Keep
-                }
-            }),
-        ))
-    } else {
-        // The user didn't pick a file, so stay on the scenario picker
-        Transition::Keep
-    }
+pub fn import(ctx: &mut EventCtx) -> Transition {
+    Transition::Push(FutureLoader::<App, Option<String>>::new(
+        ctx,
+        Box::pin(async {
+            let result = rfd::AsyncFileDialog::new()
+                .pick_file()
+                .await
+                .map(|x| x.path().display().to_string());
+            let wrap: Box<dyn Send + FnOnce(&App) -> Option<String>> =
+                Box::new(move |_: &App| result);
+            Ok(wrap)
+        }),
+        "Waiting for a file to be chosen",
+        Box::new(|ctx, app, maybe_path| {
+            if let Ok(Some(path)) = maybe_path {
+                Transition::Replace(RunCommand::new(
+                    ctx,
+                    app,
+                    vec![
+                        format!("{}/import_grid2demand", find_exe_dir()),
+                        format!("--map={}", app.primary.map.get_name().path()),
+                        format!("--input={}", path),
+                    ],
+                    Box::new(|_, app, success, _| {
+                        if success {
+                            Transition::Replace(SandboxMode::simple_new(
+                                app,
+                                GameplayMode::PlayScenario(
+                                    app.primary.map.get_name().clone(),
+                                    "grid2demand".to_string(),
+                                    Vec::new(),
+                                ),
+                            ))
+                        } else {
+                            // The popup already explained the failure
+                            Transition::Keep
+                        }
+                    }),
+                ))
+            } else {
+                // The user didn't pick a file, so stay on the scenario picker
+                Transition::Pop
+            }
+        }),
+    ))
 }
