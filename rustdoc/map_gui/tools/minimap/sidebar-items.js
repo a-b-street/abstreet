5837a530c240b1629833ead63d3f480bf61f5255
dabreegster <dabreegster@users.noreply.github.com>
2021-04-16 02:34:42 +0000

    deploy: 0697e1f649c1c26e97f2e55378ec8966e503e2ab

--- rustdoc/map_gui/tools/minimap/sidebar-items.js
+++ rustdoc/map_gui/tools/minimap/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["Minimap",""]],"trait":[["MinimapControls","Customize the appearance and behavior of a minimap."]]});
+initSidebarItems({"static":[["MINIMAP_HEIGHT",""],["MINIMAP_WIDTH",""]],"struct":[["Minimap",""]],"trait":[["MinimapControls","Customize the appearance and behavior of a minimap."]]});
