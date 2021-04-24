97b41b9e19cd5f7b6073038e1f82b6de8c3dfa1b
Dustin Carlino <dabreegster@gmail.com>
2021-04-24 13:57:00 -0700

    Make the deploy_actdev script work again

--- release/deploy_actdev.sh
+++ release/deploy_actdev.sh
@@ -1,18 +1,16 @@
 #!/bin/bash
 # This is like deploy_web.sh, but just creates a directory for https://github.com/cyipt/actdev
+#
+# IMPORTANT: First run deploy_web.sh to actually build everything. This script
+# assumes web/build/dist is up-to-date.
 
+set -x
 set -e
 
-mkdir -p abst_actdev
-cd game
-wasm-pack build --release --target web -- --no-default-features --features wasm
-# Temporarily remove the symlink to the data directory
-rm -f pkg/system
-# Expand symlinks
-cp -Hv pkg/* ../abst_actdev
-# Restore the symlink
-git checkout pkg/system
-cd ..
+mkdir abst_actdev
+cp -Rv web/build/dist/abstreet* abst_actdev
+# We're not packaging multiple apps, so set the index.html
+mv abst_actdev/abstreet.html abst_actdev/index.html
 
 # Copy just what's needed from data
 mkdir -p abst_actdev/system/gb
@@ -22,7 +20,7 @@ for dir in allerton_bywater ashton_park aylesbury aylesham bailrigg bath_riversi
 	cp -Rv data/system/gb/$dir abst_actdev/system/gb
 done
 cp -Rv data/system/study_areas abst_actdev/system
-gzip `find abst_actdev/ | grep bin | xargs`
+gzip -v `find abst_actdev/ | grep bin | xargs`
 
 zip -r abst_actdev abst_actdev
 rm -rf abst_actdev
