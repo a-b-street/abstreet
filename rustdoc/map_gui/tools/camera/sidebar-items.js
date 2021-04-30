b7c15f9102827b93b0a617597d94d073c16e6762
dabreegster <dabreegster@users.noreply.github.com>
2021-04-30 19:48:35 +0000

    deploy: d3c035ffb3e6b4cee6eec8390281acfd74fa4470

--- rustdoc/map_gui/tools/camera/sidebar-items.js
+++ rustdoc/map_gui/tools/camera/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["CameraState","Represents the state of a widgetry Canvas."]]});
+initSidebarItems({"struct":[["CameraState","Represents the state of a widgetry Canvas."],["DefaultMap","Track the last map used, to resume next session."]]});
