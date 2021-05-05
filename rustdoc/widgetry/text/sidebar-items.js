d82c0cccdafc2989f35f9140bef1478244306214
dabreegster <dabreegster@users.noreply.github.com>
2021-05-05 20:58:58 +0000

    deploy: 8826af31cba342662694014e549322a727605339

--- rustdoc/widgetry/text/sidebar-items.js
+++ rustdoc/widgetry/text/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"constant":[["BG_COLOR",""],["DEFAULT_FONT",""],["DEFAULT_FONT_SIZE",""],["MAX_CHAR_WIDTH",""],["SCALE_LINE_HEIGHT",""]],"enum":[["Font",""]],"fn":[["Line",""],["render_line",""]],"struct":[["Text",""],["TextSpan",""]],"trait":[["TextExt",""]]});
+initSidebarItems({"constant":[["BG_COLOR",""],["DEFAULT_FONT",""],["DEFAULT_FONT_SIZE",""],["SCALE_LINE_HEIGHT",""]],"enum":[["Font",""]],"fn":[["Line",""],["render_line",""]],"struct":[["Text",""],["TextSpan",""]],"trait":[["TextExt",""]]});
