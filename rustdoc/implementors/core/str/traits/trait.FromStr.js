abd9c7faf0ff06386eaf7b298c369edc966c47a3
dabreegster <dabreegster@users.noreply.github.com>
2021-05-06 15:55:56 +0000

    deploy: 4080d0d1302e79a0a326c431ce664ce025ba67ec

---
+++ rustdoc/implementors/core/str/traits/trait.FromStr.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["map_model"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/str/traits/trait.FromStr.html\" title=\"trait core::str::traits::FromStr\">FromStr</a> for <a class=\"enum\" href=\"map_model/objects/building/enum.AmenityType.html\" title=\"enum map_model::objects::building::AmenityType\">AmenityType</a>","synthetic":false,"types":["map_model::objects::building::AmenityType"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
