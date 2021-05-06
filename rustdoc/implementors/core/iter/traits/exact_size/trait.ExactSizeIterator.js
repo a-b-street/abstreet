4d9cbaf7160f834426a5bf15ef2f8dbf7f4e65a0
dabreegster <dabreegster@users.noreply.github.com>
2021-05-06 16:04:07 +0000

    deploy: b29a613b427bd7d020a8008f58a22c295f333421

---
+++ rustdoc/implementors/core/iter/traits/exact_size/trait.ExactSizeIterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["map_model"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/iter/traits/exact_size/trait.ExactSizeIterator.html\" title=\"trait core::iter::traits::exact_size::ExactSizeIterator\">ExactSizeIterator</a> for <a class=\"struct\" href=\"map_model/objects/building/struct.AmenityTypeIter.html\" title=\"struct map_model::objects::building::AmenityTypeIter\">AmenityTypeIter</a>","synthetic":false,"types":["map_model::objects::building::AmenityTypeIter"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
---
+++ rustdoc/implementors/strum/trait.IntoEnumIterator.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["map_model"] = [{"text":"impl IntoEnumIterator for <a class=\"enum\" href=\"map_model/objects/building/enum.AmenityType.html\" title=\"enum map_model::objects::building::AmenityType\">AmenityType</a>","synthetic":false,"types":["map_model::objects::building::AmenityType"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
