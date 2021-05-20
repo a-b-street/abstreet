7f2776ecc8b5dad1a95f3ff6a22f1478b06a9c05
dabreegster <dabreegster@users.noreply.github.com>
2021-05-20 21:48:56 +0000

    deploy: ef907217415f0acb2d94480361e3a77ddc617914

--- rustdoc/geom/polygon/sidebar-items.js
+++ rustdoc/geom/polygon/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["downsize",""],["from_geojson_file_inner",""],["from_multi",""],["to_geo",""]],"struct":[["Polygon",""],["Triangle",""]]});
+initSidebarItems({"fn":[["downsize",""],["from_multi",""],["to_geo",""]],"struct":[["Polygon",""],["Triangle",""]]});
