c95a0b0f6417e1f256b1aecc449b9e6c1d60cd01
Dustin Carlino <dabreegster@gmail.com>
2021-05-25 07:31:22 -0700

    Handle another case for creating multiple left/right turns at an intersection. Use the new validation for connectivity at an intersection and give up on using OSM turn restrictions in some cases.
    39 problems in Montlake down to 33, but regressions on some of the LCing unit tests...

--- map_model/src/make/turns.rs
+++ map_model/src/make/turns.rs
@@ -18,82 +18,25 @@ pub fn make_all_turns(map: &Map, i: &Intersection) -> Vec<Turn> {
         i,
     ));
     let unique_turns = ensure_unique(raw_turns);
+    // Never allow turns that go against road-level turn restrictions; that upstream OSM data is
+    // usually not extremely broken.
+    let all_turns: Vec<Turn> = unique_turns
+        .into_iter()
+        .filter(|t| does_turn_pass_road_level_restrictions(t, i, map))
+        .collect();
 
-    let mut final_turns: Vec<Turn> = Vec::new();
-    let mut filtered_turns: HashMap<LaneID, Vec<Turn>> = HashMap::new();
-    for turn in unique_turns {
-        if !does_turn_pass_restrictions(&turn, i, map) {
-            continue;
-        }
-
-        if is_turn_allowed(&turn, map) {
-            final_turns.push(turn);
-        } else {
-            filtered_turns
-                .entry(turn.id.src)
-                .or_insert_with(Vec::new)
-                .push(turn);
-        }
-    }
-
-    // Make sure every incoming lane has a turn originating from it, and every outgoing lane has a
-    // turn leading to it.
-    let mut incoming_missing: HashSet<LaneID> = HashSet::new();
-    for l in &i.incoming_lanes {
-        if map.get_l(*l).lane_type.supports_any_movement() {
-            incoming_missing.insert(*l);
-        }
-    }
-    for t in &final_turns {
-        incoming_missing.remove(&t.id.src);
-    }
-    for (l, turns) in filtered_turns {
-        // Do turn restrictions orphan a lane?
-        if incoming_missing.contains(&l) {
-            // Restrictions on turn lanes may sometimes actually be more like change:lanes
-            // (https://wiki.openstreetmap.org/wiki/Key:change). Try to interpret them that way
-            // here, choosing one turn from a bunch of options.
-
-            // If all the turns go to a single road, then ignore the turn type.
-            let dst_r = map.get_l(turns[0].id.dst).parent;
-            let single_group: Vec<Turn> =
-                if turns.iter().all(|t| map.get_l(t.id.dst).parent == dst_r) {
-                    turns.clone()
-                } else {
-                    // Fall back to preferring all the straight turns
-                    turns
-                        .iter()
-                        .filter(|t| t.turn_type == TurnType::Straight)
-                        .cloned()
-                        .collect()
-                };
-            if !single_group.is_empty() {
-                // Just pick one, with the lowest lane-changing cost. Not using Turn's penalty()
-                // here, because
-                // 1) We haven't populated turns yet, so from_idx won't work
-                // 2) It counts from the right, but I think we actually want to count from the left
-                let best = single_group
-                    .into_iter()
-                    .min_by_key(|t| lc_penalty(t, map))
-                    .unwrap();
-                final_turns.push(best);
-                info!(
-                    "Restricted lane-changing on approach to turn lanes at {}",
-                    l
-                );
-            } else {
-                warn!("Turn restrictions broke {} outbound, so restoring turns", l);
-                final_turns.extend(turns);
-            }
-            incoming_missing.remove(&l);
-        }
-    }
-
-    final_turns = remove_merging_turns(map, final_turns, TurnType::Right);
-    final_turns = remove_merging_turns(map, final_turns, TurnType::Left);
-
+    // Try to use turn lane tags...
+    let filtered_turns: Vec<Turn> = all_turns
+        .clone()
+        .into_iter()
+        .filter(|t| does_turn_pass_lane_level_restrictions(t, map))
+        .collect();
+    // And remove merging left or right turns. If we wanted to remove the "lane-changing at
+    // intersections" behavior, we could do this for TurnType::Straight too.
+    let filtered_turns = remove_merging_turns(map, filtered_turns, TurnType::Right);
+    let mut filtered_turns = remove_merging_turns(map, filtered_turns, TurnType::Left);
     if i.merged {
-        final_turns.retain(|turn| {
+        filtered_turns.retain(|turn| {
             if turn.turn_type == TurnType::UTurn {
                 warn!("Removing u-turn from merged intersection: {}", turn.id);
                 false
@@ -103,24 +46,14 @@ pub fn make_all_turns(map: &Map, i: &Intersection) -> Vec<Turn> {
         });
     }
 
-    let mut outgoing_missing: HashSet<LaneID> = HashSet::new();
-    for l in &i.outgoing_lanes {
-        if map.get_l(*l).lane_type.supports_any_movement() {
-            outgoing_missing.insert(*l);
+    // But then see how all of that filtering affects lane connectivity.
+    match verify_vehicle_connectivity(&filtered_turns, i, map) {
+        Ok(()) => filtered_turns,
+        Err(err) => {
+            warn!("Not filtering turns. {}", err);
+            all_turns
         }
     }
-    for t in &final_turns {
-        outgoing_missing.remove(&t.id.dst);
-    }
-
-    if !incoming_missing.is_empty() || !outgoing_missing.is_empty() {
-        warn!(
-            "Turns for {} orphan some lanes. Incoming: {:?}, outgoing: {:?}",
-            i.id, incoming_missing, outgoing_missing
-        );
-    }
-
-    final_turns
 }
 
 fn ensure_unique(turns: Vec<Turn>) -> Vec<Turn> {
@@ -140,10 +73,10 @@ fn ensure_unique(turns: Vec<Turn>) -> Vec<Turn> {
     keep
 }
 
-fn is_turn_allowed(turn: &Turn, map: &Map) -> bool {
+fn does_turn_pass_lane_level_restrictions(turn: &Turn, map: &Map) -> bool {
     if let Some(types) = map
         .get_l(turn.id.src)
-        .get_turn_restrictions(map.get_parent(turn.id.src))
+        .get_lane_level_turn_restrictions(map.get_parent(turn.id.src))
     {
         types.contains(&turn.turn_type)
     } else {
@@ -151,7 +84,7 @@ fn is_turn_allowed(turn: &Turn, map: &Map) -> bool {
     }
 }
 
-fn does_turn_pass_restrictions(turn: &Turn, i: &Intersection, map: &Map) -> bool {
+fn does_turn_pass_road_level_restrictions(turn: &Turn, i: &Intersection, map: &Map) -> bool {
     if turn.between_sidewalks() {
         return true;
     }
@@ -362,43 +295,6 @@ fn from_pt(pt: Point2d<f64>) -> Pt2D {
     Pt2D::new(pt.x, pt.y)
 }
 
-fn lc_penalty(t: &Turn, map: &Map) -> isize {
-    let from = map.get_l(t.id.src);
-    let to = map.get_l(t.id.dst);
-
-    let from_idx = {
-        let mut cnt = 0;
-        let r = map.get_r(from.parent);
-        for (l, lt) in r.children(from.dir) {
-            if from.lane_type != lt {
-                continue;
-            }
-            cnt += 1;
-            if from.id == l {
-                break;
-            }
-        }
-        cnt
-    };
-
-    let to_idx = {
-        let mut cnt = 0;
-        let r = map.get_r(to.parent);
-        for (l, lt) in r.children(to.dir) {
-            if to.lane_type != lt {
-                continue;
-            }
-            cnt += 1;
-            if to.id == l {
-                break;
-            }
-        }
-        cnt
-    };
-
-    ((from_idx as isize) - (to_idx as isize)).abs()
-}
-
 fn remove_merging_turns(map: &Map, input: Vec<Turn>, turn_type: TurnType) -> Vec<Turn> {
     let mut turns = Vec::new();
 
@@ -417,6 +313,7 @@ fn remove_merging_turns(map: &Map, input: Vec<Turn>, turn_type: TurnType) -> Vec
                 .or_insert_with(Vec::new)
                 .push(t);
         } else {
+            // Other turn types always pass through
             turns.push(t);
         }
     }
@@ -427,16 +324,58 @@ fn remove_merging_turns(map: &Map, input: Vec<Turn>, turn_type: TurnType) -> Vec
             continue;
         }
 
-        // From one to many is fine
-        if group.iter().map(|t| t.id.src).collect::<HashSet<_>>().len() == 1 {
+        let num_src_lanes = group.iter().map(|t| t.id.src).collect::<HashSet<_>>().len();
+        let num_dst_lanes = group.iter().map(|t| t.id.src).collect::<HashSet<_>>().len();
+
+        // Allow all turns from one to many
+        if num_src_lanes == 1 {
             turns.extend(group);
             continue;
         }
 
-        // We have multiple lanes all with a turn to the same destination road. Most likely, only
-        // the rightmost or leftmost can actually make the turn.
-        // TODO If OSM turn restrictions explicitly have something like "left|left|", then there
-        // are multiple source lanes!
+        // If the number of source and destination lanes is the same, match them up in order,
+        // without any crossing.
+        if num_src_lanes == num_dst_lanes {
+            // But we want to match things up -- leftmost turn lane leads to leftmost destination.
+            let mut src_lanes_in_order: Vec<LaneID> = group.iter().map(|t| t.id.src).collect();
+            src_lanes_in_order.sort_by_key(|l| map.get_parent(*l).dir_and_offset(*l).1);
+            let mut dst_lanes_in_order: Vec<LaneID> = group.iter().map(|t| t.id.dst).collect();
+            dst_lanes_in_order.sort_by_key(|l| map.get_parent(*l).dir_and_offset(*l).1);
+
+            for t in group {
+                let src_order = src_lanes_in_order
+                    .iter()
+                    .position(|l| t.id.src == *l)
+                    .unwrap();
+                let dst_order = dst_lanes_in_order
+                    .iter()
+                    .position(|l| t.id.dst == *l)
+                    .unwrap();
+                if src_order == dst_order {
+                    turns.push(t);
+                }
+            }
+            continue;
+        }
+
+        // If src < dst and src isn't 1, then one source lane gets to access multiple destination
+        // lanes. For now, just give up figuring this out, and allow all combinations.
+        //
+        // TODO https://wiki.openstreetmap.org/wiki/Relation:connectivity may have hints about a
+        // better algorithm.
+        if num_src_lanes < num_dst_lanes {
+            turns.extend(group);
+            continue;
+        }
+
+        // If we get here, then multiple source lanes are forced to merge into one destination
+        // lane.
+        //
+        // Just kind of give up on these cases for now, and fall-back to only allowing the leftmost
+        // or rightmost source lane to make these turns.
+        //
+        // That left or rightmost lane can turn into all lanes on the destination road. Tempting to
+        // remove this, but it may remove some valid U-turn movements (like on Mercer).
         let road = map.get_parent(group[0].id.src);
         let src = if turn_type == TurnType::Right {
             group
@@ -460,9 +399,6 @@ fn remove_merging_turns(map: &Map, input: Vec<Turn>, turn_type: TurnType) -> Vec
                 turns.push(t);
             }
         }
-
-        // That left or rightmost lane can turn into all lanes on the destination road. Tempting to
-        // remove this, but it may remove some valid U-turn movements (like on Mercer).
     }
     turns
 }
