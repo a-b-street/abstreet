f39a50a7e548f243db3ab64dbb03725b0c613ded
Dustin Carlino <dabreegster@gmail.com>
2021-06-06 15:45:40 -0700

    new release

--- map_gui/src/tools/updater.rs
+++ map_gui/src/tools/updater.rs
@@ -11,7 +11,7 @@ use crate::tools::{ChooseSomething, PopupMsg};
 use crate::AppLike;
 
 // Update this ___before___ pushing the commit with "[rebuild] [release]".
-const NEXT_RELEASE: &str = "0.2.46";
+const NEXT_RELEASE: &str = "0.2.47";
 
 // For each city, how many total bytes do the runtime files cost to download?
 
