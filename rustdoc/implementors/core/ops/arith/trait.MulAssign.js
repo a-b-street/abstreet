89a56c4f544c1455e8248a4b207d6e3a93357fe0
dabreegster <dabreegster@users.noreply.github.com>
2021-07-25 20:16:33 +0000

    deploy: 835e4bf7a75898f1cc8cf3fc6bfeb9532f2aea2d

---
+++ rustdoc/implementors/core/ops/arith/trait.MulAssign.js
@@ -0,0 +1,3 @@
+(function() {var implementors = {};
+implementors["geom"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/nightly/core/ops/arith/trait.MulAssign.html\" title=\"trait core::ops::arith::MulAssign\">MulAssign</a>&lt;<a class=\"primitive\" href=\"https://doc.rust-lang.org/nightly/std/primitive.f64.html\">f64</a>&gt; for <a class=\"struct\" href=\"geom/distance/struct.Distance.html\" title=\"struct geom::distance::Distance\">Distance</a>","synthetic":false,"types":["geom::distance::Distance"]}];
+if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
