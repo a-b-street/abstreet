1cd33e8b4f1b33b1d059b0f6b004465612e7e522
dabreegster <dabreegster@users.noreply.github.com>
2021-08-16 23:42:08 +0000

    deploy: 3d710afba2bf8ffa9d897b75ca5f08bd92ccbedf

--- rustdoc/map_gui/tools/url/sidebar-items.js
+++ rustdoc/map_gui/tools/url/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["change_url_cam",""],["change_url_free_param",""],["update_url",""]],"struct":[["URLManager","Utilities for reflecting the current map and viewport in the URL on the web. No effect on native."]]});
+initSidebarItems({"fn":[["change_url_free_param",""],["change_url_param",""],["update_url",""]],"struct":[["URLManager","Utilities for reflecting the current map and viewport in the URL on the web. No effect on native."]]});
