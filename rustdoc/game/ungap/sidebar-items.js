277dc0f435232f12cd77f8f37ad7e882ef9afcb7
dabreegster <dabreegster@users.noreply.github.com>
2021-08-16 20:36:53 +0000

    deploy: 01fbfd37ae7864abd827ea9f26bc2d2a6258c630

--- rustdoc/game/ungap/sidebar-items.js
+++ rustdoc/game/ungap/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["make_legend",""],["make_top_panel",""]],"mod":[["layers",""],["magnifying",""],["nearby",""],["quick_sketch",""]],"struct":[["ExploreMap",""]]});
+initSidebarItems({"fn":[["make_legend",""],["make_top_panel",""],["share_proposal",""]],"mod":[["layers",""],["magnifying",""],["nearby",""],["quick_sketch",""]],"struct":[["ExploreMap",""]]});
