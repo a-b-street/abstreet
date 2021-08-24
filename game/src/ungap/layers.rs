f5c47d6c8f601837d1f7997cc6fda1d5f1ab259a
Dustin Carlino <dabreegster@gmail.com>
2021-08-24 13:45:49 -0700

    Make the layers panel only show bike network legend when enabled.
    (Steep streets legend still gets lost sometimes)

--- game/src/ungap/layers.rs
+++ game/src/ungap/layers.rs
@@ -6,8 +6,8 @@ use map_model::osm::RoadRank;
 use map_model::LaneType;
 use widgetry::{
     ButtonBuilder, Color, ControlState, Drawable, EdgeInsets, EventCtx, GeomBatch, GfxCtx,
-    HorizontalAlignment, Key, Line, Outcome, Panel, RewriteColor, Text, Toggle, VerticalAlignment,
-    Widget,
+    HorizontalAlignment, Image, Key, Line, Outcome, Panel, RewriteColor, Text, Toggle,
+    VerticalAlignment, Widget,
 };
 
 use crate::app::{App, Transition};
@@ -18,7 +18,7 @@ use crate::ungap::labels::DrawRoadLabels;
 /// A bottom-right panel for managing a bunch of toggleable layers in the "ungap the map" tool.
 pub struct Layers {
     panel: Panel,
-    bike_network_layer: Option<DrawNetworkLayer>,
+    bike_network: Option<DrawNetworkLayer>,
     labels: Option<DrawRoadLabels>,
     elevation: bool,
     steep_streets: Option<Drawable>,
@@ -31,68 +31,26 @@ pub struct Layers {
 
 impl Layers {
     pub fn new(ctx: &mut EventCtx, app: &App) -> Layers {
-        Layers {
-            panel: make_bottom_right_panel(ctx, app, true, true, false, false),
-            bike_network_layer: Some(DrawNetworkLayer::new()),
+        let mut l = Layers {
+            panel: Panel::empty(ctx),
+            bike_network: Some(DrawNetworkLayer::new()),
             labels: Some(DrawRoadLabels::new()),
             elevation: false,
             steep_streets: None,
             road_types: HashMap::new(),
             zoom_enabled_cache_key: zoom_enabled_cache_key(ctx),
             map_edit_key: usize::MAX,
-        }
-    }
-
-    fn highlight_road_type(&mut self, ctx: &mut EventCtx, app: &App, name: &str) {
-        // TODO Button enums would rock
-        if name == "bike network"
-            || name == "road labels"
-            || name == "elevation"
-            || name == "steep streets"
-            || name.starts_with("about ")
-        {
-            return;
-        }
-        if self.road_types.contains_key(name) {
-            return;
-        }
-
-        let mut batch = GeomBatch::new();
-        for r in app.primary.map.all_roads() {
-            let rank = r.get_rank();
-            let mut bike_lane = false;
-            let mut buffer = false;
-            for (_, _, lt) in r.lanes_ltr() {
-                if lt == LaneType::Biking {
-                    bike_lane = true;
-                } else if matches!(lt, LaneType::Buffer(_)) {
-                    buffer = true;
-                }
-            }
-
-            let show = (name == "highway" && rank == RoadRank::Highway)
-                || (name == "major street" && rank == RoadRank::Arterial)
-                || (name == "minor street" && rank == RoadRank::Local)
-                || (name == "trail" && r.is_cycleway())
-                || (name == "protected bike lane" && bike_lane && buffer)
-                || (name == "painted bike lane" && bike_lane && !buffer)
-                || (name == "greenway" && bike_network::is_greenway(r));
-            if show {
-                // TODO If it's a bike element, should probably thicken for the unzoomed scale...
-                // the maximum amount?
-                batch.push(Color::CYAN, r.get_thick_polygon(&app.primary.map));
-            }
-        }
-
-        self.road_types.insert(name.to_string(), ctx.upload(batch));
+        };
+        l.update_panel(ctx, app);
+        l
     }
 
     pub fn event(&mut self, ctx: &mut EventCtx, app: &mut App) -> Option<Transition> {
         let key = app.primary.map.get_edits_change_key();
         if self.map_edit_key != key {
             self.map_edit_key = key;
-            if self.bike_network_layer.is_some() {
-                self.bike_network_layer = Some(DrawNetworkLayer::new());
+            if self.bike_network.is_some() {
+                self.bike_network = Some(DrawNetworkLayer::new());
             }
             self.road_types.clear();
         }
@@ -133,14 +91,12 @@ impl Layers {
                     "about the elevation data" => PopupMsg::new_state(ctx, "About the elevation data", vec!["Biking uphill next to traffic without any dedicated space isn't fun.", "Biking downhill next to traffic, especially in the door-zone of parked cars, and especially on Seattle's bumpy roads... is downright terrifying.", "", "Note the elevation data is incorrect near bridges.", "Thanks to King County LIDAR for the data, and Eldan Goldenberg for processing it."]),
                    "zoom map out" => {
                         ctx.canvas.center_zoom(-8.0);
-                        debug!("clicked zoomed out to: {}", ctx.canvas.cam_zoom);
-                        self.panel = make_bottom_right_panel(ctx, app, self.bike_network_layer.is_some(), self.labels.is_some(), self.elevation, self.steep_streets.is_some());
+                        self.update_panel(ctx, app);
                         return Some(Transition::Keep);
                     },
                     "zoom map in" => {
                         ctx.canvas.center_zoom(8.0);
-                        debug!("clicked zoomed in to: {}", ctx.canvas.cam_zoom);
-                        self.panel = make_bottom_right_panel(ctx, app, self.bike_network_layer.is_some(), self.labels.is_some(), self.elevation, self.steep_streets.is_some());
+                        self.update_panel(ctx, app);
                         return Some(Transition::Keep);
                     },
                     _ => unreachable!(),
@@ -149,10 +105,11 @@ impl Layers {
             Outcome::Changed(x) => match x.as_ref() {
                 "bike network" => {
                     if self.panel.is_checked("bike network") {
-                        self.bike_network_layer = Some(DrawNetworkLayer::new());
+                        self.bike_network = Some(DrawNetworkLayer::new());
                     } else {
-                        self.bike_network_layer = None;
+                        self.bike_network = None;
                     }
+                    self.update_panel(ctx, app);
                 }
                 "road labels" => {
                     if self.panel.is_checked("road labels") {
@@ -163,14 +120,7 @@ impl Layers {
                 }
                 "elevation" => {
                     self.elevation = self.panel.is_checked("elevation");
-                    self.panel = make_bottom_right_panel(
-                        ctx,
-                        app,
-                        self.bike_network_layer.is_some(),
-                        self.labels.is_some(),
-                        self.elevation,
-                        self.steep_streets.is_some(),
-                    );
+                    self.update_panel(ctx, app);
                     if self.elevation {
                         let name = app.primary.map.get_name().clone();
                         if app.session.elevation_contours.key() != Some(name.clone()) {
@@ -225,14 +175,7 @@ impl Layers {
 
         if self.zoom_enabled_cache_key != zoom_enabled_cache_key(ctx) {
             // approriately disable/enable zoom buttons in case user scroll-zoomed
-            self.panel = make_bottom_right_panel(
-                ctx,
-                app,
-                self.bike_network_layer.is_some(),
-                self.labels.is_some(),
-                self.elevation,
-                self.steep_streets.is_some(),
-            );
+            self.update_panel(ctx, app);
             self.zoom_enabled_cache_key = zoom_enabled_cache_key(ctx);
         }
 
@@ -242,7 +185,7 @@ impl Layers {
     pub fn draw(&self, g: &mut GfxCtx, app: &App) {
         self.panel.draw(g);
         if g.canvas.cam_zoom < app.opts.min_zoom_for_detail {
-            if let Some(ref n) = self.bike_network_layer {
+            if let Some(ref n) = self.bike_network {
                 n.draw(g, app);
             }
             if let Some(ref l) = self.labels {
@@ -264,6 +207,123 @@ impl Layers {
                 }
             }
         }
+    }
+
+    fn update_panel(&mut self, ctx: &mut EventCtx, app: &App) {
+        self.panel = Panel::new_builder(Widget::col(vec![
+            make_zoom_controls(ctx).align_right().padding_right(16),
+            self.make_legend(ctx, app)
+                .padding(16)
+                .bg(ctx.style().panel_bg),
+        ]))
+        .aligned(HorizontalAlignment::Right, VerticalAlignment::Bottom)
+        .build_custom(ctx);
+    }
+
+    fn make_legend(&self, ctx: &mut EventCtx, app: &App) -> Widget {
+        Widget::col(vec![
+            Widget::row(vec![
+                Image::from_path("system/assets/tools/layers.svg")
+                    .dims(30.0)
+                    .into_widget(ctx)
+                    .centered_vert(),
+                Widget::custom_row(vec![
+                    // TODO Looks too close to access restrictions
+                    legend_item(ctx, app.cs.unzoomed_highway, "highway"),
+                    legend_item(ctx, app.cs.unzoomed_arterial, "major street"),
+                    legend_item(ctx, app.cs.unzoomed_residential, "minor street"),
+                ]),
+            ]),
+            Widget::custom_row({
+                let mut row = vec![Toggle::checkbox(
+                    ctx,
+                    "bike network",
+                    Key::B,
+                    self.bike_network.is_some(),
+                )];
+                if self.bike_network.is_some() {
+                    row.push(legend_item(ctx, *bike_network::DEDICATED_TRAIL, "trail"));
+                    row.push(legend_item(
+                        ctx,
+                        *bike_network::PROTECTED_BIKE_LANE,
+                        "protected bike lane",
+                    ));
+                    row.push(legend_item(
+                        ctx,
+                        *bike_network::PAINTED_BIKE_LANE,
+                        "painted bike lane",
+                    ));
+                    row.push(legend_item(ctx, *bike_network::GREENWAY, "greenway"));
+                }
+                row
+            }),
+            // TODO Distinguish door-zone bike lanes?
+            // TODO Call out bike turning boxes?
+            // TODO Call out bike signals?
+            Toggle::checkbox(ctx, "road labels", Key::L, self.labels.is_some()),
+            Widget::row(vec![
+                Toggle::checkbox(ctx, "elevation", Key::E, self.elevation),
+                ctx.style()
+                    .btn_plain
+                    .icon("system/assets/tools/info.svg")
+                    .build_widget(ctx, "about the elevation data")
+                    .centered_vert(),
+                Text::new()
+                    .into_widget(ctx)
+                    .named("current elevation")
+                    .centered_vert(),
+            ]),
+            Widget::row(vec![
+                Toggle::checkbox(ctx, "steep streets", Key::S, self.steep_streets.is_some()),
+                // A placeholder
+                Text::new().into_widget(ctx).named("steep streets legend"),
+            ]),
+            // TODO Probably a collisions layer
+        ])
+    }
+
+    fn highlight_road_type(&mut self, ctx: &mut EventCtx, app: &App, name: &str) {
+        // TODO Button enums would rock
+        if name == "bike network"
+            || name == "road labels"
+            || name == "elevation"
+            || name == "steep streets"
+            || name.starts_with("about ")
+        {
+            return;
+        }
+        if self.road_types.contains_key(name) {
+            return;
+        }
+
+        let mut batch = GeomBatch::new();
+        for r in app.primary.map.all_roads() {
+            let rank = r.get_rank();
+            let mut bike_lane = false;
+            let mut buffer = false;
+            for (_, _, lt) in r.lanes_ltr() {
+                if lt == LaneType::Biking {
+                    bike_lane = true;
+                } else if matches!(lt, LaneType::Buffer(_)) {
+                    buffer = true;
+                }
+            }
+
+            let show = (name == "highway" && rank == RoadRank::Highway)
+                || (name == "major street" && rank == RoadRank::Arterial)
+                || (name == "minor street" && rank == RoadRank::Local)
+                || (name == "trail" && r.is_cycleway())
+                || (name == "protected bike lane" && bike_lane && buffer)
+                || (name == "painted bike lane" && bike_lane && !buffer)
+                || (name == "greenway" && bike_network::is_greenway(r));
+            if show {
+                // TODO If it's a bike element, should probably thicken for the unzoomed scale...
+                // the maximum amount?
+                batch.push(Color::CYAN, r.get_thick_polygon(&app.primary.map));
+            }
+        }
+
+        self.road_types.insert(name.to_string(), ctx.upload(batch));
     }
 }
 
@@ -301,75 +361,6 @@ fn make_zoom_controls(ctx: &mut EventCtx) -> Widget {
             .disabled(ctx.canvas.is_min_zoom())
             .build_widget(ctx, "zoom map out"),
     ])
-}
-
-fn make_legend(
-    ctx: &mut EventCtx,
-    app: &App,
-    bike_network: bool,
-    labels: bool,
-    elevation: bool,
-    steep_streets: bool,
-) -> Widget {
-    Widget::col(vec![
-        Widget::custom_row(vec![
-            // TODO Looks too close to access restrictions
-            legend_item(ctx, app.cs.unzoomed_highway, "highway"),
-            legend_item(ctx, app.cs.unzoomed_arterial, "major street"),
-            legend_item(ctx, app.cs.unzoomed_residential, "minor street"),
-        ]),
-        Widget::custom_row(vec![
-            Toggle::checkbox(ctx, "bike network", Key::B, bike_network),
-            legend_item(ctx, *bike_network::DEDICATED_TRAIL, "trail"),
-            legend_item(
-                ctx,
-                *bike_network::PROTECTED_BIKE_LANE,
-                "protected bike lane",
-            ),
-            legend_item(ctx, *bike_network::PAINTED_BIKE_LANE, "painted bike lane"),
-            legend_item(ctx, *bike_network::GREENWAY, "greenway"),
-        ]),
-        // TODO Distinguish door-zone bike lanes?
-        // TODO Call out bike turning boxes?
-        // TODO Call out bike signals?
-        Toggle::checkbox(ctx, "road labels", Key::L, labels),
-        Widget::row(vec![
-            Toggle::checkbox(ctx, "elevation", Key::E, elevation),
-            ctx.style()
-                .btn_plain
-                .icon("system/assets/tools/info.svg")
-                .build_widget(ctx, "about the elevation data")
-                .centered_vert(),
-            Text::new()
-                .into_widget(ctx)
-                .named("current elevation")
-                .centered_vert(),
-        ]),
-        Widget::row(vec![
-            Toggle::checkbox(ctx, "steep streets", Key::S, steep_streets),
-            // A placeholder
-            Text::new().into_widget(ctx).named("steep streets legend"),
-        ]),
-        // TODO Probably a collisions layer
-    ])
-}
-
-fn make_bottom_right_panel(
-    ctx: &mut EventCtx,
-    app: &App,
-    bike_network: bool,
-    labels: bool,
-    elevation: bool,
-    steep_streets: bool,
-) -> Panel {
-    Panel::new_builder(Widget::col(vec![
-        make_zoom_controls(ctx).align_right().padding_right(16),
-        make_legend(ctx, app, bike_network, labels, elevation, steep_streets)
-            .padding(16)
-            .bg(ctx.style().panel_bg),
-    ]))
-    .aligned(HorizontalAlignment::Right, VerticalAlignment::Bottom)
-    .build_custom(ctx)
 }
 
 fn legend_batch(ctx: &mut EventCtx, color: Color, txt: Text) -> GeomBatch {
