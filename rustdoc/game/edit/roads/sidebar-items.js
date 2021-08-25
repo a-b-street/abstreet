d01e34795f032c32c6cca48b7a9b3713f7283974
dabreegster <dabreegster@users.noreply.github.com>
2021-08-25 18:29:30 +0000

    deploy: c298124dcc540ebefab8235d0d496170e1dbae77

--- rustdoc/game/edit/roads/sidebar-items.js
+++ rustdoc/game/edit/roads/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["build_lane_highlights",""],["can_reverse",""],["lane_type_to_icon",""],["make_main_panel",""],["make_top_panel",""],["selected_lane_bg",""],["width_choices",""]],"struct":[["RoadEditor",""]]});
+initSidebarItems({"fn":[["build_lane_highlights",""],["can_reverse",""],["fade_irrelevant",""],["lane_type_to_icon",""],["make_main_panel",""],["make_top_panel",""],["selected_lane_bg",""],["width_choices",""]],"struct":[["RoadEditor",""]]});
