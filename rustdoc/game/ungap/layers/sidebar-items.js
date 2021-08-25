3036f833ece8f230e505761e99ab5e21061ee40a
dabreegster <dabreegster@users.noreply.github.com>
2021-08-25 16:41:56 +0000

    deploy: f15a40efd95e7fc9d8bfece375056ad2c8f43bbe

--- rustdoc/game/ungap/layers/sidebar-items.js
+++ rustdoc/game/ungap/layers/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["legend_batch",""],["legend_item",""],["make_zoom_controls",""],["zoom_enabled_cache_key",""]],"struct":[["Layers","A bottom-right panel for managing a bunch of toggleable layers in the “ungap the map” tool."]]});
+initSidebarItems({"fn":[["legend_btn",""],["make_zoom_controls",""],["zoom_enabled_cache_key",""]],"struct":[["Layers","A bottom-right panel for managing a bunch of toggleable layers in the “ungap the map” tool."]]});
