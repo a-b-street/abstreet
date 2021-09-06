05d4474926454ce83ff6aa6a39a2a6a891082f71
Dustin Carlino <dabreegster@gmail.com>
2021-09-06 15:46:33 -0700

    Deploy the RawMap editor to the web. I want to point to it from the intersection geometry article for a slightly more interactive experience.
    It'll be a bit brittle, because it'll always point at the dev version of
    RawMaps. It seems like a huge waste of storage to put RawMaps under
    versioning too.

---
+++ map_editor/src/lib.rs
@@ -0,0 +1,42 @@
+//! The map_editor renders and lets you edit RawMaps, which are a format in between OSM and the
+//! full Map. It's useful for debugging maps imported from OSM, and for drawing synthetic maps for
+//! testing.
+
+#[macro_use]
+extern crate log;
+
+use widgetry::Settings;
+
+use crate::app::App;
+
+mod app;
+mod edit;
+mod model;
+mod world;
+
+pub fn main() {
+    let settings = Settings::new("RawMap editor");
+    run(settings);
+}
+
+fn run(mut settings: Settings) {
+    settings = settings.read_svg(Box::new(abstio::slurp_bytes));
+    widgetry::run(settings, |ctx| {
+        let (app, state) = app::MainState::new(ctx);
+        (app, vec![Box::new(state)])
+    });
+}
+
+#[cfg(target_arch = "wasm32")]
+use wasm_bindgen::prelude::*;
+
+#[cfg(target_arch = "wasm32")]
+#[wasm_bindgen(js_name = "run")]
+pub fn run_wasm(root_dom_id: String, assets_base_url: String, assets_are_gzipped: bool) {
+    let settings = Settings::new("RawMap editor")
+        .root_dom_element_id(root_dom_id)
+        .assets_base_url(assets_base_url)
+        .assets_are_gzipped(assets_are_gzipped);
+
+    run(settings);
+}
