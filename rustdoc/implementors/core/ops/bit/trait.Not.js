efd0c42337243e4174220bacbb60f84e3b06813d
dabreegster <dabreegster@users.noreply.github.com>
2021-09-09 16:19:47 +0000

    deploy: 50b8c533bfcb98d79bbb029ec444ce37e871ee17

--- rustdoc/implementors/core/ops/bit/trait.Not.js
+++ rustdoc/implementors/core/ops/bit/trait.Not.js
@@ -1,3 +1,3 @@
 (function() {var implementors = {};
-implementors["map_model"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.54.0/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"enum\" href=\"map_model/pathfind/enum.PathConstraints.html\" title=\"enum map_model::pathfind::PathConstraints\">PathConstraints</a>","synthetic":false,"types":["map_model::pathfind::PathConstraints"]}];
+implementors["map_model"] = [{"text":"impl <a class=\"trait\" href=\"https://doc.rust-lang.org/1.55.0/core/ops/bit/trait.Not.html\" title=\"trait core::ops::bit::Not\">Not</a> for <a class=\"enum\" href=\"map_model/pathfind/enum.PathConstraints.html\" title=\"enum map_model::pathfind::PathConstraints\">PathConstraints</a>","synthetic":false,"types":["map_model::pathfind::PathConstraints"]}];
 if (window.register_implementors) {window.register_implementors(implementors);} else {window.pending_implementors = implementors;}})()
