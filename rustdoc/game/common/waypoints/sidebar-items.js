43c4f0dd194394ca9fc5f55a2b3a19ae7484d664
dabreegster <dabreegster@users.noreply.github.com>
2021-09-18 20:20:16 +0000

    deploy: 19d74b46ac75c6a48a25218d9880bb6aa87f751c

--- rustdoc/game/common/waypoints/sidebar-items.js
+++ rustdoc/game/common/waypoints/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"struct":[["InputWaypoints","Click to add waypoints, drag them, see the list on a panel and delete them. The caller owns the Panel, since there’s probably more stuff there too."],["Waypoint",""]]});
+initSidebarItems({"fn":[["get_waypoint_text",""]],"struct":[["InputWaypoints","Click to add waypoints, drag them, see the list on a panel and delete them. The caller owns the Panel, since there’s probably more stuff there too."],["Waypoint",""]]});
