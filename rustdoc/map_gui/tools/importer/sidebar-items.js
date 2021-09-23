747b965259a1c394b1c1fdc10ec98815ec95a006
dabreegster <dabreegster@users.noreply.github.com>
2021-09-23 17:21:22 +0000

    deploy: 87dd0295132564b2c93a25e2dc1702940c75c49d

--- rustdoc/map_gui/tools/importer/sidebar-items.js
+++ rustdoc/map_gui/tools/importer/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["grab_geojson_from_clipboard",""]],"struct":[["ImportCity",""]]});
+initSidebarItems({"fn":[["generate_new_map_name",""],["grab_geojson_from_clipboard",""],["sanitize_name",""]],"struct":[["ImportCity",""]]});
