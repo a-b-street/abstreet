8cddd72329b706c2903e801df14551437ad7d5b9
Dustin Carlino <dabreegster@gmail.com>
2021-09-24 14:43:30 -0700

    And the last bit of glue, toggling simple modal filters on road segments

--- game/src/ltn/mod.rs
+++ game/src/ltn/mod.rs
@@ -1,5 +1,6 @@
 use std::collections::BTreeSet;
 
+use geom::{Distance, Line};
 use map_gui::tools::{nice_map_name, CityPicker, ColorDiscrete};
 use map_gui::ID;
 use map_model::{IntersectionID, Map, Road, RoadID};
@@ -14,18 +15,21 @@ mod algorithms;
 
 pub struct Viewer {
     panel: Panel,
-    _neighborhood: Neighborhood,
+    neighborhood: Neighborhood,
     draw_neighborhood: Drawable,
+    // Rat runs and modal filters
+    draw_dynamic_stuff: Drawable,
 
-    rat_runs: Vec<RatRun>,
-    current_idx: usize,
-    draw_rat_run: Drawable,
+    current_rat_run_idx: usize,
 }
 
 struct Neighborhood {
     interior: BTreeSet<RoadID>,
     perimeter: BTreeSet<RoadID>,
     borders: BTreeSet<IntersectionID>,
+
+    modal_filters: BTreeSet<RoadID>,
+    rat_runs: Vec<RatRun>,
 }
 
 struct RatRun {
@@ -52,7 +56,6 @@ impl Viewer {
     fn start_from_road(ctx: &mut EventCtx, app: &App, start: RoadID) -> Box<dyn State<App>> {
         let neighborhood = Neighborhood::from_road(&app.primary.map, start);
         let (draw_neighborhood, legend) = neighborhood.render(ctx, app);
-        let rat_runs = neighborhood.find_rat_runs(&app.primary.map);
         let panel = Panel::new_builder(Widget::col(vec![
             Widget::row(vec![
                 Line("LTN tool").small_heading().into_widget(ctx),
@@ -67,7 +70,6 @@ impl Viewer {
                     .align_right(),
             ]),
             legend,
-            "Click a road to re-center".text_widget(ctx),
             Text::new().into_widget(ctx).named("rat runs"),
         ]))
         .aligned(HorizontalAlignment::Left, VerticalAlignment::Top)
@@ -75,47 +77,56 @@ impl Viewer {
 
         let mut viewer = Viewer {
             panel,
-            _neighborhood: neighborhood,
+            neighborhood: neighborhood,
             draw_neighborhood,
-            rat_runs,
-            current_idx: 0,
-            draw_rat_run: Drawable::empty(ctx),
+            current_rat_run_idx: 0,
+            draw_dynamic_stuff: Drawable::empty(ctx),
         };
         viewer.recalculate(ctx, app);
         Box::new(viewer)
     }
 
     fn recalculate(&mut self, ctx: &mut EventCtx, app: &App) {
-        if self.rat_runs.is_empty() {
+        if self.neighborhood.rat_runs.is_empty() {
             let controls = "No rat runs!".text_widget(ctx);
             self.panel.replace(ctx, "rat runs", controls);
             return;
         }
 
-        let run = &self.rat_runs[self.current_idx];
+        let run = &self.neighborhood.rat_runs[self.current_rat_run_idx];
 
         let controls = Widget::col(vec![
             Widget::row(vec![
                 "Rat runs:".text_widget(ctx).centered_vert(),
                 ctx.style()
                     .btn_prev()
-                    .disabled(self.current_idx == 0)
+                    .disabled(self.current_rat_run_idx == 0)
                     .hotkey(Key::LeftArrow)
                     .build_widget(ctx, "previous rat run"),
                 Text::from(
-                    Line(format!("{}/{}", self.current_idx + 1, self.rat_runs.len())).secondary(),
+                    Line(format!(
+                        "{}/{}",
+                        self.current_rat_run_idx + 1,
+                        self.neighborhood.rat_runs.len()
+                    ))
+                    .secondary(),
                 )
                 .into_widget(ctx)
                 .centered_vert(),
                 ctx.style()
                     .btn_next()
-                    .disabled(self.current_idx == self.rat_runs.len() - 1)
+                    .disabled(self.current_rat_run_idx == self.neighborhood.rat_runs.len() - 1)
                     .hotkey(Key::RightArrow)
                     .build_widget(ctx, "next rat run"),
             ]),
             format!(
                 "This run has a length ratio of {:.2} vs the shortest path",
                 run.length_ratio
+            )
+            .text_widget(ctx),
+            format!(
+                "{} modal filters currently added",
+                self.neighborhood.modal_filters.len()
             )
             .text_widget(ctx),
         ]);
@@ -131,7 +142,26 @@ impl Viewer {
         for road in run.roads(map) {
             batch.push(Color::RED, road.get_thick_polygon());
         }
-        self.draw_rat_run = batch.upload(ctx);
+
+        for r in &self.neighborhood.modal_filters {
+            let road = map.get_r(*r);
+            // TODO If these roads touch a border, probably place it closer to the border. If it's
+            // an inner neighborhood split, maybe in the middle is more reasonable.
+            if let Ok((pt, angle)) = road.center_pts.dist_along(road.center_pts.length() / 2.0) {
+                let filter_len = road.get_width();
+                let barrier = Line::must_new(
+                    pt.project_away(filter_len, angle.rotate_degs(90.0)),
+                    pt.project_away(filter_len, angle.rotate_degs(-90.0)),
+                )
+                .make_polygons(Distance::meters(10.0));
+                batch.push(Color::GREEN, barrier.clone());
+                if let Ok(outline) = barrier.to_outline(Distance::meters(2.0)) {
+                    batch.push(Color::BLACK, outline);
+                }
+            }
+        }
+
+        self.draw_dynamic_stuff = batch.upload(ctx);
     }
 }
 
@@ -148,7 +178,13 @@ impl State<App> for Viewer {
         }
         if let Some(ID::Road(r)) = app.primary.current_selection {
             if ctx.normal_left_click() {
-                return Transition::Replace(Viewer::start_from_road(ctx, app, r));
+                if self.neighborhood.interior.contains(&r) {
+                    self.neighborhood.toggle_modal_filter(&app.primary.map, r);
+                    self.current_rat_run_idx = 0;
+                    self.recalculate(ctx, app);
+                } else {
+                    return Transition::Replace(Viewer::start_from_road(ctx, app, r));
+                }
             }
         }
 
@@ -162,11 +198,11 @@ impl State<App> for Viewer {
                     ));
                 }
                 "previous rat run" => {
-                    self.current_idx -= 1;
+                    self.current_rat_run_idx -= 1;
                     self.recalculate(ctx, app);
                 }
                 "next rat run" => {
-                    self.current_idx += 1;
+                    self.current_rat_run_idx += 1;
                     self.recalculate(ctx, app);
                 }
                 _ => unreachable!(),
@@ -175,25 +211,36 @@ impl State<App> for Viewer {
         Transition::Keep
     }
 
-    fn draw(&self, g: &mut GfxCtx, _: &App) {
+    fn draw(&self, g: &mut GfxCtx, app: &App) {
         self.panel.draw(g);
         g.redraw(&self.draw_neighborhood);
-        g.redraw(&self.draw_rat_run);
+        g.redraw(&self.draw_dynamic_stuff);
+
+        if let Some(ID::Road(r)) = app.primary.current_selection {
+            if self.neighborhood.interior.contains(&r) {
+                if self.neighborhood.modal_filters.contains(&r) {
+                    g.draw_mouse_tooltip(Text::from(Line("Click to remove this modal filter")));
+                } else {
+                    g.draw_mouse_tooltip(Text::from(Line("Click to add a modal filter here")));
+                }
+            } else {
+                g.draw_mouse_tooltip(Text::from(Line("Click to analyze this neighborhood")));
+            }
+        }
     }
 }
 
 impl Neighborhood {
     // Also a legend
     fn render(&self, ctx: &mut EventCtx, app: &App) -> (Drawable, Widget) {
-        let interior = Color::BLUE;
-        let perimeter = Color::hex("#40B5AD");
-        let border = Color::CYAN;
         let mut colorer = ColorDiscrete::no_fading(
             app,
             vec![
-                ("interior", interior),
-                ("perimeter", perimeter),
-                ("border", border),
+                ("interior", Color::BLUE.alpha(0.8)),
+                ("perimeter", Color::hex("#40B5AD").alpha(0.8)),
+                ("border", Color::CYAN.alpha(0.8)),
+                ("rat-run", Color::RED.alpha(0.8)),
+                ("modal filter", Color::GREEN),
             ],
         );
         for r in &self.interior {
