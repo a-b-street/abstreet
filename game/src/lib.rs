a-b-street/abstreet

commit 57584a61b918e268fae4d486b59b6808c3bc0644

For Ungap's route mode, add a button to toggle back and forth between (#783)
the original and edited map. This is an easy way to evaluate how a
proposal influences a particular route.

game/src/lib.rs
@@ -236,6 +236,7 @@ fn setup_app(ctx: &mut EventCtx, mut setup: Setup) -> (App, Vec<Box<dyn State<Ap
         let app = App {
             primary,
             secondary,
+            store_unedited_map_in_secondary: false,
             cs,
             opts: setup.opts.clone(),
             per_obj: crate::app::PerObjectActions::new(),
@@ -269,6 +270,7 @@ fn setup_app(ctx: &mut EventCtx, mut setup: Setup) -> (App, Vec<Box<dyn State<Ap
         let mut app = App {
             primary,
             secondary,
+            store_unedited_map_in_secondary: false,
             cs,
             opts: setup.opts.clone(),
             per_obj: crate::app::PerObjectActions::new(),
@@ -383,6 +385,9 @@ fn finish_app_setup(
     edits: Option<MapEdits>,
     setup: Setup,
 ) -> Vec<Box<dyn State<App>>> {
+    if setup.ungap {
+        app.store_unedited_map_in_secondary = true;
+    }
     if let Some(edits) = edits {
         ctx.loading_screen("apply initial edits", |ctx, mut timer| {
             crate::edit::apply_map_edits(ctx, app, edits);

game/src/app.rs
@@ -31,10 +31,15 @@ pub type Transition = widgetry::Transition<App>;
 
 /// The top-level data that lasts through the entire game, no matter what state the game is in.
 pub struct App {
+    /// State (like the simulation and drawing stuff) associated with the "primary" map.
     pub primary: PerMap,
-    /// This is only used right now for a debug mode to compare two versions of the same map. In
-    /// the future, it might be used for some kind of live "side-by-side" comparison.
+    /// Represents state for a different version of the `primary` map. `swap_map` can be used to
+    /// switch the primary and secondary state. This is currently used to compare an edited and
+    /// unedited map for Ungap the Map and as a debug mode to compare different files representing
+    /// the same area.
     pub secondary: Option<PerMap>,
+    pub store_unedited_map_in_secondary: bool,
+
     pub cs: ColorScheme,
     pub opts: Options,
 
@@ -446,6 +451,14 @@ impl App {
             self.set_prebaked(None);
         });
     }
+
+    /// This swaps the primary and secondary PerMaps. Depending on what state the rest of the app
+    /// is in, things like IDs might totally change!
+    pub fn swap_map(&mut self) {
+        let secondary = self.secondary.take().expect("no secondary map set");
+        let primary = std::mem::replace(&mut self.primary, secondary);
+        self.secondary = Some(primary);
+    }
 }
 
 impl App {
@@ -514,6 +527,7 @@ impl map_gui::AppLike for App {
             timer,
         );
         self.primary.init_camera_for_loaded_map(ctx, false);
+        self.secondary = None;
     }
 
     fn draw_with_opts(&self, g: &mut GfxCtx, opts: DrawOptions) {
@@ -606,7 +620,8 @@ pub struct PerMap {
     /// Any ScenarioModifiers in effect?
     pub has_modified_trips: bool,
 
-    /// If the map has been edited, store the unedited version here.
+    /// If the map has been edited and `app.store_unedited_map_in_secondary` is false, store the
+    /// unedited map here.
     pub unedited_map: Option<Map>,
 
     pub layer: Option<Box<dyn Layer>>,

game/src/common/mod.rs
@@ -309,11 +309,7 @@ impl CommonState {
             return Some(Transition::Push(warp::DebugWarp::new_state(ctx)));
         }
         if app.secondary.is_some() && ctx.input.pressed(lctrl(Key::Tab)) {
-            // This swaps the primary and secondary PerMaps. Depending on what state the rest of
-            // the app is in, things like IDs might totally change! This is very much a debug tool.
-            let secondary = app.secondary.take().unwrap();
-            let primary = std::mem::replace(&mut app.primary, secondary);
-            app.secondary = Some(primary);
+            app.swap_map();
         }
         None
     }

game/src/edit/mod.rs
@@ -705,11 +705,28 @@ fn make_topcenter(ctx: &mut EventCtx, app: &App) -> Panel {
 
 pub fn apply_map_edits(ctx: &mut EventCtx, app: &mut App, edits: MapEdits) {
     ctx.loading_screen("apply map edits", |ctx, timer| {
-        if app.primary.unedited_map.is_none() {
+        if !app.store_unedited_map_in_secondary && app.primary.unedited_map.is_none() {
             timer.start("save unedited map");
             assert!(app.primary.map.get_edits().commands.is_empty());
             app.primary.unedited_map = Some(app.primary.map.clone());
             timer.stop("save unedited map");
+        }
+        if app.store_unedited_map_in_secondary && app.secondary.is_none() {
+            timer.start("save unedited map for toggling");
+            assert!(app.primary.map.get_edits().commands.is_empty());
+            let mut per_map = crate::app::PerMap::map_loaded(
+                app.primary.map.clone(),
+                app.primary.sim.clone(),
+                app.primary.current_flags.clone(),
+                &app.opts,
+                &app.cs,
+                ctx,
+                timer,
+            );
+            // is_secondary indicates the unedited map
+            per_map.is_secondary = true;
+            app.secondary = Some(per_map);
+            timer.stop("save unedited map for toggling");
         }
 
         timer.start("edit map");
@@ -741,6 +758,8 @@ pub fn apply_map_edits(ctx: &mut EventCtx, app: &mut App, edits: MapEdits) {
         if app.primary.layer.as_ref().and_then(|l| l.name()) == Some("map edits") {
             app.primary.layer = Some(Box::new(crate::layer::map::Static::edits(ctx, app)));
         }
+        // Other parts of the UI poll map.get_edits_change_key() to recalculate things based on
+        // edits.
 
         // Autosave
         app.primary.map.save_edits();

game/src/ungap/predict.rs
@@ -349,9 +349,9 @@ impl ModeShiftData {
         timer: &mut Timer,
     ) -> ModeShiftData {
         let unedited_map = app
-            .primary
-            .unedited_map
+            .secondary
             .as_ref()
+            .map(|x| &x.map)
             .unwrap_or(&app.primary.map);
         let all_candidate_trips = timer
             .parallelize(
@@ -407,9 +407,9 @@ impl ModeShiftData {
 
     fn recalculate_gaps(&mut self, ctx: &mut EventCtx, app: &App, timer: &mut Timer) {
         let unedited_map = app
-            .primary
-            .unedited_map
+            .secondary
             .as_ref()
+            .map(|x| &x.map)
             .unwrap_or(&app.primary.map);
 
         // Find all high-stress roads, since we'll filter by them next

game/src/ungap/trip/mod.rs
@@ -1,6 +1,8 @@
 use map_model::RoutingParams;
 use widgetry::mapspace::{ObjectID, World, WorldOutcome};
-use widgetry::{EventCtx, GfxCtx, Outcome, Panel, State, Toggle, Widget};
+use widgetry::{
+    ControlState, EventCtx, GfxCtx, Key, Line, Outcome, Panel, State, Text, Toggle, Widget,
+};
 
 use self::results::RouteDetails;
 use crate::app::{App, Transition};
@@ -198,6 +200,14 @@ impl State<App> for TripPlanner {
                 }
                 return t;
             }
+            if x == "show original map" || x == "show edited map" {
+                app.swap_map();
+                // We're assuming building and intersection IDs haven't changed
+                self.recalculate_routes(ctx, app);
+                // Immediately recalculate the edited layer
+                self.layers.event(ctx, app);
+                return Transition::Keep;
+            }
         }
         if let Outcome::Changed(ref x) = panel_outcome {
             if x == "Avoid steep hills" || x == "Avoid stressful roads" {
@@ -241,6 +251,13 @@ impl State<App> for TripPlanner {
         self.world.draw(g);
         self.main_route.draw(g, &self.input_panel);
     }
+
+    fn on_destroy(&mut self, _: &mut EventCtx, app: &mut App) {
+        // When we switch tabs, always use the edited map
+        if app.primary.is_secondary {
+            app.swap_map();
+        }
+    }
 }
 
 #[derive(Clone, Copy, PartialEq)]
@@ -274,4 +291,37 @@ impl RoutingPreferences {
             ..Default::default()
         }
     }
+}
+
+fn before_after_button(ctx: &mut EventCtx, app: &App) -> Widget {
+    let edits = app.primary.map.get_edits();
+    if app.secondary.is_none() {
+        return Widget::nothing();
+    }
+    let (txt, label) = if edits.commands.is_empty() {
+        (
+            Text::from_all(vec![
+                Line("After / ").secondary(),
+                Line("Before"),
+                Line(" proposal"),
+            ]),
+            "show edited map",
+        )
+    } else {
+        (
+            Text::from_all(vec![
+                Line("After"),
+                Line(" / Before").secondary(),
+                Line(" proposal"),
+            ]),
+            "show original map",
+        )
+    };
+
+    ctx.style()
+        .btn_outline
+        .btn()
+        .label_styled_text(txt, ControlState::Default)
+        .hotkey(Key::Slash)
+        .build_widget(ctx, label)
 }

game/src/ungap/trip/results.rs
@@ -10,7 +10,7 @@ use widgetry::{
     ScreenDims, Series, Text, Widget,
 };
 
-use super::RoutingPreferences;
+use super::{before_after_button, RoutingPreferences};
 use crate::app::{App, Transition};
 
 /// A temporary structure that the caller should unpack and use as needed.
@@ -373,6 +373,7 @@ fn make_detail_widget(
 
     Widget::col(vec![
         Line("Route details").small_heading().into_widget(ctx),
+        before_after_button(ctx, app),
         Text::from_all(vec![
             Line("Distance: ").secondary(),
             Line(stats.total_distance.to_string(&app.opts.units)),

commit cf3d00f234365ab3de8e1e12ff49c162352f191c

Fix the --diff map feature, broken by 57584a61b918e268fae4d486b59b6808c3bc0644

game/src/lib.rs
@@ -16,7 +16,7 @@ use map_model::{Map, MapEdits};
 use sim::{Sim, SimFlags};
 use widgetry::{EventCtx, Settings, State, Transition};
 
-use crate::app::{App, Flags};
+use crate::app::{App, Flags, PerMap};
 use crate::common::jump_to_time_upon_startup;
 use crate::pregame::TitleScreen;
 use crate::sandbox::{GameplayMode, SandboxMode};
@@ -201,7 +201,7 @@ fn setup_app(ctx: &mut EventCtx, mut setup: Setup) -> (App, Vec<Box<dyn State<Ap
             let mut map: Map = abstio::read_binary(path.clone(), &mut timer);
             map.map_loaded_directly(&mut timer);
             let sim = Sim::new(&map, setup.flags.sim_flags.opts.clone());
-            let mut per_map = crate::app::PerMap::map_loaded(
+            let mut per_map = PerMap::map_loaded(
                 map,
                 sim,
                 setup.flags.clone(),
@@ -224,7 +224,7 @@ fn setup_app(ctx: &mut EventCtx, mut setup: Setup) -> (App, Vec<Box<dyn State<Ap
         // Get App created with a dummy blank map
         let map = Map::blank();
         let sim = Sim::new(&map, setup.flags.sim_flags.opts.clone());
-        let primary = crate::app::PerMap::map_loaded(
+        let primary = PerMap::map_loaded(
             map,
             sim,
             setup.flags.clone(),
@@ -235,7 +235,7 @@ fn setup_app(ctx: &mut EventCtx, mut setup: Setup) -> (App, Vec<Box<dyn State<Ap
         );
         let app = App {
             primary,
-            secondary,
+            secondary: None,
             store_unedited_map_in_secondary: false,
             cs,
             opts: setup.opts.clone(),
@@ -247,7 +247,9 @@ fn setup_app(ctx: &mut EventCtx, mut setup: Setup) -> (App, Vec<Box<dyn State<Ap
             ctx,
             &app,
             map_name,
-            Box::new(move |ctx, app| Transition::Clear(continue_app_setup(ctx, app, title, setup))),
+            Box::new(move |ctx, app| {
+                Transition::Clear(continue_app_setup(ctx, app, title, setup, secondary))
+            }),
         )];
         (app, states)
     } else {
@@ -256,7 +258,7 @@ fn setup_app(ctx: &mut EventCtx, mut setup: Setup) -> (App, Vec<Box<dyn State<Ap
         let primary = ctx.loading_screen("load map", |ctx, mut timer| {
             assert!(setup.flags.sim_flags.modifiers.is_empty());
             let (map, sim, _) = setup.flags.sim_flags.load_synchronously(timer);
-            crate::app::PerMap::map_loaded(
+            PerMap::map_loaded(
                 map,
                 sim,
                 setup.flags.clone(),
@@ -269,7 +271,7 @@ fn setup_app(ctx: &mut EventCtx, mut setup: Setup) -> (App, Vec<Box<dyn State<Ap
         assert!(secondary.is_none());
         let mut app = App {
             primary,
-            secondary,
+            secondary: None,
             store_unedited_map_in_secondary: false,
             cs,
             opts: setup.opts.clone(),
@@ -277,7 +279,7 @@ fn setup_app(ctx: &mut EventCtx, mut setup: Setup) -> (App, Vec<Box<dyn State<Ap
             session: crate::app::SessionState::empty(),
         };
 
-        let states = continue_app_setup(ctx, &mut app, title, setup);
+        let states = continue_app_setup(ctx, &mut app, title, setup, None);
         (app, states)
     }
 }
@@ -287,7 +289,11 @@ fn continue_app_setup(
     app: &mut App,
     title: bool,
     setup: Setup,
+    secondary: Option<PerMap>,
 ) -> Vec<Box<dyn State<App>>> {
+    // Run this after loading the primary map. That process wipes out app.secondary.
+    app.secondary = secondary;
+
     if !URLManager::change_camera(
         ctx,
         setup.center_camera.as_ref(),