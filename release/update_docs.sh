0c9ac9d74dab51c9b5e594af625b509bc17847b4
Dustin Carlino <dabreegster@gmail.com>
2021-11-14 15:06:53 -0800

    adjust software versions [rebuild] [release]

--- release/update_docs.sh
+++ release/update_docs.sh
@@ -3,21 +3,25 @@
 
 set -e
 
-MAJOR=0
-MINOR=2
+OLD_MAJOR=0
+OLD_MINOR=2	# TODO Update this
 OLD_PATCH=$1
+
+NEW_MAJOR=0
+NEW_MINOR=3
 NEW_PATCH=$2
+
 if [ "$OLD_PATCH" == "" ] || [ "$NEW_PATCH" == "" ]; then
 	echo Missing args;
 	exit 1;
 fi
 
 # This assumes https://github.com/a-b-street/docs is checked out at ~/docs
-perl -pi -e "s/${MAJOR}_${MINOR}_${OLD_PATCH}/${MAJOR}_${MINOR}_${NEW_PATCH}/g" README.md ~/docs/book/src/user/README.md ~/docs/book/src/software/*.md ~/docs/book/src/proposals/*/*.md
-perl -pi -e "s/${MAJOR}\.${MINOR}\.${OLD_PATCH}/${MAJOR}\.${MINOR}\.${NEW_PATCH}/g" README.md ~/docs/book/src/user/README.md ~/docs/book/src/software/*.md ~/docs/book/src/software/*/*.md ~/docs/book/src/proposals/*/*.md
+perl -pi -e "s/${OLD_MAJOR}_${OLD_MINOR}_${OLD_PATCH}/${NEW_MAJOR}_${NEW_MINOR}_${NEW_PATCH}/g" README.md ~/docs/book/src/user/README.md ~/docs/book/src/software/*.md ~/docs/book/src/proposals/*/*.md
+perl -pi -e "s/${OLD_MAJOR}\.${OLD_MINOR}\.${OLD_PATCH}/${NEW_MAJOR}\.${NEW_MINOR}\.${NEW_PATCH}/g" README.md ~/docs/book/src/user/README.md ~/docs/book/src/software/*.md ~/docs/book/src/software/*/*.md ~/docs/book/src/proposals/*/*.md
 
 echo "Don't forget to:"
 echo "1) ./release/deploy_web.sh"
-echo "2) aws s3 cp --recursive --exclude 'data/input/*' s3://abstreet/dev/ s3://abstreet/${MAJOR}.${MINOR}.${NEW_PATCH}"
+echo "2) aws s3 cp --recursive --exclude 'data/input/*' s3://abstreet/dev/ s3://abstreet/${NEW_MAJOR}.${NEW_MINOR}.${NEW_PATCH}"
 echo "3) Update map_gui/src/tools/mod.rs"
 echo "4) Push the docs repo too"
