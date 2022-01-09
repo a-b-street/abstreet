27452f0e081adaa2909488fdcbce42c5a9cf962c
Dustin Carlino <dabreegster@gmail.com>
2022-01-09 20:47:43 +0000

    new release

--- map_gui/src/tools/mod.rs
+++ map_gui/src/tools/mod.rs
@@ -43,7 +43,7 @@ mod updater;
 mod url;
 
 // Update this ___before___ pushing the commit with "[rebuild] [release]".
-const NEXT_RELEASE: &str = "0.3.6";
+const NEXT_RELEASE: &str = "0.3.7";
 
 /// Returns the version of A/B Street to link to. When building for a release, this points to that
 /// new release. Otherwise it points to the current dev version.
