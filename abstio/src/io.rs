a-b-street/abstreet

commit c08484505e0694ff8029cb7f636315417331f788

Ignore hidden files like .DS_Store, fixing #832

abstio/src/io.rs
@@ -82,6 +82,17 @@ pub fn load_all_objects<T: DeserializeOwned>(dir: String) -> Vec<(String, T)> {
 }
 
 /// Just list all things from a directory, return sorted by name, with file extension removed.
+///
+/// Hidden files (starting with `.`) are filtered out.
 pub fn list_all_objects(dir: String) -> Vec<String> {
-    list_dir(dir).into_iter().map(basename).collect()
+    list_dir(dir)
+        .into_iter()
+        .filter_map(|x| {
+            if x.starts_with(".") {
+                None
+            } else {
+                Some(basename(x))
+            }
+        })
+        .collect()
 }

commit d3ff9d3412c5c1b17f1b86942fdc98bbca610ea7

Fix c08484505e0694ff8029cb7f636315417331f788, which broke listing files in a directory! This impacted loading newly imported maps and finding Seattle/UK scenario files.

abstio/src/io.rs
@@ -87,12 +87,7 @@ pub fn load_all_objects<T: DeserializeOwned>(dir: String) -> Vec<(String, T)> {
 pub fn list_all_objects(dir: String) -> Vec<String> {
     list_dir(dir)
         .into_iter()
-        .filter_map(|x| {
-            if x.starts_with(".") {
-                None
-            } else {
-                Some(basename(x))
-            }
-        })
+        .map(basename)
+        .filter(|x| !x.starts_with("."))
         .collect()
 }