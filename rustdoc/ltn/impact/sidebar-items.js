5322b056a39566e0263abe0067b8bc874c0d5259
dabreegster <dabreegster@users.noreply.github.com>
2022-01-26 11:36:25 +0000

    deploy: ec0fd1b94daf99d5f83ccddeb6c534cdde2dd50d

--- rustdoc/ltn/impact/sidebar-items.js
+++ rustdoc/ltn/impact/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"enum":[["Obj",""]],"fn":[["count_throughput",""],["make_world",""],["ranked_intersections",""],["ranked_roads",""]],"mod":[["ui",""]],"struct":[["Results",""]]});
+initSidebarItems({"enum":[["Obj",""]],"fn":[["count_throughput",""],["make_world",""]],"mod":[["ui",""]],"struct":[["Results",""]]});
