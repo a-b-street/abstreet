b794b1ff253f44c51deb07e915a064e65b9f2684
dabreegster <dabreegster@users.noreply.github.com>
2022-02-03 14:29:22 +0000

    deploy: f22590ae0cedd756eadb018cd02afc77729e8ec4

---
+++ rustdoc/synthpop/borders/sidebar-items.js
@@ -0,0 +1,1 @@
+initSidebarItems({"struct":[["MapBorder",""],["MapBorders","Lists all border intersections of the map, broken down by mode and whether they support incoming or outgoing traffic."]]});
