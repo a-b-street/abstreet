58991ab041e8c0f23c1f0d9271d52d89599380fe
Dustin Carlino <dabreegster@gmail.com>
2022-02-20 11:31:05 +0000

    Start to reorganize code that transforms RawMaps, particularly things that try to merge short roads #654

---
+++ raw_map/src/transform/merge_short_road.rs
@@ -0,0 +1,189 @@
+use std::collections::BTreeMap;
+
+use anyhow::Result;
+
+use crate::{osm, IntersectionType, OriginalRoad, RawMap, RestrictionType};
+
+// TODO After merging a road, trying to drag the surviving intersection in map_editor crashes. I
+// bet the underlying problem there would help debug automated transformations near merged roads
+// too.
+
+impl RawMap {
+    /// Returns (the surviving intersection, the deleted intersection, deleted roads, new roads)
+    pub fn merge_short_road(
+        &mut self,
+        short: OriginalRoad,
+    ) -> Result<(
+        osm::NodeID,
+        osm::NodeID,
+        Vec<OriginalRoad>,
+        Vec<OriginalRoad>,
+    )> {
+        // If either intersection attached to this road has been deleted, then we're probably
+        // dealing with a short segment in the middle of a cluster of intersections. Just delete
+        // the segment and move on.
+        if !self.intersections.contains_key(&short.i1)
+            || !self.intersections.contains_key(&short.i2)
+        {
+            self.roads.remove(&short).unwrap();
+            bail!(
+                "One endpoint of {} has already been deleted, skipping",
+                short
+            );
+        }
+
+        // First a sanity check.
+        {
+            let i1 = &self.intersections[&short.i1];
+            let i2 = &self.intersections[&short.i2];
+            if i1.intersection_type == IntersectionType::Border
+                || i2.intersection_type == IntersectionType::Border
+            {
+                bail!("{} touches a border", short);
+            }
+        }
+
+        // TODO Fix up turn restrictions. Many cases:
+        // [ ] road we're deleting has simple restrictions
+        // [ ] road we're deleting has complicated restrictions
+        // [X] road we're deleting is the target of a simple BanTurns restriction
+        // [ ] road we're deleting is the target of a simple OnlyAllowTurns restriction
+        // [ ] road we're deleting is the target of a complicated restriction
+        // [X] road we're deleting is the 'via' of a complicated restriction
+        // [ ] road we're deleting has turn lanes that wind up orphaning something
+
+        let (i1, i2) = (short.i1, short.i2);
+        if i1 == i2 {
+            bail!("Can't merge {} -- it's a loop on {}", short, i1);
+        }
+        // Remember the original connections to i1 before we merge. None of these will change IDs.
+        let mut connected_to_i1 = self.roads_per_intersection(i1);
+        connected_to_i1.retain(|x| *x != short);
+
+        // Retain some geometry...
+        {
+            let mut trim_roads_for_merging = BTreeMap::new();
+            for i in [i1, i2] {
+                for r in self.roads_per_intersection(i) {
+                    // If we keep this in there, it might accidentally overwrite the
+                    // trim_roads_for_merging key for a surviving road!
+                    if r == short {
+                        continue;
+                    }
+                    // If we're going to delete this later, don't bother!
+                    // TODO When we do automatic consolidation and don't just look for this tag,
+                    // we'll need to get more clever here, or temporarily apply this tag.
+                    if self.roads[&r].osm_tags.is("junction", "intersection") {
+                        continue;
+                    }
+
+                    if let Some(pl) = self.trimmed_road_geometry(r) {
+                        if r.i1 == i {
+                            if trim_roads_for_merging.contains_key(&(r.osm_way_id, true)) {
+                                panic!("trim_roads_for_merging has an i1 duplicate for {}", r);
+                            }
+                            trim_roads_for_merging.insert((r.osm_way_id, true), pl.first_pt());
+                        } else {
+                            if trim_roads_for_merging.contains_key(&(r.osm_way_id, false)) {
+                                panic!("trim_roads_for_merging has an i2 duplicate for {}", r);
+                            }
+                            trim_roads_for_merging.insert((r.osm_way_id, false), pl.last_pt());
+                        }
+                    } else {
+                        panic!("No trimmed_road_geometry at all for {}", r);
+                    }
+                }
+            }
+            self.intersections
+                .get_mut(&i1)
+                .unwrap()
+                .trim_roads_for_merging
+                .extend(trim_roads_for_merging);
+        }
+
+        self.roads.remove(&short).unwrap();
+
+        // Arbitrarily keep i1 and destroy i2. If the intersection types differ, upgrade the
+        // surviving interesting.
+        {
+            // Don't use delete_intersection; we're manually fixing up connected roads
+            let i = self.intersections.remove(&i2).unwrap();
+            if i.intersection_type == IntersectionType::TrafficSignal {
+                self.intersections.get_mut(&i1).unwrap().intersection_type =
+                    IntersectionType::TrafficSignal;
+            }
+        }
+
+        // Fix up all roads connected to i2. Delete them and create a new copy; the ID changes,
+        // since one intersection changes.
+        let mut deleted = vec![short];
+        let mut created = Vec::new();
+        let mut old_to_new = BTreeMap::new();
+        let mut new_to_old = BTreeMap::new();
+        for r in self.roads_per_intersection(i2) {
+            deleted.push(r);
+            let road = self.roads.remove(&r).unwrap();
+            let mut new_id = r;
+            if r.i1 == i2 {
+                new_id.i1 = i1;
+            } else {
+                assert_eq!(r.i2, i2);
+                new_id.i2 = i1;
+            }
+            old_to_new.insert(r, new_id);
+            new_to_old.insert(new_id, r);
+
+            self.roads.insert(new_id, road);
+            created.push(new_id);
+        }
+
+        // If we're deleting the target of a simple restriction somewhere, update it.
+        for (from_id, road) in &mut self.roads {
+            let mut fix_trs = Vec::new();
+            for (rt, to) in road.turn_restrictions.drain(..) {
+                if to == short && rt == RestrictionType::BanTurns {
+                    // Remove this restriction, replace it with a new one to each of the successors
+                    // of the deleted road. Depending if the intersection we kept is the one
+                    // connecting these two roads, the successors differ.
+                    if new_to_old
+                        .get(from_id)
+                        .cloned()
+                        .unwrap_or(*from_id)
+                        .common_endpt(short)
+                        == i1
+                    {
+                        for x in &created {
+                            fix_trs.push((rt, *x));
+                        }
+                    } else {
+                        for x in &connected_to_i1 {
+                            fix_trs.push((rt, *x));
+                        }
+                    }
+                } else {
+                    fix_trs.push((rt, to));
+                }
+            }
+            road.turn_restrictions = fix_trs;
+        }
+
+        // If we're deleting the 'via' of a complicated restriction somewhere, change it to a
+        // simple restriction.
+        for road in self.roads.values_mut() {
+            let mut add = Vec::new();
+            road.complicated_turn_restrictions.retain(|(via, to)| {
+                if *via == short {
+                    // Depending which intersection we're deleting, the ID of 'to' might change
+                    let to_id = old_to_new.get(to).cloned().unwrap_or(*to);
+                    add.push((RestrictionType::BanTurns, to_id));
+                    false
+                } else {
+                    true
+                }
+            });
+            road.turn_restrictions.extend(add);
+        }
+
+        Ok((i1, i2, deleted, created))
+    }
+}
--- raw_map/src/transform/mod.rs
+++ raw_map/src/transform/mod.rs
@@ -1,4 +1,45 @@
-pub mod collapse_intersections;
-pub mod merge_intersections;
-pub mod remove_disconnected;
-pub mod snappy;
+use abstutil::Timer;
+
+use crate::RawMap;
+
+mod collapse_intersections;
+mod merge_intersections;
+mod merge_short_road;
+mod remove_disconnected;
+mod snappy;
+
+impl RawMap {
+    /// Run a sequence of transformations to the RawMap before converting it to a full Map.
+    ///
+    /// We don't want to run these during the OSM->RawMap import stage, because we want to use the
+    /// map_editor tool to debug the RawMap.
+    pub fn run_all_simplifications(
+        &mut self,
+        consolidate_all_intersections: bool,
+        timer: &mut Timer,
+    ) {
+        timer.start("trimming dead-end cycleways (round 1)");
+        collapse_intersections::trim_deadends(self);
+        timer.stop("trimming dead-end cycleways (round 1)");
+
+        timer.start("snap separate cycleways");
+        snappy::snap_cycleways(self);
+        timer.stop("snap separate cycleways");
+
+        // More dead-ends can be created after snapping cycleways. But also, snapping can be easier
+        // to do after trimming some dead-ends. So... just run it twice.
+        timer.start("trimming dead-end cycleways (round 2)");
+        collapse_intersections::trim_deadends(self);
+        timer.stop("trimming dead-end cycleways (round 2)");
+
+        remove_disconnected::remove_disconnected_roads(self, timer);
+
+        timer.start("merging short roads");
+        merge_intersections::merge_short_roads(self, consolidate_all_intersections);
+        timer.stop("merging short roads");
+
+        timer.start("collapsing degenerate intersections");
+        collapse_intersections::collapse(self);
+        timer.stop("collapsing degenerate intersections");
+    }
+}
