a-b-street/abstreet

commit 2bf7ce232adf935a3414506f6f169efa55a5273c

Port map_editor to the shared map-space World abstraction. #763
There may be small UX bugs, but the code is vastly simpler now, and all
main functionality works fine.

widgetry/src/mapspace/world.rs
@@ -36,7 +36,8 @@ pub enum WorldOutcome<ID: ObjectID> {
     /// A left click occurred while not hovering on any object
     ClickedFreeSpace(Pt2D),
     /// An object is being dragged. The given offsets are relative to the previous dragging event.
-    /// The current position of the cursor is included.
+    /// The current position of the cursor is included. If you're dragging a large object, applying
+    /// the offset will likely feel more natural than centering on the cursor.
     Dragging {
         obj: ID,
         dx: f64,
@@ -47,6 +48,13 @@ pub enum WorldOutcome<ID: ObjectID> {
     Keypress(&'static str, ID),
     /// A hoverable object was clicked
     ClickedObject(ID),
+    /// The object being hovered on changed from (something before, to something after). Note this
+    /// transition may also occur outside of `event` -- such as during `delete` or `initialize_hover`.
+    ///
+    /// TODO Bug in the map_editor: If you delete one object, then the caller does initialize_hover
+    /// and we immediately wind up on another road beneath, we don't detect this and start showing
+    /// road points.
+    HoverChanged(Option<ID>, Option<ID>),
     /// Nothing interesting happened
     Nothing,
 }
@@ -76,6 +84,19 @@ impl<I: ObjectID> WorldOutcome<I> {
             }),
             WorldOutcome::Keypress(action, id) => Some(WorldOutcome::Keypress(action, f(id)?)),
             WorldOutcome::ClickedObject(id) => Some(WorldOutcome::ClickedObject(f(id)?)),
+            WorldOutcome::HoverChanged(before, after) => {
+                // If f returns None, bail out. But preserve None if before or after originally was
+                // that.
+                let before = match before {
+                    Some(x) => Some(f(x)?),
+                    None => None,
+                };
+                let after = match after {
+                    Some(x) => Some(f(x)?),
+                    None => None,
+                };
+                Some(WorldOutcome::HoverChanged(before, after))
+            }
             WorldOutcome::Nothing => Some(WorldOutcome::Nothing),
         }
     }
@@ -240,7 +261,7 @@ impl<'a, ID: ObjectID> ObjectBuilder<'a, ID> {
     }
 
     /// Finalize the object, adding it to the `World`.
-    pub fn build(mut self, ctx: &mut EventCtx) {
+    pub fn build(mut self, ctx: &EventCtx) {
         let hitbox = self.hitbox.take().expect("didn't specify hitbox");
         let bounds = hitbox.get_bounds();
         let quadtree_id = self
@@ -361,7 +382,11 @@ impl<ID: ObjectID> World<ID> {
     }
 
     /// After adding all objects to a `World`, call this to initially detect if the cursor is
-    /// hovering on an object.
+    /// hovering on an object. This may also be called after adding or deleting objects to
+    /// immediately recalculate hover before the mouse moves.
+    // TODO Maybe we should automatically do this after mutations? Except we don't want to in the
+    // middle of a bulk operation, like initial setup or a many-step mutation. So maybe the caller
+    // really should handle it.
     pub fn initialize_hover(&mut self, ctx: &EventCtx) {
         self.hovering = ctx
             .canvas
@@ -401,11 +426,16 @@ impl<ID: ObjectID> World<ID> {
                     return WorldOutcome::ClickedObject(self.hovering.unwrap());
                 }
 
+                let before = self.hovering;
                 self.hovering = ctx
                     .canvas
                     .get_cursor_in_map_space()
                     .and_then(|cursor| self.calculate_hover(cursor));
-                return WorldOutcome::Nothing;
+                return if before == self.hovering {
+                    WorldOutcome::Nothing
+                } else {
+                    WorldOutcome::HoverChanged(before, self.hovering)
+                };
             }
             // Allow zooming, but not panning, while dragging
             if let Some((_, dy)) = ctx.input.get_mouse_scroll() {
@@ -432,13 +462,22 @@ impl<ID: ObjectID> World<ID> {
         let cursor = if let Some(pt) = ctx.canvas.get_cursor_in_map_space() {
             pt
         } else {
-            self.hovering = None;
-            return WorldOutcome::Nothing;
+            let before = self.hovering.take();
+            return if before.is_some() {
+                WorldOutcome::HoverChanged(before, None)
+            } else {
+                WorldOutcome::Nothing
+            };
         };
 
         // Possibly recalculate hovering
+        let mut neutral_outcome = WorldOutcome::Nothing;
         if ctx.redo_mouseover() {
+            let before = self.hovering;
             self.hovering = self.calculate_hover(cursor);
+            if before != self.hovering {
+                neutral_outcome = WorldOutcome::HoverChanged(before, self.hovering);
+            }
         }
 
         // If we're hovering on a draggable thing, only allow zooming, not panning
@@ -456,7 +495,7 @@ impl<ID: ObjectID> World<ID> {
                 allow_panning = false;
                 if ctx.input.left_mouse_button_pressed() {
                     self.dragging_from = Some((cursor, false));
-                    return WorldOutcome::Nothing;
+                    return neutral_outcome;
                 }
             }
 
@@ -477,7 +516,7 @@ impl<ID: ObjectID> World<ID> {
             ctx.canvas.zoom(dy, ctx.canvas.get_cursor());
         }
 
-        WorldOutcome::Nothing
+        neutral_outcome
     }
 
     fn calculate_hover(&self, cursor: Pt2D) -> Option<ID> {
@@ -547,6 +586,23 @@ impl<ID: ObjectID> World<ID> {
         } else {
             false
         }
+    }
+
+    /// Calculate the object currently underneath the cursor. This should only be used when the
+    /// `World` is not being actively updated by calling `event`. If another state temporarily
+    /// needs to disable most interactions with objects, it can poll this instead.
+    pub fn calculate_hovering(&self, ctx: &EventCtx) -> Option<ID> {
+        // TODO Seems expensive! Maybe instead set some kind of "locked" mode and disable
+        // everything except hovering?
+        ctx.canvas
+            .get_cursor_in_map_space()
+            .and_then(|cursor| self.calculate_hover(cursor))
+    }
+
+    /// If an object is currently being hovered on, return its keybindings. This should be used to
+    /// describe interactions; to detect the keypresses, listen for `WorldOutcome::Keypress`.
+    pub fn get_hovered_keybindings(&self) -> Option<&Vec<(MultiKey, &'static str)>> {
+        Some(&self.objects[&self.hovering?].keybindings)
     }
 }
 

apps/map_editor/src/world.rs
@@ -1,143 +1,0 @@
-use std::collections::HashMap;
-use std::fmt::Debug;
-use std::hash::Hash;
-
-use aabb_quadtree::{ItemId, QuadTree};
-
-use geom::{Bounds, Circle, Distance, Polygon, Pt2D};
-use widgetry::{Color, Drawable, EventCtx, GeomBatch, GfxCtx};
-
-pub trait ObjectID: Clone + Copy + Debug + Eq + Hash {
-    // Higher shows up in the front.
-    fn zorder(&self) -> usize;
-}
-
-pub struct Object<ID: ObjectID> {
-    id: ID,
-    pub geometry: Vec<(Color, Polygon)>,
-}
-
-impl<ID: ObjectID> Object<ID> {
-    pub fn new(id: ID, color: Color, poly: Polygon) -> Object<ID> {
-        Object {
-            id,
-            geometry: vec![(color, poly)],
-        }
-    }
-}
-
-struct WorldObject {
-    unioned_polygon: Polygon,
-    draw: Drawable,
-    quadtree_id: ItemId,
-}
-
-pub struct World<ID: ObjectID> {
-    objects: HashMap<ID, WorldObject>,
-    quadtree: QuadTree<ID>,
-    current_selection: Option<ID>,
-}
-
-impl<ID: ObjectID> World<ID> {
-    pub fn new() -> World<ID> {
-        World {
-            objects: HashMap::new(),
-            // Force the quadtree to support any possible positions. Especially when creating
-            // synthetic maps, the bounds change, but updating the quadtree is nontrivial. But they
-            // have to be non-negative.
-            quadtree: QuadTree::default(
-                Bounds::from(&[Pt2D::new(0.0, 0.0), Pt2D::new(std::f64::MAX, std::f64::MAX)])
-                    .as_bbox(),
-            ),
-            current_selection: None,
-        }
-    }
-
-    pub fn draw<F: Fn(ID) -> bool>(&self, g: &mut GfxCtx, show: F) {
-        let mut objects: Vec<ID> = Vec::new();
-        for &(id, _, _) in &self.quadtree.query(g.get_screen_bounds().as_bbox()) {
-            if show(*id) {
-                objects.push(*id);
-            }
-        }
-        objects.sort_by_key(|id| id.zorder());
-
-        for id in objects {
-            let obj = &self.objects[&id];
-            g.redraw(&obj.draw);
-        }
-
-        if let Some(id) = self.current_selection {
-            let obj = &self.objects[&id];
-            g.draw_polygon(Color::CYAN.alpha(0.5), obj.unioned_polygon.clone());
-        }
-    }
-
-    pub fn handle_mouseover(&mut self, ctx: &EventCtx) {
-        self.current_selection = None;
-
-        let cursor = if let Some(pt) = ctx.canvas.get_cursor_in_map_space() {
-            pt
-        } else {
-            return;
-        };
-
-        let mut objects: Vec<ID> = Vec::new();
-        for &(id, _, _) in &self.quadtree.query(
-            Circle::new(cursor, Distance::meters(3.0))
-                .get_bounds()
-                .as_bbox(),
-        ) {
-            objects.push(*id);
-        }
-        objects.sort_by_key(|id| id.zorder());
-        objects.reverse();
-
-        for id in objects {
-            if self.objects[&id].unioned_polygon.contains_pt(cursor) {
-                self.current_selection = Some(id);
-                return;
-            }
-        }
-    }
-
-    pub fn force_set_selection(&mut self, id: ID) {
-        self.current_selection = Some(id);
-    }
-
-    pub fn get_selection(&self) -> Option<ID> {
-        self.current_selection
-    }
-
-    // TODO This and delete assume the original bounds passed to the quadtree are still valid.
-    pub fn add(&mut self, ctx: &EventCtx, obj: Object<ID>) {
-        let unioned_polygon =
-            Polygon::union_all(obj.geometry.iter().map(|(_, p)| p.clone()).collect());
-
-        let bounds = unioned_polygon.get_bounds();
-        // This might break, it might not; the quadtree impl is a little unclear.
-        if bounds.min_x < 0.0 || bounds.min_y < 0.0 {
-            warn!("{:?} has negative coordinates {:?}", obj.id, bounds);
-        }
-        let quadtree_id = self.quadtree.insert_with_box(obj.id, bounds.as_bbox());
-        let draw = ctx.upload(GeomBatch::from(obj.geometry));
-        self.objects.insert(
-            obj.id,
-            WorldObject {
-                unioned_polygon,
-                draw,
-                quadtree_id,
-            },
-        );
-    }
-
-    pub fn delete(&mut self, id: ID) {
-        let obj = self.objects.remove(&id).unwrap();
-        if self.quadtree.remove(obj.quadtree_id).is_none() {
-            // TODO This happens for nodes that're totally off-map. They're retained because we
-            // defer running the remove_disconnected code, so map_editor can actually debug more
-            // stuff.
-            error!("{:?} wasn't in the quadtree", id);
-        }
-    }
-}

apps/map_editor/src/lib.rs
@@ -16,7 +16,6 @@ mod camera;
 mod edit;
 mod load;
 mod model;
-mod world;
 
 pub fn main() {
     let settings = Settings::new("RawMap editor");

commit b07177c21a81f177a3fe294cc7227cb7b547a7f8

Fix crash in the LTN and Ungap pathfinding tool, introduced in 2bf7ce232adf935a3414506f6f169efa55a5273c

apps/game/src/ungap/trip/mod.rs
@@ -189,10 +189,13 @@ impl State<App> for TripPlanner {
                 self.recalculate_routes(ctx, app);
                 return Transition::Keep;
             }
-            x => x.map_id(|id| match id {
-                ID::Waypoint(id) => id,
-                _ => unreachable!(),
-            }),
+            x => x
+                .maybe_map_id(|id| match id {
+                    ID::Waypoint(id) => Some(id),
+                    // Ignore HoverChanged events
+                    _ => None,
+                })
+                .unwrap_or(WorldOutcome::Nothing),
         };
 
         let panel_outcome = self.input_panel.event(ctx);

apps/ltn/src/pathfinding.rs
@@ -4,7 +4,7 @@ use map_gui::tools::{
 };
 use map_model::{PathfinderCaching, NORMAL_LANE_THICKNESS};
 use synthpop::{TripEndpoint, TripMode};
-use widgetry::mapspace::{ObjectID, ToggleZoomed, World};
+use widgetry::mapspace::{ObjectID, ToggleZoomed, World, WorldOutcome};
 use widgetry::{
     Color, EventCtx, GfxCtx, Line, Outcome, Panel, RoundedF64, Spinner, State, Text, Widget,
 };
@@ -249,10 +249,13 @@ impl State<App> for RoutePlanner {
             // Fall through. Clicking free space and other ID-less outcomes will match here, but we
             // don't want them to.
         }
-        let world_outcome_for_waypoints = world_outcome.map_id(|id| match id {
-            Obj::Waypoint(id) => id,
-            _ => unreachable!(),
-        });
+        // Ignore HoverChanged events for filterable objects
+        let world_outcome_for_waypoints = world_outcome
+            .maybe_map_id(|id| match id {
+                Obj::Waypoint(id) => Some(id),
+                _ => None,
+            })
+            .unwrap_or(WorldOutcome::Nothing);
 
         let panel_outcome = self.panel.event(ctx);
         if let Outcome::Clicked(ref x) = panel_outcome {

widgetry/src/mapspace/world.rs
@@ -63,11 +63,6 @@ impl<I: ObjectID> WorldOutcome<I> {
     /// If the outcome references some ID, transform it to another type. This is useful when some
     /// component owns a World that contains a few different types of objects, some of which are
     /// managed by another component that only cares about its IDs.
-    pub fn map_id<O: ObjectID, F: Fn(I) -> O>(self, f: F) -> WorldOutcome<O> {
-        self.maybe_map_id(|id| Some(f(id))).unwrap()
-    }
-
-    /// Like `map_id`, but the transformation may fail.
     pub fn maybe_map_id<O: ObjectID, F: Fn(I) -> Option<O>>(self, f: F) -> Option<WorldOutcome<O>> {
         match self {
             WorldOutcome::ClickedFreeSpace(pt) => Some(WorldOutcome::ClickedFreeSpace(pt)),