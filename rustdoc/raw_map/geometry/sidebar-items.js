2ff271b75fd487c29e4678edcdae013b874da6da
dabreegster <dabreegster@users.noreply.github.com>
2022-04-21 15:27:42 +0000

    deploy: d1756521497bfd2dd8c761fc681b6034bf88c442

--- rustdoc/raw_map/geometry/sidebar-items.js
+++ rustdoc/raw_map/geometry/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["intersection_polygon","Also returns a list of labeled polygons for debugging."],["intersection_polygon_v2",""],["osm2polygon","Process the file produced by `save_osm2polygon_input`, then write the output as GeoJSON."]],"mod":[["algorithm",""],["geojson",""]],"struct":[["InputRoad",""],["Results",""]]});
+initSidebarItems({"fn":[["intersection_polygon",""],["osm2polygon","Process the file produced by `save_osm2polygon_input`, then write the output as GeoJSON."]],"mod":[["algorithm",""],["geojson",""]],"struct":[["InputRoad",""],["Results",""]]});
