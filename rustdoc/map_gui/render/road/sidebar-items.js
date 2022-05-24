837d877b1759ecc59c479cb4260d48c60971162e
dabreegster <dabreegster@users.noreply.github.com>
2022-05-24 14:03:15 +0000

    deploy: ee7f843ff657316ff198c515a129ed967ebb420b

--- rustdoc/map_gui/render/road/sidebar-items.js
+++ rustdoc/map_gui/render/road/sidebar-items.js
@@ -1,1 +1,1 @@
-initSidebarItems({"fn":[["draw_building_driveway",""]],"struct":[["DrawRoad",""]]});
+initSidebarItems({"constant":[["DRAW_CURVEY_LABEL",""],["LABEL_SCALE_FACTOR",""]],"fn":[["draw_building_driveway",""],["render_center_line","If `text_width` is defined, don’t draw the center line in the middle of the road for this amount of space"]],"struct":[["DrawRoad",""]]});
