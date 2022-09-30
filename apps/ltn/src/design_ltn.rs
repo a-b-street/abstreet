a-b-street/abstreet

commit dd99afddc676abda7d3252c72a843c999f27a09a

Always store shortcuts with a neighbourhood. This should simplify the next UI changes, and once some more issues are fixed, speed up performance of switching between UIs.
Some lesser-used features will get a bit slower, though.

apps/ltn/src/filters/auto.rs
@@ -7,7 +7,6 @@ use abstutil::Timer;
 use map_model::RoadID;
 use widgetry::{Choice, EventCtx};
 
-use crate::shortcuts::find_shortcuts;
 use crate::{after_edit, App, Neighbourhood};
 
 #[derive(Clone, Copy, Debug, PartialEq)]
@@ -66,7 +65,7 @@ impl Heuristic {
         app.session.modal_filters.before_edit();
 
         match self {
-            Heuristic::Greedy => greedy(ctx, app, neighbourhood, timer),
+            Heuristic::Greedy => greedy(ctx, app, neighbourhood),
             Heuristic::BruteForce => brute_force(ctx, app, neighbourhood, timer),
             Heuristic::SplitCells => split_cells(ctx, app, neighbourhood, timer),
             Heuristic::OnlyOneBorder => only_one_border(app, neighbourhood),
@@ -82,12 +81,12 @@ impl Heuristic {
     }
 }
 
-fn greedy(ctx: &EventCtx, app: &mut App, neighbourhood: &Neighbourhood, timer: &mut Timer) {
-    let shortcuts = find_shortcuts(app, &neighbourhood, timer);
+fn greedy(ctx: &EventCtx, app: &mut App, neighbourhood: &Neighbourhood) {
     // TODO How should we break ties? Some shortcuts are worse than others; use that weight?
     // TODO Should this operation be per cell instead? We could hover on a road belonging to that
     // cell to select it
-    if let Some((r, _)) = shortcuts
+    if let Some((r, _)) = neighbourhood
+        .shortcuts
         .count_per_road
         .borrow()
         .iter()
@@ -115,11 +114,7 @@ fn brute_force(ctx: &EventCtx, app: &mut App, neighbourhood: &Neighbourhood, tim
             continue;
         }
         if let Some(new) = try_to_filter_road(ctx, app, neighbourhood, *r) {
-            let num_shortcuts =
-                // This spams too many logs, and can't be used within a start_iter anyway
-                find_shortcuts(app, &new, &mut Timer::throwaway())
-                    .paths
-                    .len();
+            let num_shortcuts = new.shortcuts.paths.len();
             // TODO Again, break ties. Just the number of paths is kind of a weak metric.
             if best.map(|(_, score)| num_shortcuts < score).unwrap_or(true) {
                 best = Some((*r, num_shortcuts));

apps/ltn/src/neighbourhood.rs
@@ -7,20 +7,20 @@ use map_gui::tools::DrawRoadLabels;
 use map_model::{Direction, IntersectionID, Map, PathConstraints, Perimeter, RoadID};
 use widgetry::{Drawable, EventCtx, GeomBatch};
 
+use crate::shortcuts::Shortcuts;
 use crate::{App, ModalFilters, NeighbourhoodID};
 
+// Once constructed, a Neighbourhood is immutable
 pub struct Neighbourhood {
     pub id: NeighbourhoodID,
 
-    // These're fixed
     pub orig_perimeter: Perimeter,
     pub perimeter: BTreeSet<RoadID>,
     pub borders: BTreeSet<IntersectionID>,
     pub interior_intersections: BTreeSet<IntersectionID>,
 
-    // The cells change as a result of modal filters, which're stored for all neighbourhoods in
-    // app.session.
     pub cells: Vec<Cell>,
+    pub shortcuts: Shortcuts,
 
     pub fade_irrelevant: Drawable,
     pub labels: DrawRoadLabels,
@@ -121,6 +121,7 @@ impl Neighbourhood {
             interior_intersections: BTreeSet::new(),
 
             cells: Vec::new(),
+            shortcuts: Shortcuts::empty(),
 
             fade_irrelevant: Drawable::empty(ctx),
             // Temporary value
@@ -163,6 +164,10 @@ impl Neighbourhood {
         label_roads.extend(n.orig_perimeter.interior.clone());
         n.labels =
             DrawRoadLabels::new(Box::new(move |r| label_roads.contains(&r.id))).light_background();
+
+        // TODO The timer could be nice for large areas. But plumbing through one everywhere is
+        // tedious, and would hit a nested start_iter bug anyway.
+        n.shortcuts = crate::shortcuts::find_shortcuts(app, &n, &mut abstutil::Timer::throwaway());
 
         n
     }

apps/ltn/src/edit/mod.rs
@@ -7,7 +7,6 @@ use widgetry::mapspace::{ObjectID, World};
 use widgetry::tools::PopupMsg;
 use widgetry::{EventCtx, Key, Line, Panel, PanelBuilder, Widget, DEFAULT_CORNER_RADIUS};
 
-use crate::shortcuts::Shortcuts;
 use crate::{after_edit, App, BrowseNeighbourhoods, Neighbourhood, Transition};
 
 // TODO This is only used for styling now
@@ -107,19 +106,12 @@ impl EditNeighbourhood {
         }
     }
 
-    pub fn new(
-        ctx: &mut EventCtx,
-        app: &App,
-        neighbourhood: &Neighbourhood,
-        shortcuts: &Shortcuts,
-    ) -> Self {
+    pub fn new(ctx: &mut EventCtx, app: &App, neighbourhood: &Neighbourhood) -> Self {
         Self {
             world: match app.session.edit_mode {
-                EditMode::Filters => filters::make_world(ctx, app, neighbourhood, shortcuts),
+                EditMode::Filters => filters::make_world(ctx, app, neighbourhood),
                 EditMode::Oneways => one_ways::make_world(ctx, app, neighbourhood),
-                EditMode::Shortcuts(focus) => {
-                    shortcuts::make_world(ctx, app, neighbourhood, shortcuts, focus)
-                }
+                EditMode::Shortcuts(focus) => shortcuts::make_world(ctx, app, neighbourhood, focus),
             },
         }
     }
@@ -130,7 +122,7 @@ impl EditNeighbourhood {
         app: &App,
         tab: Tab,
         top_panel: &Panel,
-        shortcuts: &Shortcuts,
+        neighbourhood: &Neighbourhood,
         per_tab_contents: Widget,
     ) -> PanelBuilder {
         let contents = Widget::col(vec![
@@ -143,7 +135,7 @@ impl EditNeighbourhood {
             match app.session.edit_mode {
                 EditMode::Filters => filters::widget(ctx, app),
                 EditMode::Oneways => one_ways::widget(ctx),
-                EditMode::Shortcuts(focus) => shortcuts::widget(ctx, app, shortcuts, focus),
+                EditMode::Shortcuts(focus) => shortcuts::widget(ctx, app, neighbourhood, focus),
             }
             .section(ctx),
             tab.make_buttons(ctx, app),

apps/ltn/src/browse.rs
@@ -32,10 +32,7 @@ impl BrowseNeighbourhoods {
                     app.session.alt_proposals = crate::save::AltProposals::new();
                     crate::clear_current_proposal(ctx, app, timer);
                 }
-                (
-                    make_world(ctx, app, timer),
-                    draw_over_roads(ctx, app, timer),
-                )
+                (make_world(ctx, app), draw_over_roads(ctx, app))
             });
 
         let top_panel = crate::components::TopPanel::panel(ctx, app);
@@ -127,8 +124,8 @@ impl State<App> for BrowseNeighbourhoods {
                     app.session.draw_neighbourhood_style = self.left_panel.dropdown_value("style");
 
                     ctx.loading_screen("change style", |ctx, timer| {
-                        self.world = make_world(ctx, app, timer);
-                        self.draw_over_roads = draw_over_roads(ctx, app, timer);
+                        self.world = make_world(ctx, app);
+                        self.draw_over_roads = draw_over_roads(ctx, app);
                     });
                 }
             }
@@ -160,7 +157,7 @@ impl State<App> for BrowseNeighbourhoods {
     }
 }
 
-fn make_world(ctx: &mut EventCtx, app: &App, timer: &mut Timer) -> World<NeighbourhoodID> {
+fn make_world(ctx: &mut EventCtx, app: &App) -> World<NeighbourhoodID> {
     let mut world = World::bounded(app.map.get_bounds());
     let map = &app.map;
     for (id, info) in app.session.partitioning.all_neighbourhoods() {
@@ -194,9 +191,9 @@ fn make_world(ctx: &mut EventCtx, app: &App, timer: &mut Timer) -> World<Neighbo
             }
             Style::Quietness => {
                 let neighbourhood = Neighbourhood::new(ctx, app, *id);
-                let shortcuts = crate::shortcuts::find_shortcuts(app, &neighbourhood, timer);
-                let (quiet_streets, total_streets) =
-                    shortcuts.quiet_and_total_streets(&neighbourhood);
+                let (quiet_streets, total_streets) = neighbourhood
+                    .shortcuts
+                    .quiet_and_total_streets(&neighbourhood);
                 let pct = if total_streets == 0 {
                     0.0
                 } else {
@@ -226,7 +223,7 @@ fn make_world(ctx: &mut EventCtx, app: &App, timer: &mut Timer) -> World<Neighbo
     world
 }
 
-fn draw_over_roads(ctx: &mut EventCtx, app: &App, timer: &mut Timer) -> ToggleZoomed {
+fn draw_over_roads(ctx: &mut EventCtx, app: &App) -> ToggleZoomed {
     if app.session.draw_neighbourhood_style != Style::Shortcuts {
         return ToggleZoomed::empty(ctx);
     }
@@ -236,9 +233,8 @@ fn draw_over_roads(ctx: &mut EventCtx, app: &App, timer: &mut Timer) -> ToggleZo
 
     for id in app.session.partitioning.all_neighbourhoods().keys() {
         let neighbourhood = Neighbourhood::new(ctx, app, *id);
-        let shortcuts = crate::shortcuts::find_shortcuts(app, &neighbourhood, timer);
-        count_per_road.extend(shortcuts.count_per_road);
-        count_per_intersection.extend(shortcuts.count_per_intersection);
+        count_per_road.extend(neighbourhood.shortcuts.count_per_road);
+        count_per_intersection.extend(neighbourhood.shortcuts.count_per_intersection);
     }
 
     // TODO It's a bit weird to draw one heatmap covering streets in every neighbourhood. The

apps/ltn/src/connectivity.rs
@@ -11,7 +11,6 @@ use widgetry::{
 use crate::draw_cells::RenderCells;
 use crate::edit::{EditMode, EditNeighbourhood, EditOutcome, Tab};
 use crate::filters::auto::Heuristic;
-use crate::shortcuts::{find_shortcuts, Shortcuts};
 use crate::{colors, App, Neighbourhood, NeighbourhoodID, Transition};
 
 pub struct Viewer {
@@ -45,7 +44,7 @@ impl Viewer {
     }
 
     fn update(&mut self, ctx: &mut EventCtx, app: &App) {
-        let (edit, draw_top_layer, draw_under_roads_layer, render_cells, highlight_cell, shortcuts) =
+        let (edit, draw_top_layer, draw_under_roads_layer, render_cells, highlight_cell) =
             setup_editing(ctx, app, &self.neighbourhood);
         self.edit = edit;
         self.draw_top_layer = draw_top_layer;
@@ -88,7 +87,7 @@ impl Viewer {
                 app,
                 Tab::Connectivity,
                 &self.top_panel,
-                &shortcuts,
+                &self.neighbourhood,
                 Widget::col(vec![
                     format!(
                         "Neighbourhood area: {}",
@@ -179,7 +178,7 @@ impl State<App> for Viewer {
                 app.session.heuristic = self.left_panel.dropdown_value("heuristic");
 
                 if x != "heuristic" {
-                    let (edit, draw_top_layer, draw_under_roads_layer, _, highlight_cell, _) =
+                    let (edit, draw_top_layer, draw_under_roads_layer, _, highlight_cell) =
                         setup_editing(ctx, app, &self.neighbourhood);
                     self.edit = edit;
                     self.draw_top_layer = draw_top_layer;
@@ -247,13 +246,8 @@ fn setup_editing(
     Drawable,
     RenderCells,
     World<DummyID>,
-    Shortcuts,
 ) {
-    let shortcuts = ctx.loading_screen("find shortcuts", |_, timer| {
-        find_shortcuts(app, neighbourhood, timer)
-    });
-
-    let edit = EditNeighbourhood::new(ctx, app, neighbourhood, &shortcuts);
+    let edit = EditNeighbourhood::new(ctx, app, neighbourhood);
     let map = &app.map;
 
     // Draw some stuff under roads and other stuff on top
@@ -295,10 +289,13 @@ fn setup_editing(
     }
 
     let mut colorer = ColorNetwork::no_fading(app);
-    colorer.ranked_roads(shortcuts.count_per_road.clone(), &app.cs.good_to_bad_red);
+    colorer.ranked_roads(
+        neighbourhood.shortcuts.count_per_road.clone(),
+        &app.cs.good_to_bad_red,
+    );
     // TODO These two will be on different scales, which'll look really weird!
     colorer.ranked_intersections(
-        shortcuts.count_per_intersection.clone(),
+        neighbourhood.shortcuts.count_per_intersection.clone(),
         &app.cs.good_to_bad_red,
     );
 
@@ -357,7 +354,6 @@ fn setup_editing(
         ctx.upload(draw_under_roads_layer),
         render_cells,
         highlight_cell,
-        shortcuts,
     )
 }
 

apps/ltn/src/edit/filters.rs
@@ -4,7 +4,6 @@ use widgetry::tools::open_browser;
 use widgetry::{lctrl, EventCtx, Image, Key, Line, Text, TextExt, Widget};
 
 use super::{EditOutcome, Obj};
-use crate::shortcuts::Shortcuts;
 use crate::{after_edit, colors, App, DiagonalFilter, Neighbourhood};
 
 pub fn widget(ctx: &mut EventCtx, app: &App) -> Widget {
@@ -40,12 +39,7 @@ pub fn widget(ctx: &mut EventCtx, app: &App) -> Widget {
 
 /// Creates clickable objects for managing filters on roads and intersections. Everything is
 /// invisible; the caller is responsible for drawing things.
-pub fn make_world(
-    ctx: &mut EventCtx,
-    app: &App,
-    neighbourhood: &Neighbourhood,
-    shortcuts: &Shortcuts,
-) -> World<Obj> {
+pub fn make_world(ctx: &mut EventCtx, app: &App, neighbourhood: &Neighbourhood) -> World<Obj> {
     let map = &app.map;
     let mut world = World::bounded(map.get_bounds());
 
@@ -58,7 +52,7 @@ pub fn make_world(
             .hover_outline(colors::OUTLINE, Distance::meters(5.0))
             .tooltip(Text::from(format!(
                 "{} possible shortcuts cross {}",
-                shortcuts.count_per_road.get(*r),
+                neighbourhood.shortcuts.count_per_road.get(*r),
                 road.get_name(app.opts.language.as_ref()),
             )))
             .hotkey(lctrl(Key::D), "debug")
@@ -74,7 +68,7 @@ pub fn make_world(
             .hover_outline(colors::OUTLINE, Distance::meters(5.0))
             .tooltip(Text::from(format!(
                 "{} possible shortcuts cross this intersection",
-                shortcuts.count_per_intersection.get(*i)
+                neighbourhood.shortcuts.count_per_intersection.get(*i)
             )))
             .clickable()
             .hotkey(lctrl(Key::D), "debug")

apps/ltn/src/edit/shortcuts.rs
@@ -4,19 +4,18 @@ use widgetry::mapspace::{World, WorldOutcome};
 use widgetry::{Color, EventCtx, Text, TextExt, Widget};
 
 use super::{EditMode, EditOutcome, Obj};
-use crate::shortcuts::Shortcuts;
 use crate::{colors, App, Neighbourhood};
 
 pub fn widget(
     ctx: &mut EventCtx,
     app: &App,
-    shortcuts: &Shortcuts,
+    neighbourhood: &Neighbourhood,
     focus: Option<RoadID>,
 ) -> Widget {
     match focus {
         Some(r) => Widget::col(vec![format!(
             "{} possible shortcuts cross {}",
-            shortcuts.count_per_road.get(r),
+            neighbourhood.shortcuts.count_per_road.get(r),
             app.map.get_r(r).get_name(app.opts.language.as_ref()),
         )
         .text_widget(ctx)]),
@@ -30,7 +29,6 @@ pub fn make_world(
     ctx: &mut EventCtx,
     app: &App,
     neighbourhood: &Neighbourhood,
-    shortcuts: &Shortcuts,
     focus: Option<RoadID>,
 ) -> World<Obj> {
     let map = &app.map;
@@ -52,7 +50,7 @@ pub fn make_world(
                 .hover_outline(colors::OUTLINE, Distance::meters(5.0))
                 .tooltip(Text::from(format!(
                     "{} possible shortcuts cross {}",
-                    shortcuts.count_per_road.get(*r),
+                    neighbourhood.shortcuts.count_per_road.get(*r),
                     road.get_name(app.opts.language.as_ref()),
                 )))
                 .clickable()

commit c1e352a32a6c43222817d1d58b8b5cec0aae0191

Only calculate the drawing parts of a Neighbourhood when needed. This fixes the speed problems introduced by dd99afddc676abda7d3252c72a843c999f27a09a for an earlier refactor

apps/ltn/src/design_ltn.rs
@@ -1,4 +1,5 @@
-use geom::{ArrowCap, Distance, PolyLine};
+use geom::{ArrowCap, Distance, PolyLine, Polygon};
+use map_gui::tools::DrawSimpleRoadLabels;
 use street_network::Direction;
 use widgetry::mapspace::{DummyID, World};
 use widgetry::tools::{ChooseSomething, PopupMsg};
@@ -19,6 +20,8 @@ pub struct DesignLTN {
     neighbourhood: Neighbourhood,
     draw_top_layer: Drawable,
     draw_under_roads_layer: Drawable,
+    fade_irrelevant: Drawable,
+    labels: DrawSimpleRoadLabels,
     highlight_cell: World<DummyID>,
     edit: EditNeighbourhood,
     // Expensive to calculate
@@ -35,7 +38,30 @@ impl DesignLTN {
     ) -> Box<dyn State<App>> {
         app.per_map.current_neighbourhood = Some(id);
 
-        let neighbourhood = Neighbourhood::new(ctx, app, id);
+        let neighbourhood = Neighbourhood::new(app, id);
+
+        let fade_area = Polygon::with_holes(
+            app.per_map
+                .map
+                .get_boundary_polygon()
+                .get_outer_ring()
+                .clone(),
+            vec![app
+                .per_map
+                .partitioning
+                .neighbourhood_boundary_polygon(app, id)
+                .into_outer_ring()],
+        );
+        let fade_irrelevant = GeomBatch::from(vec![(app.cs.fade_map_dark, fade_area)]).upload(ctx);
+
+        let mut label_roads = neighbourhood.perimeter.clone();
+        label_roads.extend(neighbourhood.orig_perimeter.interior.clone());
+        let labels = DrawSimpleRoadLabels::new(
+            ctx,
+            app,
+            colors::ROAD_LABEL,
+            Box::new(move |r| label_roads.contains(&r.id)),
+        );
 
         let mut state = Self {
             appwide_panel: AppwidePanel::new(ctx, app, Mode::ModifyNeighbourhood),
@@ -43,6 +69,8 @@ impl DesignLTN {
             neighbourhood,
             draw_top_layer: Drawable::empty(ctx),
             draw_under_roads_layer: Drawable::empty(ctx),
+            fade_irrelevant,
+            labels,
             highlight_cell: World::unbounded(),
             edit: EditNeighbourhood::temporary(),
             preserve_state: crate::save::PreserveState::DesignLTN(
@@ -201,7 +229,7 @@ impl State<App> for DesignLTN {
 
     fn draw(&self, g: &mut GfxCtx, app: &App) {
         app.draw_with_layering(g, |g| g.redraw(&self.draw_under_roads_layer));
-        g.redraw(&self.neighbourhood.fade_irrelevant);
+        g.redraw(&self.fade_irrelevant);
         self.draw_top_layer.draw(g);
         self.highlight_cell.draw(g);
         self.edit.world.draw(g);
@@ -209,7 +237,7 @@ impl State<App> for DesignLTN {
         self.appwide_panel.draw(g);
         self.bottom_panel.draw(g);
         app.session.layers.draw(g, app);
-        self.neighbourhood.labels.draw(g);
+        self.labels.draw(g);
         app.per_map.draw_all_filters.draw(g);
         app.per_map.draw_poi_icons.draw(g);
 
@@ -355,7 +383,7 @@ fn launch_autoplace_filters(ctx: &mut EventCtx, id: NeighbourhoodID) -> Transiti
         Heuristic::choices(),
         Box::new(move |heuristic, ctx, app| {
             match ctx.loading_screen("automatically filter a neighbourhood", |ctx, timer| {
-                let neighbourhood = Neighbourhood::new(ctx, app, id);
+                let neighbourhood = Neighbourhood::new(app, id);
                 heuristic.apply(ctx, app, &neighbourhood, timer)
             }) {
                 Ok(()) => Transition::Multi(vec![Transition::Pop, Transition::Recreate]),

apps/ltn/src/export.rs
@@ -1,18 +1,17 @@
 use anyhow::Result;
 
 use geom::{PolyLine, Pt2D};
-use widgetry::EventCtx;
 
 use crate::{App, Neighbourhood};
 
 /// Returns the path where the file was written
-pub fn write_geojson_file(ctx: &mut EventCtx, app: &App) -> Result<String> {
-    let contents = geojson_string(ctx, app)?;
+pub fn write_geojson_file(app: &App) -> Result<String> {
+    let contents = geojson_string(app)?;
     let path = format!("ltn_{}.geojson", app.per_map.map.get_name().map);
     abstio::write_file(path, contents)
 }
 
-fn geojson_string(ctx: &mut EventCtx, app: &App) -> Result<String> {
+fn geojson_string(app: &App) -> Result<String> {
     use geo::MapCoordsInPlace;
     use geojson::{Feature, FeatureCollection, GeoJson, Geometry, Value};
 
@@ -32,8 +31,7 @@ fn geojson_string(ctx: &mut EventCtx, app: &App) -> Result<String> {
         features.push(feature);
 
         // Cells per neighbourhood
-        let render_cells =
-            crate::draw_cells::RenderCells::new(map, &Neighbourhood::new(ctx, app, *id));
+        let render_cells = crate::draw_cells::RenderCells::new(map, &Neighbourhood::new(app, *id));
         for (idx, multipolygon) in render_cells.to_multipolygons().into_iter().enumerate() {
             let mut feature = Feature {
                 bbox: None,

apps/ltn/src/filters/auto.rs
@@ -65,9 +65,9 @@ impl Heuristic {
         app.per_map.edits.before_edit();
 
         match self {
-            Heuristic::Greedy => greedy(ctx, app, neighbourhood),
-            Heuristic::BruteForce => brute_force(ctx, app, neighbourhood, timer),
-            Heuristic::SplitCells => split_cells(ctx, app, neighbourhood, timer),
+            Heuristic::Greedy => greedy(app, neighbourhood),
+            Heuristic::BruteForce => brute_force(app, neighbourhood, timer),
+            Heuristic::SplitCells => split_cells(app, neighbourhood, timer),
             Heuristic::OnlyOneBorder => only_one_border(app, neighbourhood),
         }
 
@@ -81,7 +81,7 @@ impl Heuristic {
     }
 }
 
-fn greedy(ctx: &mut EventCtx, app: &mut App, neighbourhood: &Neighbourhood) {
+fn greedy(app: &mut App, neighbourhood: &Neighbourhood) {
     // TODO How should we break ties? Some shortcuts are worse than others; use that weight?
     // TODO Should this operation be per cell instead? We could hover on a road belonging to that
     // cell to select it
@@ -92,19 +92,14 @@ fn greedy(ctx: &mut EventCtx, app: &mut App, neighbourhood: &Neighbourhood) {
         .iter()
         .max_by_key(|pair| pair.1)
     {
-        if try_to_filter_road(ctx, app, neighbourhood, *r).is_none() {
+        if try_to_filter_road(app, neighbourhood, *r).is_none() {
             warn!("Filtering {} disconnects a cell, never mind", r);
             // TODO Try the next choice
         }
     }
 }
 
-fn brute_force(
-    ctx: &mut EventCtx,
-    app: &mut App,
-    neighbourhood: &Neighbourhood,
-    timer: &mut Timer,
-) {
+fn brute_force(app: &mut App, neighbourhood: &Neighbourhood, timer: &mut Timer) {
     // Which road leads to the fewest shortcuts?
     let mut best: Option<(RoadID, usize)> = None;
 
@@ -118,7 +113,7 @@ fn brute_force(
         if app.per_map.edits.roads.contains_key(r) {
             continue;
         }
-        if let Some(new) = try_to_filter_road(ctx, app, neighbourhood, *r) {
+        if let Some(new) = try_to_filter_road(app, neighbourhood, *r) {
             let num_shortcuts = new.shortcuts.paths.len();
             // TODO Again, break ties. Just the number of paths is kind of a weak metric.
             if best.map(|(_, score)| num_shortcuts < score).unwrap_or(true) {
@@ -132,16 +127,11 @@ fn brute_force(
     }
 
     if let Some((r, _)) = best {
-        try_to_filter_road(ctx, app, neighbourhood, r).unwrap();
+        try_to_filter_road(app, neighbourhood, r).unwrap();
     }
 }
 
-fn split_cells(
-    ctx: &mut EventCtx,
-    app: &mut App,
-    neighbourhood: &Neighbourhood,
-    timer: &mut Timer,
-) {
+fn split_cells(app: &mut App, neighbourhood: &Neighbourhood, timer: &mut Timer) {
     // Filtering which road leads to new cells with the MOST streets in the smaller cell?
     let mut best: Option<(RoadID, usize)> = None;
 
@@ -155,7 +145,7 @@ fn split_cells(
         if app.per_map.edits.roads.contains_key(r) {
             continue;
         }
-        if let Some(new) = try_to_filter_road(ctx, app, neighbourhood, *r) {
+        if let Some(new) = try_to_filter_road(app, neighbourhood, *r) {
             // Did we split the cell?
             if new.cells.len() > neighbourhood.cells.len() {
                 // Find the two new cells
@@ -183,7 +173,7 @@ fn split_cells(
     }
 
     if let Some((r, _)) = best {
-        try_to_filter_road(ctx, app, neighbourhood, r).unwrap();
+        try_to_filter_road(app, neighbourhood, r).unwrap();
     }
 }
 
@@ -217,7 +207,6 @@ fn only_one_border(app: &mut App, neighbourhood: &Neighbourhood) {
 // If successful, returns a Neighbourhood and leaves the new filter in place. If it disconncts a
 // cell, reverts the change and returns None
 fn try_to_filter_road(
-    ctx: &mut EventCtx,
     app: &mut App,
     neighbourhood: &Neighbourhood,
     r: RoadID,
@@ -227,8 +216,7 @@ fn try_to_filter_road(
         r,
         RoadFilter::new_by_user(road.length() / 2.0, app.session.filter_type),
     );
-    // TODO This is expensive; can we just do the design_ltn work and not drawing?
-    let new_neighbourhood = Neighbourhood::new(ctx, app, neighbourhood.id);
+    let new_neighbourhood = Neighbourhood::new(app, neighbourhood.id);
     if new_neighbourhood.cells.iter().any(|c| c.is_disconnected()) {
         app.per_map.edits.roads.remove(&r).unwrap();
         None

apps/ltn/src/neighbourhood.rs
@@ -3,12 +3,10 @@ use std::collections::{BTreeMap, BTreeSet};
 use maplit::btreeset;
 
 use geom::{ArrowCap, Distance, PolyLine, Polygon};
-use map_gui::tools::DrawSimpleRoadLabels;
 use map_model::{Direction, IntersectionID, Map, Perimeter, RoadID};
-use widgetry::{Drawable, EventCtx, GeomBatch};
 
 use crate::shortcuts::Shortcuts;
-use crate::{colors, is_private, App, Edits, NeighbourhoodID};
+use crate::{is_private, App, Edits, NeighbourhoodID};
 
 // Once constructed, a Neighbourhood is immutable
 pub struct Neighbourhood {
@@ -21,9 +19,6 @@ pub struct Neighbourhood {
 
     pub cells: Vec<Cell>,
     pub shortcuts: Shortcuts,
-
-    pub fade_irrelevant: Drawable,
-    pub labels: DrawSimpleRoadLabels,
 }
 
 /// A partitioning of the interior of a neighbourhood based on driving design_ltn
@@ -104,7 +99,7 @@ pub struct DistanceInterval {
 }
 
 impl Neighbourhood {
-    pub fn new(ctx: &mut EventCtx, app: &App, id: NeighbourhoodID) -> Neighbourhood {
+    pub fn new(app: &App, id: NeighbourhoodID) -> Neighbourhood {
         let map = &app.per_map.map;
         let orig_perimeter = app
             .per_map
@@ -122,9 +117,6 @@ impl Neighbourhood {
 
             cells: Vec::new(),
             shortcuts: Shortcuts::empty(),
-
-            fade_irrelevant: Drawable::empty(ctx),
-            labels: DrawSimpleRoadLabels::empty(ctx),
         };
 
         for id in &n.orig_perimeter.roads {
@@ -133,15 +125,6 @@ impl Neighbourhood {
             n.borders.insert(road.src_i);
             n.borders.insert(road.dst_i);
         }
-        let fade_area = Polygon::with_holes(
-            map.get_boundary_polygon().get_outer_ring().clone(),
-            vec![app
-                .per_map
-                .partitioning
-                .neighbourhood_boundary_polygon(app, id)
-                .into_outer_ring()],
-        );
-        n.fade_irrelevant = GeomBatch::from(vec![(app.cs.fade_map_dark, fade_area)]).upload(ctx);
 
         for r in &n.orig_perimeter.interior {
             let road = map.get_r(*r);
@@ -153,15 +136,6 @@ impl Neighbourhood {
         }
 
         n.cells = find_cells(map, &n.orig_perimeter, &n.borders, &app.per_map.edits);
-
-        let mut label_roads = n.perimeter.clone();
-        label_roads.extend(n.orig_perimeter.interior.clone());
-        n.labels = DrawSimpleRoadLabels::new(
-            ctx,
-            app,
-            colors::ROAD_LABEL,
-            Box::new(move |r| label_roads.contains(&r.id)),
-        );
 
         // TODO The timer could be nice for large areas. But plumbing through one everywhere is
         // tedious, and would hit a nested start_iter bug anyway.

apps/ltn/src/pick_area.rs
@@ -147,7 +147,7 @@ fn make_world(ctx: &mut EventCtx, app: &App) -> World<NeighbourhoodID> {
                         .build(ctx);
                 }
                 Style::Cells => {
-                    let neighbourhood = Neighbourhood::new(ctx, app, *id);
+                    let neighbourhood = Neighbourhood::new(app, *id);
                     let render_cells = crate::draw_cells::RenderCells::new(map, &neighbourhood);
                     let hovered_batch = render_cells.draw_colored_areas();
                     world
@@ -159,7 +159,7 @@ fn make_world(ctx: &mut EventCtx, app: &App) -> World<NeighbourhoodID> {
                         .build(ctx);
                 }
                 Style::Quietness => {
-                    let neighbourhood = Neighbourhood::new(ctx, app, *id);
+                    let neighbourhood = Neighbourhood::new(app, *id);
                     let (quiet_streets, total_streets) = neighbourhood
                         .shortcuts
                         .quiet_and_total_streets(&neighbourhood);
@@ -202,7 +202,7 @@ fn draw_over_roads(ctx: &mut EventCtx, app: &App) -> Drawable {
     let mut count_per_intersection = Counter::new();
 
     for id in app.per_map.partitioning.all_neighbourhoods().keys() {
-        let neighbourhood = Neighbourhood::new(ctx, app, *id);
+        let neighbourhood = Neighbourhood::new(app, *id);
         count_per_road.extend(neighbourhood.shortcuts.count_per_road);
         count_per_intersection.extend(neighbourhood.shortcuts.count_per_intersection);
     }

apps/ltn/src/save/mod.rs
@@ -374,7 +374,7 @@ impl AltProposals {
                 return Some(Transition::Push(share::ShareProposal::new_state(ctx, app)));
             }
             "Export GeoJSON" => {
-                let result = crate::export::write_geojson_file(ctx, app);
+                let result = crate::export::write_geojson_file(app);
                 return Some(Transition::Push(match result {
                     Ok(path) => PopupMsg::new_state(
                         ctx,