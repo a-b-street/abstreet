74a51b1afa2bd8f409def7fbc9a39279b9944def
dabreegster <dabreegster@users.noreply.github.com>
2022-10-22 14:10:36 +0000

    deploy: 1064ed257fe178689ea2ae021da22faea87980f7

---
+++ rustdoc/ltn/crossings/sidebar-items.js
@@ -0,0 +1,1 @@
+window.SIDEBAR_ITEMS = {"fn":[["boundary_roads",""],["draw_crossings",""],["draw_porosity",""],["help",""],["make_bottom_panel",""],["make_world",""],["svg_path",""]],"struct":[["Crossings",""],["ID",""]]};
