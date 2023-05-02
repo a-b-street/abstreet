1ec17a2c6338032c82031e8247f252eadcce2e87
AnishPhadnis <anishphadnis.ca@gmail.com>
2023-05-02 01:27:30 -0400

    handle duped ring points bug

--- apps/game/src/sandbox/time_warp.rs
+++ apps/game/src/sandbox/time_warp.rs
@@ -45,15 +45,20 @@ impl JumpToTime {
             Widget::col(vec![
                 Line("Jump to what time?").small_heading().into_widget(ctx),
                 if app.has_prebaked().is_some() {
-                    GeomBatch::from(vec![(
-                        ctx.style().icon_fg.alpha(0.7),
-                        area_under_curve(
-                            app.prebaked().active_agents(end_of_day),
-                            slider_width,
-                            50.0,
-                        ),
-                    )])
-                    .into_widget(ctx)
+                    if let Ok(polygon) = area_under_curve(
+                        app.prebaked().active_agents(end_of_day),
+                        slider_width,
+                        50.0,
+                    ) {
+                        GeomBatch::from(vec![(
+                            ctx.style().icon_fg.alpha(0.7),
+                            polygon,)])
+                        .into_widget(ctx)
+                    } else {
+                        // ADD LOG MSG HERE
+                        Widget::nothing()
+                    }
+
                 } else {
                     Widget::nothing()
                 },
@@ -401,7 +406,7 @@ impl State<App> for TimeWarpScreen {
     }
 }
 
-fn area_under_curve(raw: Vec<(Time, usize)>, width: f64, height: f64) -> Polygon {
+fn area_under_curve(raw: Vec<(Time, usize)>, width: f64, height: f64) -> Result<Polygon, anyhow::Error> {
     assert!(!raw.is_empty());
     let min_x = Time::START_OF_DAY;
     let min_y = 0;
@@ -421,7 +426,15 @@ fn area_under_curve(raw: Vec<(Time, usize)>, width: f64, height: f64) -> Polygon
     }
     downsampled.push(Pt2D::new(width, height));
     downsampled.push(downsampled[0]);
-    Ring::must_new(downsampled).into_polygon()
+
+    Ring::deduping_new(downsampled)
+        .map_or_else(|e| {
+            // ADD LOG MSG
+            Err(e)
+        },
+        |ring| {
+            Ok(ring.into_polygon())
+        })
 }
 
 // TODO Maybe color, put in helpers
