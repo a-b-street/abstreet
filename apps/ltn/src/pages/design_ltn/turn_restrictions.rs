e11265ac7f6ffc8f72f75d511ab06271238f407f
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2023-07-21 11:56:40 +0100

    failing attempt to show turn destinations on mouseover
    This *should* highlight possible destinations on mouseover events. However, it is not working correctly and fails often.

--- apps/ltn/src/pages/design_ltn/turn_restrictions.rs
+++ apps/ltn/src/pages/design_ltn/turn_restrictions.rs
@@ -1,12 +1,13 @@
 use geom::Distance;
 use map_model::{PathV2, RoadID};
-use osm2streets::RestrictionType;
+use osm2streets::{RestrictionType, Direction};
 use widgetry::mapspace::{World, WorldOutcome};
 use widgetry::{Color, EventCtx, GeomBatch, Key, Line, Text, TextExt, Widget};
 
 use super::{road_name, EditMode, EditOutcome, Obj};
 use crate::render::colors;
 use crate::{App, Neighbourhood};
+use crate::logic::turn_restrictions::destination_roads;
 
 use super::shortcuts::FocusedRoad;
 
@@ -38,6 +39,7 @@ pub fn make_world(
     let focused_road = focus.as_ref().map(|f| f.r);
 
     let mut restricted_destinations: Vec<RoadID> = Vec::new();
+    let mut possible_destinations: Vec<RoadID> = Vec::new();
     if focused_road.is_some() {
         let focused_r = map.get_r(focused_road.unwrap());
         for (restriction, r2) in &focused_r.turn_restrictions {
@@ -50,11 +52,24 @@ pub fn make_world(
             restricted_destinations.push(*via);
             restricted_destinations.push(*r2);
         }
+
+        // Account for one way streets when determining possible destinations
+        // TODO This accounts for the oneway direction of the source street, 
+        // but not the oneway direction of the destination street
+        possible_destinations = destination_roads(map, &focused_road.unwrap());
+
     }
 
     println!("TURN RESTRICTIONS: make_world :{:?}", focused_road);
     println!("TURN RESTRICTIONS: restricted_destinations :{:?}", restricted_destinations);
-    for r in &neighbourhood.interior_roads {
+    println!("TURN RESTRICTIONS: possible_destinations :{:?}", possible_destinations);
+
+    let all_r_id = [&neighbourhood.perimeter_roads, &neighbourhood.interior_roads]
+        .into_iter()
+        .flatten();
+
+    for r in all_r_id {
+    // for r in &neighbourhood.interior_roads {
         let road = map.get_r(*r);
         if focused_road == Some(*r) {
             let mut batch = GeomBatch::new();
@@ -81,11 +96,26 @@ pub fn make_world(
                 .draw(batch)
                 .build(ctx);
 
+        } else if possible_destinations.contains(r) {
+            let mut batch = GeomBatch::new();
+            batch.push(
+                Color::YELLOW,
+                road.get_thick_polygon().to_outline(Distance::meters(3.0)),
+            );
+
+            world
+                .add(Obj::Road(*r))
+                .hitbox(road.get_thick_polygon())
+                .draw(batch)
+                .build(ctx);
+
         } else {
             world
                 .add(Obj::Road(*r))
                 .hitbox(road.get_thick_polygon())
-                .draw_color(colors::LOCAL_ROAD_LABEL.invert())
+                .drawn_in_master_batch()
+                //.invisibly_hoverable()
+                //.draw_color(colors::LOCAL_ROAD_LABEL.invert())
                 .hover_color(colors::HOVER)
                 .tooltip(Text::from(format!(
                     "{}",
@@ -100,7 +130,7 @@ pub fn make_world(
     // Highlight the current prohibited destination roads
     // Highlight the potential prohibited destination roads
 
-    // world.initialize_hover(ctx);
+    world.initialize_hover(ctx);
     world
 }
 
@@ -109,7 +139,7 @@ pub fn handle_world_outcome(
     outcome: WorldOutcome<Obj>,
     neighbourhood: &Neighbourhood,
 ) -> EditOutcome {
-        println!("TURN RESTRICTIONS: handle_world_outcome");
+        // println!("TURN RESTRICTIONS: handle_world_outcome");
 
     match outcome {
         WorldOutcome::ClickedObject(Obj::Road(r)) => {
@@ -123,20 +153,30 @@ pub fn handle_world_outcome(
 
 
             let subset = neighbourhood.shortcuts.subset(neighbourhood, r);
-            if subset.paths.is_empty() {
-                EditOutcome::Nothing
-            } else {
-                app.session.edit_mode = EditMode::TurnRestrictions(Some(FocusedRoad {
-                    r,
-                    paths: subset.paths,
-                    current_idx: 0,
-                }));
-                EditOutcome::UpdatePanelAndWorld
-            }
+            app.session.edit_mode = EditMode::TurnRestrictions(Some(FocusedRoad {
+                r,
+                paths: subset.paths,
+                current_idx: 0,
+            }));
+            println!("TURN RESTRICTIONS: handle_world_outcome - updatepanelandworld");
+            EditOutcome::UpdatePanelAndWorld
         }
         WorldOutcome::ClickedFreeSpace(_) => {
             app.session.edit_mode = EditMode::TurnRestrictions(None);
             EditOutcome::UpdatePanelAndWorld
+        }
+        WorldOutcome::HoverChanged(Some(Obj::Road(old_r)), Some(Obj::Road(new_r))) => {
+            // TODO Fixme. At present I'm just doing this so that I can reuse the FocusedRoad
+            // struct here. I suspect that it would be better to create a single path which artificially 
+            // runs from one end of the FocusRoad to the other.
+            let subset = neighbourhood.shortcuts.subset(neighbourhood, new_r);
+            app.session.edit_mode = EditMode::TurnRestrictions(Some(FocusedRoad {
+                r: new_r,
+                paths: subset.paths,
+                current_idx: 0,
+            }));
+            println!("TURN RESTRICTIONS: handle_world_outcome - hoverChanged");
+            EditOutcome::UpdateAll
         }
         _ => EditOutcome::Nothing,
     }
