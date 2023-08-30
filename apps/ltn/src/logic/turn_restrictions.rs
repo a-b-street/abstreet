3aff317824d6898254839c68acfaa8041ba7d215
Andy Smith <andrewphilipsmith@users.noreply.github.com>
2023-08-30 16:56:38 +0100

    first pass at applying consistent language for turn restrictions

--- apps/ltn/src/logic/turn_restrictions.rs
+++ apps/ltn/src/logic/turn_restrictions.rs
@@ -346,9 +346,9 @@ mod tests {
 
 }
 
-fn is_road_drivable_from_i(map: &Map, r: RoadID, i: IntersectionID) -> bool {
+fn is_road_drivable_from_i(map: &Map, target_r: RoadID, i: IntersectionID) -> bool {
 
-    let road = map.get_r(r);
+    let road = map.get_r(target_r);
     let one_way = road.oneway_for_driving();
     
     return (road.src_i == i && one_way != Some(Direction::Back)) ||
@@ -359,7 +359,7 @@ fn is_road_drivable_from_i(map: &Map, r: RoadID, i: IntersectionID) -> bool {
 #[cfg(test)]
 mod tests {
     use tests::{import_map, get_test_file_path};
-    use super::{destination_roads, restricted_destination_roads, FocusedTurns};
+    use super::{possible_destination_roads, restricted_destination_roads, FocusedTurns};
     use map_model::{RoadID, IntersectionID};
     use std::collections::HashSet;
     use geom::Pt2D;
@@ -404,24 +404,27 @@ mod tests {
         let map = import_map(file_name.unwrap());
 
         // hard coded values for "turn_restriction_ltn_boundary"
-        let src_r = RoadID(11);
-        let src_road = map.get_r(src_r);
-        let expected_all_r = vec![3usize, 4, 9, 12].iter().map(|n| RoadID(*n)).collect::<HashSet<_>>();
-        let expected_filters_dst_i = vec![9usize, 12].iter().map(|n| RoadID(*n)).collect::<HashSet<_>>();
-        let expected_filters_src_i = vec![3usize, 4].iter().map(|n| RoadID(*n)).collect::<HashSet<_>>();
+        let from_r = RoadID(11);
+        let from_road = map.get_r(from_r);
+        // Expected possible turns for either intersection
+        let expected_possible_all_r = vec![3usize, 4, 9, 12].iter().map(|n| RoadID(*n)).collect::<HashSet<_>>();
+        // Expected possible turns via `from_r.dst_i`
+        let expected_possible_for_dst_i = vec![9usize, 12].iter().map(|n| RoadID(*n)).collect::<HashSet<_>>();
+        // Expected possible turns via `from_r.src_i`
+        let expected_possible_for_src_i = vec![3usize, 4].iter().map(|n| RoadID(*n)).collect::<HashSet<_>>();
 
         // Three test cases
         for (i , expected) in [
-            (None, expected_all_r),
-            (Some(src_road.dst_i), expected_filters_dst_i),
-            (Some(src_road.src_i), expected_filters_src_i),
+            (None, expected_possible_all_r),
+            (Some(from_road.dst_i), expected_possible_for_dst_i),
+            (Some(from_road.src_i), expected_possible_for_src_i),
         ] {
-            let actual_vec = destination_roads(&map, src_r, i);
+            let actual_vec = possible_destination_roads(&map, from_r, i);
             let mut actual = HashSet::<RoadID>::new();
             actual.extend(actual_vec.iter());
 
-            for dst_r in actual.iter() {
-                println!("destination_roads, src_r {}, dst_r = {}", src_r, dst_r);
+            for target_r in actual.iter() {
+                println!("destination_roads, src_r {}, dst_r = {}", from_r, target_r);
             }
             assert_eq!(actual, expected);
         }
@@ -432,42 +435,42 @@ mod tests {
     fn test_destination_roads_connected_one_ways() -> Result<(), anyhow::Error> {
         struct TurnRestrictionTestCase {
             pub input_file: String,
-            pub r: RoadID,
-            pub permitted_dst_i: HashSet<RoadID>,
-            pub permitted_src_i: HashSet<RoadID>,
-            pub prohibited_dst_i: HashSet<RoadID>,
-            pub prohibited_src_i: HashSet<RoadID>,
+            pub from_r: RoadID,
+            pub possible_for_dst_i: HashSet<RoadID>,
+            pub possible_for_src_i: HashSet<RoadID>,
+            pub restricted_for_dst_i: HashSet<RoadID>,
+            pub restricted_for_src_i: HashSet<RoadID>,
         }
         
         let test_cases = [
             TurnRestrictionTestCase {
                 input_file: String::from("input/false_positive_u_turns.osm"),
                 // north end is dst according to JOSM
-                r: RoadID(5),
+                from_r: RoadID(5),
                 // Can continue on north bound left-hand lane past central barrier
-                permitted_dst_i: HashSet::from([RoadID(1)]),
+                possible_for_dst_i: HashSet::from([RoadID(1)]),
                 // Cannot continue on southbound right-hand (opposing oneway) past barrier RoadID(3)
                 // but this is already restricted by virtue of being oneway 
-                prohibited_dst_i: HashSet::new(),
+                restricted_for_dst_i: HashSet::new(),
                 // Can continue south onto Tyne bridge (RoadID(0))
                 // Right turn would prevent turing onto right on Pilgrim Street North bound (RoadID(2))
-                permitted_src_i: HashSet::from([RoadID(2), RoadID(0)]),
+                possible_for_src_i: HashSet::from([RoadID(2), RoadID(0)]),
                 // Cannot turn right onto Pilgrim Street North bound (RoadID(2)).
                 // Also cannot go backward up southbound ramp (RoadID(4) which is a oneway.
-                prohibited_src_i: HashSet::from([RoadID(2)]),
+                restricted_for_src_i: HashSet::from([RoadID(2)]),
             },
             TurnRestrictionTestCase {
                 input_file: String::from("input/false_positive_u_turns.osm"),
                 // north end is src according to JOSM
-                r: RoadID(0),
+                from_r: RoadID(0),
                 // Off the edge of the map
-                permitted_dst_i: HashSet::new(),
+                possible_for_dst_i: HashSet::new(),
                 // Off the edge of the map
-                prohibited_dst_i: HashSet::new(),
+                restricted_for_dst_i: HashSet::new(),
                 // Can continue south onto Tyne bridge
-                permitted_src_i: HashSet::from([RoadID(5), RoadID(2)]),
+                possible_for_src_i: HashSet::from([RoadID(5), RoadID(2)]),
                 // Cannot turn right onto Pilgrim Street North bound - Cannot go backward up southbound oneway ramp (RoadID(4))
-                prohibited_src_i: HashSet::new(),
+                restricted_for_src_i: HashSet::new(),
             },
         ];
 
@@ -477,23 +480,23 @@ mod tests {
             let map = import_map(file_name.unwrap());
 
             // Three combinations of road/intersection for each test case
-            for (i , expected_permitted, expected_prohibited) in [
-                (Some(map.get_r(tc.r).dst_i), tc.permitted_dst_i, tc.prohibited_dst_i),
-                (Some(map.get_r(tc.r).src_i), tc.permitted_src_i, tc.prohibited_src_i),
+            for (i , expected_possible, expected_restricted) in [
+                (Some(map.get_r(tc.from_r).dst_i), tc.possible_for_dst_i, tc.restricted_for_dst_i),
+                (Some(map.get_r(tc.from_r).src_i), tc.possible_for_src_i, tc.restricted_for_src_i),
                 // (None,
                 //  tc.permitted_dst_i.union(tc.permitted_src_i).collect::<HashSet<_>>(),
                 //  tc.prohibited_dst_i.union(tc.prohibited_src_i).collect::<HashSet<_>>()
                 // )
             ] {
-                let actual_permitted = destination_roads(&map, tc.r, i);
-                let actual_prohibited = restricted_destination_roads(&map, tc.r, i);
+                let actual_possible = possible_destination_roads(&map, tc.from_r, i);
+                let actual_restricted = restricted_destination_roads(&map, tc.from_r, i);
 
-                println!("r={:?}, i={:?}, file={:?}", &tc.r, i, &tc.input_file);
-                for dst_r in actual_permitted.iter() {
-                    println!("destination_roads, src_r {}, dst_r = {}", tc.r, dst_r);
+                println!("r={:?}, i={:?}, file={:?}", &tc.from_r, i, &tc.input_file);
+                for target_r in actual_possible.iter() {
+                    println!("destination_roads, src_r {}, dst_r = {}", tc.from_r, target_r);
                 }
-                assert_eq!(actual_prohibited, expected_prohibited);
-                assert_eq!(actual_permitted, expected_permitted);
+                assert_eq!(actual_restricted, expected_restricted);
+                assert_eq!(actual_possible, expected_possible);
             }
         }
         Ok(())
