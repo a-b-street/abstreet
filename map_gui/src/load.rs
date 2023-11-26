4daf9a89aceef6e072f71992d248a4c5e8099028
Dustin Carlino <dabreegster@gmail.com>
2023-11-26 19:16:54 +0000

    Add a Hebrew font. #1128

--- map_gui/src/load.rs
+++ map_gui/src/load.rs
@@ -31,6 +31,7 @@ impl MapLoader {
     ) -> Box<dyn State<A>> {
         // TODO Generalize this more, maybe with some kind of country code -> font config
         if let Some(extra_font) = match name.city.country.as_ref() {
+            "il" => Some("NotoSansHebrew-Regular.ttf"),
             "ir" | "ly" => Some("NotoSansArabic-Regular.ttf"),
             "jp" => Some("NotoSerifCJKtc-Regular.otf"),
             "tw" => Some("NotoSerifCJKtc-Regular.otf"),
