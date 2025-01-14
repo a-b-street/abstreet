4e96ba48041e8076ab67c5a015fe9a2d6966567f
dabreegster <dabreegster@users.noreply.github.com>
2025-01-14 22:12:18 +0000

    deploy: 8c0ae4a7c1e6fc23a19f663207292e2612a67085

--- rustdoc/trait.impl/abstutil/clone/trait.CloneableAny.js
+++ rustdoc/trait.impl/abstutil/clone/trait.CloneableAny.js
@@ -1,9 +1,9 @@
 (function() {
-    var implementors = Object.fromEntries([["abstio",[["impl <a class=\"trait\" href=\"abstutil/clone/trait.CloneableAny.html\" title=\"trait abstutil::clone::CloneableAny\">CloneableAny</a> for <a class=\"struct\" href=\"abstio/abst_paths/struct.MapName.html\" title=\"struct abstio::abst_paths::MapName\">MapName</a>"]]],["abstutil",[]],["sim",[["impl CloneableAny for <a class=\"struct\" href=\"sim/struct.TripID.html\" title=\"struct sim::TripID\">TripID</a>"]]]]);
+    var implementors = Object.fromEntries([["abstio",[["impl CloneableAny for <a class=\"struct\" href=\"abstio/abst_paths/struct.MapName.html\" title=\"struct abstio::abst_paths::MapName\">MapName</a>"]]],["abstutil",[]],["sim",[["impl CloneableAny for <a class=\"struct\" href=\"sim/struct.TripID.html\" title=\"struct sim::TripID\">TripID</a>"]]]]);
     if (window.register_implementors) {
         window.register_implementors(implementors);
     } else {
         window.pending_implementors = implementors;
     }
 })()
-//{"start":57,"fragment_lengths":[279,16,128]}
+//{"start":57,"fragment_lengths":[162,16,128]}
